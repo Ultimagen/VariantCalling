5f2c338bcc29b9086b1c37d31bae489b3c384d29
Ilya Soifer <ilya.soifer@ultimagen.com>
2019-10-17 08:22:41 +0000

    Adapted parameters to Ultima convention

--- src/python/pipelines/error_rate_metrics_pipeline.py
+++ src/python/pipelines/error_rate_metrics_pipeline.py
@@ -6,32 +6,32 @@ from os.path import join as pjoin
 params = vc_calling_pipeline_utils.parse_params_file( sys.argv[1] )
 print(params)
 vc_pipeline = ruffus.Pipeline(name="Error rate estimation pipeline")
-vc_pipeline.mkdir(params.output_dir)
-vc_pipeline.mkdir(pjoin(params.output_dir, "logs"))
+vc_pipeline.mkdir(params.em_vc_output_dir)
+vc_pipeline.mkdir(pjoin(params.em_vc_output_dir, "logs"))
 
-head_file = vc_pipeline.transform(vc_calling_pipeline_utils.head_file, params.demux_file, ruffus.formatter(), 
-	[pjoin(params.output_dir, "{basename[0]}.head.bam"),
-	pjoin(params.output_dir, "logs", "{basename[0]}.head.log")], params.number_to_sample)
+head_file = vc_pipeline.transform(vc_calling_pipeline_utils.head_file, params.em_vc_demux_file, ruffus.formatter(), 
+	[pjoin(params.em_vc_output_dir, "{basename[0]}.head.bam"),
+	pjoin(params.em_vc_output_dir, "logs", "{basename[0]}.head.log")], params.em_vc_number_to_sample)
 
 aln = vc_pipeline.transform(vc_calling_pipeline_utils.align, head_file, 
-	ruffus.formatter(), [pjoin(params.output_dir, "{basename[0]}.aln.bam"), 
-	pjoin(params.output_dir, "logs", "{basename[0]}.aln.log")],
-	extras=[params.genome])
+	ruffus.formatter(), [pjoin(params.em_vc_output_dir, "{basename[0]}.aln.bam"), 
+	pjoin(params.em_vc_output_dir, "logs", "{basename[0]}.aln.log")],
+	extras=[params.em_vc_genome])
 
 filtered_bam = vc_pipeline.transform(vc_calling_pipeline_utils.filter_quality , aln, ruffus.formatter(".aln.bam"), 
-	[pjoin(params.output_dir, "{basename[0]}.filter.aln.bam"), 
-	                             pjoin(params.output_dir, "logs", "{basename[0]}.filter.aln.log")])
+	[pjoin(params.em_vc_output_dir, "{basename[0]}.filter.aln.bam"), 
+	                             pjoin(params.em_vc_output_dir, "logs", "{basename[0]}.filter.aln.log")])
 
 sorted_bam = vc_pipeline.transform(vc_calling_pipeline_utils.sort_file, filtered_bam, ruffus.formatter("filter.aln.bam"), 
-	[pjoin(params.output_dir, "{basename[0]}.sort.bam"), 
-     pjoin(params.output_dir, "logs", "{basename[0]}.sort.log")])
+	[pjoin(params.em_vc_output_dir, "{basename[0]}.sort.bam"), 
+     pjoin(params.em_vc_output_dir, "logs", "{basename[0]}.sort.log")])
 
 index_bam = vc_pipeline.transform(vc_calling_pipeline_utils.index_file, sorted_bam, ruffus.formatter("sort.bam"), 
-	[pjoin(params.output_dir, "{basename[0]}.bam.bai"), 
-     pjoin(params.output_dir, "logs", "{basename[0]}.index.log")])
+	[pjoin(params.em_vc_output_dir, "{basename[0]}.bam.bai"), 
+     pjoin(params.em_vc_output_dir, "logs", "{basename[0]}.index.log")])
 
 error_metrics = vc_pipeline.transform(vc_calling_pipeline_utils.error_metrics, sorted_bam, 
-	ruffus.formatter("sort.bam"), [pjoin(params.output_dir, "{basename[0]}.metrics"), 
-     							   pjoin(params.output_dir, "logs", "{basename[0]}.metric.log")],
-     							   extras=[params.genome])
+	ruffus.formatter("sort.bam"), [pjoin(params.em_vc_output_dir, "{basename[0]}.metrics"), 
+     							   pjoin(params.em_vc_output_dir, "logs", "{basename[0]}.metric.log")],
+     							   extras=[params.em_vc_genome])
 vc_pipeline.run()
--- src/python/pipelines/vc_calling_pipeline.py
+++ src/python/pipelines/vc_calling_pipeline.py
@@ -7,44 +7,44 @@ from os.path import join as pjoin
 params = vc_calling_pipeline_utils.parse_params_file( sys.argv[1] )
 print(params)
 vc_pipeline = ruffus.Pipeline(name="Variant calling pipeline")
-vc_pipeline.mkdir(params.output_dir)
-vc_pipeline.mkdir(pjoin(params.output_dir, "logs"))
-mdtask=vc_pipeline.mkdir(pjoin(params.output_dir, "interval_files"))
+vc_pipeline.mkdir(params.em_vc_output_dir)
+vc_pipeline.mkdir(pjoin(params.em_vc_output_dir, "logs"))
+mdtask=vc_pipeline.mkdir(pjoin(params.em_vc_output_dir, "interval_files"))
 
-aln_merge = vc_pipeline.transform(vc_calling_pipeline_utils.align_and_merge, params.demux_file, 
-	ruffus.formatter(), [pjoin(params.output_dir, "{basename[0]}.aln.bam"), 
-	pjoin(params.output_dir, "logs", "{basename[0]}.aln.log")],
-	extras=[params.genome])
-fi_file = vc_pipeline.transform(vc_calling_pipeline_utils.prepare_fetch_intervals, params.chromosomes_list, 
+aln_merge = vc_pipeline.transform(vc_calling_pipeline_utils.align_and_merge, params.em_vc_demux_file, 
+	ruffus.formatter(), [pjoin(params.em_vc_output_dir, "{basename[0]}.aln.bam"), 
+	pjoin(params.em_vc_output_dir, "logs", "{basename[0]}.aln.log")],
+	extras=[params.em_vc_genome])
+fi_file = vc_pipeline.transform(vc_calling_pipeline_utils.prepare_fetch_intervals, params.em_vc_chromosomes_list, 
 	ruffus.formatter(),
-	pjoin(params.output_dir, "filter.bed"), extras=[params.genome])
+	pjoin(params.em_vc_output_dir, "filter.bed"), extras=[params.em_vc_genome])
 
 filtered_bam = vc_pipeline.transform(vc_calling_pipeline_utils.fetch_intervals , aln_merge, ruffus.formatter(".aln.bam"), 
-	ruffus.add_inputs(fi_file), [pjoin(params.output_dir, "{basename[0]}.filter.aln.bam"), 
-	                             pjoin(params.output_dir, "logs", "{basename[0]}.filter.aln.log")])
+	ruffus.add_inputs(fi_file), [pjoin(params.em_vc_output_dir, "{basename[0]}.filter.aln.bam"), 
+	                             pjoin(params.em_vc_output_dir, "logs", "{basename[0]}.filter.aln.log")])
 
 sorted_bam = vc_pipeline.transform(vc_calling_pipeline_utils.sort_file, filtered_bam, ruffus.formatter("filter.aln.bam"), 
-	[pjoin(params.output_dir, "{basename[0]}.sort.bam"), 
-     pjoin(params.output_dir, "logs", "{basename[0]}.sort.log")])
+	[pjoin(params.em_vc_output_dir, "{basename[0]}.sort.bam"), 
+     pjoin(params.em_vc_output_dir, "logs", "{basename[0]}.sort.log")])
 
 recalibrated_bam = vc_pipeline.transform(vc_calling_pipeline_utils.recalibrate_file, sorted_bam, ruffus.formatter("sort.bam"), 
-	[pjoin(params.output_dir, "{basename[0]}.recal.bam"), 
-     pjoin(params.output_dir, "logs", "{basename[0]}.recal.log")], extras=[params.recalibration_model])
+	[pjoin(params.em_vc_output_dir, "{basename[0]}.recal.bam"), 
+     pjoin(params.em_vc_output_dir, "logs", "{basename[0]}.recal.log")], extras=[params.em_vc_recalibration_model])
 
 index_bam = vc_pipeline.transform(vc_calling_pipeline_utils.index_file, recalibrated_bam, ruffus.formatter("recal.bam"), 
-	[pjoin(params.output_dir, "{basename[0]}.bam.bai"), 
-     pjoin(params.output_dir, "logs", "{basename[0]}.index.log")])
+	[pjoin(params.em_vc_output_dir, "{basename[0]}.bam.bai"), 
+     pjoin(params.em_vc_output_dir, "logs", "{basename[0]}.index.log")])
 
 vc_intervals = vc_pipeline.transform(vc_calling_pipeline_utils.create_variant_calling_intervals, fi_file, ruffus.formatter("filter.bed"), 
-	[pjoin(params.output_dir, "{basename[0]}.vc.intervals.bed"), 
-     pjoin(params.output_dir, "logs", "{basename[0]}.vc.intervals.log")])
+	[pjoin(params.em_vc_output_dir, "{basename[0]}.vc.intervals.bed"), 
+     pjoin(params.em_vc_output_dir, "logs", "{basename[0]}.vc.intervals.log")])
 
 vc_intervals_split = vc_pipeline.subdivide(vc_calling_pipeline_utils.split_intervals_into_files, vc_intervals, ruffus.formatter(".bed"), 
-	pjoin(params.output_dir, "interval_files", "{basename[0]}.*"), pjoin(params.output_dir, "interval_files", "{basename[0]}")).follows(mdtask)
+	pjoin(params.em_vc_output_dir, "interval_files", "{basename[0]}.*"), pjoin(params.em_vc_output_dir, "interval_files", "{basename[0]}")).follows(mdtask)
 
 vc_variant_calls = vc_pipeline.product(vc_calling_pipeline_utils.variant_calling, recalibrated_bam, ruffus.formatter(".bam"), 
 										vc_intervals_split, ruffus.formatter(), 
-										[pjoin(params.output_dir, "{basename[0][0]}{ext[1][0]}"),
-										pjoin(params.output_dir, "logs", "{basename[0][0]}{ext[1][0]}.log"),], 
-										extras=[params.genome])
+										[pjoin(params.em_vc_output_dir, "{basename[0][0]}{ext[1][0]}"),
+										pjoin(params.em_vc_output_dir, "logs", "{basename[0][0]}{ext[1][0]}.log"),], 
+										extras=[params.em_vc_genome])
 vc_pipeline.run()
