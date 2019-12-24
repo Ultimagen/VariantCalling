fd65765f6f2d1c39618bbfcbfda92680b72a3242
Ilya Soifer <ilya.soifer@ultimagen.com>
2019-12-24 21:51:12 +0000

    Updated variant calling comparison pipeline - ability to add suffix etc.

--- src/python/pipelines/run_comparison_pipeline.py
+++ src/python/pipelines/run_comparison_pipeline.py
@@ -1,29 +1,29 @@
-from . import comparison_pipeline
-from . import vcf_pipeline_utils
+from python.pipelines import comparison_pipeline
+from python.pipelines import vcf_pipeline_utils
 import argparse
 
 
 
 ap = argparse.ArgumentParser("Compare VCF to ground truth")
-ap.add("--n_parts", 'Number of parts that the VCF is split into', required=True, type=int)
-ap.add("--input_prefix", "Prefix of the input file", required=True, type=str)
-ap.add("--output_file", 'Output h5 file', requried=True, type=str)
-ap.add("--header_file", "Desired VCF header of the file", required=True, type=str)
-ap.add("--gtr_vcf", 'Ground truth VCF file', required=True,type=str)
-ap.add("--cmp_intervals", 'Ranges on which to perform comparison', required=True, type=str)
-ap.add("--highconf_intervals", 'High confidence intervals', required=True, type=str )
-ap.add("--runs_intervals", 'Runs intervals', required=False, type=str, default=None)
-ap.add("--reference", 'Reference genome', required=True, type=str)
-ap.add("--aligned_bam", 'Aligned bam', required=True, type=str)
-ap.add("--call_sample_name", 'Name of the call sample', required=True, default='sm1')
-ap.add("--truth_sample_name", 'Name of the truth sample', required=True)
-ap.add("--find_thresholds", 'Should precision recall thresholds be found', type=bool, default=False, action='store_true')
-
+ap.add_argument("--n_parts", help='Number of parts that the VCF is split into', required=True, type=int)
+ap.add_argument("--input_prefix", help="Prefix of the input file", required=True, type=str)
+ap.add_argument("--output_file", help='Output h5 file', required=True, type=str)
+ap.add_argument("--header_file", help="Desired VCF header of the file", required=True, type=str)
+ap.add_argument("--gtr_vcf", help='Ground truth VCF file', required=True,type=str)
+ap.add_argument("--cmp_intervals", help='Ranges on which to perform comparison', required=True, type=str)
+ap.add_argument("--highconf_intervals", help='High confidence intervals', required=True, type=str )
+ap.add_argument("--runs_intervals", help='Runs intervals', required=False, type=str, default=None)
+ap.add_argument("--reference", help='Reference genome', required=True, type=str)
+ap.add_argument("--aligned_bam", help='Aligned bam', required=True, type=str)
+ap.add_argument("--call_sample_name", help='Name of the call sample', required=True, default='sm1')
+ap.add_argument("--truth_sample_name", help='Name of the truth sample', required=True)
+ap.add_argument("--find_thresholds", help='Should precision recall thresholds be found', default=False, action='store_true')
+ap.add_argument("--output_suffix", help='Add suffix to the output file', required=False, default='', type=str)
 args = ap.parse_args()
 results = comparison_pipeline.pipeline(args.n_parts, args.input_prefix, 
     args.header_file, args.gtr_vcf, args.cmp_intervals, args.highconf_intervals, 
     args.runs_intervals, args.reference, args.call_sample_name, 
-    args.truth_sample_name, args.find_thresholds)
+    args.truth_sample_name, args.find_thresholds, args.output_suffix)
 
 if args.find_thresholds : 
     concordance = results[0]
