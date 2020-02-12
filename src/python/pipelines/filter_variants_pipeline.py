24b65830241d7f3a34c25ef965fb1a6223860b7e
Ilya Soifer <ilya.soifer@ultimagenome.com>
2020-02-12 17:43:47 +0200

    Version of filter_variants_pipeline.py

--- src/python/pipelines/filter_variants_pipeline.py
+++ src/python/pipelines/filter_variants_pipeline.py
@@ -21,18 +21,31 @@ df = vcftools.classify_indel(df)
 df = vcftools.is_hmer_indel(df, args.reference_file)
 df = vcftools.get_motif_around(df, 5, args.reference_file)
 
-df.to_hdf(args.output_file + ".in.vcf.h5")
+
 models_dict = pickle.load(open(args.model_file, "rb"))
-models = models_dict[args.model_name]
+model_name = args.model_name
+models = models_dict[model_name]
+
 if type(models) == list or type(models) == tuple:
-    model = models[0]
+    model_clsf = models[0]
+    model_scor = models[1]
+    is_decision_tree = True
 else:
-    model = models
-predictions = model.predict(
+    model_clsf = models
+    is_decision_tree = False
+
+
+predictions = model_clsf.predict(
     variant_filtering_utils.add_grouping_column(df,
-                                                variant_filtering_utils.get_training_selection_functions(), "group"))
+                                                variant_filtering_utils.get_training_selection_functions(),
+                                                "group"))
+predictions_score = model_scor.predict(
+    variant_filtering_utils.add_grouping_column(df,
+                                                variant_filtering_utils.get_training_selection_functions(),
+                                                "group"))
 
 predictions = np.array(predictions)
+predictions_score = np.array(predictions_score)
 
 hmer_run = np.array(df.close_to_hmer_run | df.inside_hmer_run)
 
@@ -40,6 +53,8 @@ with pysam.VariantFile(args.input_file) as infile:
     hdr = infile.header
     hdr.filters.add("HPOL_RUN", None, None, "Homopolymer run")
     hdr.filters.add("LOW_SCORE", None, None, "Low decision tree score")
+    if is_decision_tree:
+        hdr.info.add("TREE_SCORE", 1, "Float", "Filtering score")
     with pysam.VariantFile(args.output_file, mode="w", header=hdr) as outfile:
         for i, rec in enumerate(infile):
             pass_flag = True
@@ -51,4 +66,6 @@ with pysam.VariantFile(args.input_file) as infile:
                 pass_flag = False
             if pass_flag:
                 rec.filter.add("PASS")
+            if is_decision_tree:
+                rec.info["TREE_SCORE"] = predictions_score[i]
             outfile.write(rec)
