0c2086dcc812d27c36c76a06a563e6445d2ccb9f
Ilya Soifer <ilya.soifer@ultimagen.com>
2020-03-23 09:01:03 +0000

    Train models outputs results as h5

--- src/python/pipelines/train_models_pipeline.py
+++ src/python/pipelines/train_models_pipeline.py
@@ -10,7 +10,7 @@ grp = ap.add_mutually_exclusive_group(required=True)
 grp.add_argument("--input_file", help="Name of the input h5 file", type=str)
 grp.add_argument(
     "--input_fofn", help="Input file containing list of the h5 file names to concatenate", type=str)
-ap.add_argument("--output_file", help="Output pkl file",
+ap.add_argument("--output_file_prefix", help="Output .pkl file with models, .h5 file with results",
                 type=str, required=True)
 
 
@@ -140,4 +140,32 @@ results_dict['dt_model_recall_precision_include_gt_excl_hpol_runs'] = recall_pre
 results_dict['dt_model_recall_precision_curve_include_gt_excl_hpol_runs'] = recall_precision_curve_gt
 
 
-pickle.dump(results_dict, open(args.output_file, "wb"))
+pickle.dump(results_dict, open(args.output_file+".pkl", "wb"))
+
+optdict = {}
+prcdict = {}
+for model in ['threshold','dt']: 
+    for gt in ['ignore','include']:
+        for hpol in ['incl',' excl']:
+            name_optimum = f'{model}_model_{gt}_gt_{hpol}_hpol_runs'
+            optdict[name_optimum] = results_dict[name_optimum]
+            prcdict[name_optimum] = results_dict[name_optimum.replace("recall_precision", "recall_precision_curve")]
+
+results_vals = (pd.DataFrame(optdict)).unstack().reset_index()
+results_vals.columns = ['model', 'category','tmp']
+results_vals.loc[pd.isnull(results_vals['tmp']), 'tmp'] = [(np.nan, np.nan)]
+results_vals['recall'] = results_vals['tmp'].apply(lambda x: x[0])
+results_vals['precision'] = results_vals['tmp'].apply(lambda x: x[1])
+results_vals.drop('tmp',axis=1,inplace=True)
+
+results_vals.to_hdf(args.output_file_prefix+'.h5', key="optimal_recall_precision")
+
+results_vals = (pd.DataFrame(prcdict)).unstack().reset_index()
+results_vals.columns = ['model', 'category','tmp']
+results_vals.loc[pd.isnull(results_vals['tmp']),'tmp'] = [[[],[]]]
+results_vals['recall'] = results_vals['tmp'].apply(lambda x: x[0])
+results_vals['precision'] = results_vals['tmp'].apply(lambda x: x[1])
+results_vals.drop('tmp',axis=1,inplace=True)
+
+results_vals.to_hdf(args.output_file_prefix+".h5", key="recall_precision_curve")
+
