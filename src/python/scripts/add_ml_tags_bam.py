6d90a35150958bb3a4b5764f2512a7cbd71410f1
Ilya Soifer <ilya.soifer@ultimagen.com>
2020-05-13 17:52:56 +0000

    Import fix

--- src/python/scripts/add_ml_tags_bam.py
+++ src/python/scripts/add_ml_tags_bam.py
@@ -2,6 +2,7 @@ import pathmagic
 import python.error_model as error_model
 import argparse
 import os
+from os.path import dirname
 
 ap = argparse.ArgumentParser(prog="add_ml_tags_bam.py", description="Add probability tags to uBAM")
 ap.add_argument("--probability_tensor", help='Probability tensor (npy/bin)', type=str)
