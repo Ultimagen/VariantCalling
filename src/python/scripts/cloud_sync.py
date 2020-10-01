a8b25127d860e702c33677a3efe25f48604064ca
Itai Rusinek <itai.rusinek@ultimagen.com>
2020-10-01 10:18:31 +0000

    updated cloud_sync script

--- src/python/scripts/cloud_sync.py
+++ src/python/scripts/cloud_sync.py
@@ -3,49 +3,26 @@
 import argparse
 import os
 import sys
-from subprocess import call
 
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+from auxiliary.cloud_sync import cloud_sync, dir_path
 
-def dir_path(string, check_cloud_path=False):
-    if os.path.isdir(string):
-        return string
-    if check_cloud_path:
-        if not string.startswith('gs://') and not string.startswith('s3://'):
-            raise ValueError(f'Invalid cloud path {string}\nMust be an s3 or gs path')
-        return string
-    else:
-        raise NotADirectoryError(string)
-
-
-def cloud_sync(cloud_path_in, local_dir_in, print_output=False, raise_error_is_file_exists=False):
-    if not os.path.isdir(local_dir_in):
-        raise NotADirectoryError(local_dir_in)
-    dir_path(cloud_path_in, check_cloud_path=True)
-    local_path = os.path.join(
-        local_dir_in, 'cloud_sync', f'{cloud_path_in.split(":")[0]}', cloud_path_in.split("//")[1]
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Download aws s3 or google storage file to a corresponding local path"
     )
-    if os.path.isfile(local_path):
-        if raise_error_is_file_exists:
-            raise FileExistsError(f'target local file {local_path} exists')
-        if print_output:
-            sys.stdout.write(f'Local file {local_path} already exists, skipping...')
-    else:
-        os.makedirs(os.path.dirname(local_path), exist_ok=True)
-        is_gs = cloud_path_in.split(":")[0] == 'gs'
-        cmd = f'{"gsutil" if is_gs else "aws s3"} cp {cloud_path_in} {local_path}'
-        if print_output:
-            sys.stdout.write(f'Downloading to {local_path}')
-        call(cmd.split())
-    return local_path
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Download google storage file to local path')
-    parser.add_argument('cloud_path', type=str, help='full path to aws s3 / google storage file')
     parser.add_argument(
-        '--local_dir', type=dir_path, default='/cache', help='local directory the files will sync to'
+        "cloud_path", type=str, help="full path to aws s3 / google storage file"
+    )
+    parser.add_argument(
+        "--local_dir",
+        type=dir_path,
+        default="/data",
+        help="local directory the files will sync to",
     )
     args = parser.parse_args()
     cloud_path = args.cloud_path
     local_dir = args.local_dir
+    if not os.path.isdir(local_dir):
+        raise ValueError(f"local_dir {local_dir} does not exist")
     cloud_sync(cloud_path, local_dir, print_output=True)
