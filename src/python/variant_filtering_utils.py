8017d64d0cd6ff956abe3263cb1b868a820ac8b4
Ilya Soifer <ilya.soifer@ultimagen.com>
2020-12-10 10:27:11 +0000

    [BIOIN-34] Ugly hack that ignores hom. SOR
    Blacklist and usage of blacklist in filtering

--- src/python/variant_filtering_utils.py
+++ src/python/variant_filtering_utils.py
@@ -653,3 +653,91 @@ def calculate_unfiltered_model(concordance: pd.DataFrame, classify_column: str) 
     recalls_precisions = test_decision_tree_model(
         concordance, result, classify_column)
     return result, recalls_precisions
+
+
+class Blacklist(object):
+    '''Class that stores the blacklist.
+
+    Attributes
+    ----------
+    blacklist: set
+        The blacklist of positions
+    annotation: str
+        Name of the blacklist
+    selection_fcn: Callable
+        The function that selects the relevant calls from the variant dataframe
+
+    Parameters
+    ---------
+    blacklist: set
+    annotation: str
+    selection_fcn: Callable
+    '''
+
+    def __init__(self, blacklist: set, annotation: str, selection_fcn: Callable, description: str):
+        self.blacklist = blacklist
+        self.annotation = annotation
+        self.selection_fcn = selection_fcn
+        self.description = description
+
+    def apply(self, df: pd.DataFrame) -> pd.Series:
+        """Applies the blacklist on the dataframe
+
+        Parameters
+        ----------
+        df : pd.DataFrame
+            Input concordance dataframe
+
+        Returns
+        -------
+        pd.Series
+            Series with string annotation for the blacklist 
+        """
+
+        select = self.selection_fcn(df)
+        idx = set(df[select].index)
+        common_with_blacklist = idx & self.blacklist
+        result = pd.Series("PASS", index=df.index, dtype=str)
+        result.loc[common_with_blacklist] = self.annotation
+        return result
+
+
+def merge_blacklists(blacklists: list) -> pd.Series:
+    """Combines blacklist annotations from multiple blacklists
+
+    Parameters
+    ----------
+    blacklists : list
+        list of annotations from blacklist.apply
+
+    Returns
+    -------
+    pd.Series
+        Combined annotations
+    """
+    if len(blacklists) == 0:
+        return None
+    elif len(blacklists) == 1:
+        return blacklists[0]
+
+    concat = blacklists[0].str.cat(blacklists[1:], sep=";", na_rep="PASS")
+
+    return concat#.str.replace("", "PASS")
+
+
+def blacklist_cg_insertions(df: pd.DataFrame) -> pd.Series:
+    """Removes CG insertions from calls 
+
+    Parameters
+    ----------
+    df: pd.DataFrame
+        calls concordance
+
+    Returns
+    -------
+    pd.Series
+    """
+    ggc_filter = df['alleles'].apply(lambda x : 'GGC' in x or 'CCG' in x )
+    blank = pd.Series("PASS", dtype=str, index=df.index)
+    blank = blank.where(~ggc_filter, "CG_NON_HMER_INDEL")
+    return blank
