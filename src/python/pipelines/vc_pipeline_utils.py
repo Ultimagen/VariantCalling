9ef5d65e1a23466b3d72934082cff760189b8169
Itai Rusinek <itai.rusinek@ultimagen.com>
2020-12-29 10:39:25 +0200

    BIOIN-21 updated documentation for parse_cvg_metrics function

--- src/python/pipelines/vc_pipeline_utils.py
+++ src/python/pipelines/vc_pipeline_utils.py
@@ -1121,7 +1121,22 @@ def parse_md_file(md_file):
 
 
 def parse_cvg_metrics(metric_file):
-    """Parses Picard WGScoverage metrics file"""
+    """Parses Picard WGScoverage metrics file 
+    
+    Parameters
+    ----------
+    metric_file : str
+        Picard metric file 
+    
+    Returns
+    -------
+    res1 : pd.DataFrame
+        Picard metrics table 
+    res2 : str
+        Picard File Class
+    res3 : pd.DataFrame
+        Picard Histogram output 
+    """
     with open(metric_file) as infile:
         out = next(infile)
         while not out.startswith("## METRICS CLASS"):
