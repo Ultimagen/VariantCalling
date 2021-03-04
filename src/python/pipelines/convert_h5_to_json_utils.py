6310574c8eedca422cac215e236929090bba97cc
zganug <zohar.gan@ultimagen.com>
2021-03-04 09:43:53 +0000

    Convert .h5 to .json

---
+++ src/python/pipelines/convert_h5_to_json_utils.py
@@ -0,0 +1,62 @@
+import json
+import os
+import pandas as pd
+import tables
+
+
+def get_h5_keys(h5_filename):
+    with pd.HDFStore(h5_filename) as store:
+        keys = store.keys()
+        return keys
+
+
+def should_skip_h5_key(key, ignored_h5_key_substring):
+    if ignored_h5_key_substring is not None:
+        return ignored_h5_key_substring in key
+
+
+def preprocess_h5_key(key):
+    result = key
+    if result[0] == "/":
+        result = result[1:]
+    return result
+
+
+# Handle MultiIndex .h5
+def preprocess_columns(dataframe):
+    if hasattr(dataframe, 'columns'):
+        MULTI_INDEX_SEPARATOR = "___"
+        if isinstance(dataframe.columns, pd.core.indexes.multi.MultiIndex):
+            dataframe.columns = [MULTI_INDEX_SEPARATOR.join(col).rstrip(
+                MULTI_INDEX_SEPARATOR) for col in dataframe.columns.values]
+
+
+def preprocess_json_for_mongodb(json_string):
+    result = json_string
+    result = result.replace("%", "_Percent")
+    result = result.replace("$", "__")
+    return result
+
+
+def log(str):
+    print(str)
+
+
+def convert_h5_to_json(input_h5_filename, root_element, mongodb_preprocessing, ignored_h5_key_substring):
+    new_json_dict = {root_element: {}}
+    h5_keys = get_h5_keys(input_h5_filename)
+    for h5_key in h5_keys:
+        if should_skip_h5_key(h5_key, ignored_h5_key_substring):
+            log(f'Skipping: {h5_key}')
+            continue
+        log(f'Processing: {h5_key}')
+        df = pd.read_hdf(input_h5_filename, h5_key)
+        preprocess_columns(df)
+        df_to_json = df.to_json(orient="table")
+        json_dict = json.loads(df_to_json)
+        new_json_dict[root_element][preprocess_h5_key(h5_key)] = json_dict
+
+    json_string = json.dumps(new_json_dict, indent=4)
+    if mongodb_preprocessing:
+        json_string = preprocess_json_for_mongodb(json_string)
+    return json_string 
