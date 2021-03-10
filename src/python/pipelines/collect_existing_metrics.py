648b31341a28ff9eaaa1283e15ac758c8db529da
pelegwiner <peleg.winer@ultimagen.com>
2021-03-10 16:41:44 +0200

    BIOIN_114 added changes to collect_existing_metrics.py 2021/03/10

--- src/python/pipelines/collect_existing_metrics.py
+++ src/python/pipelines/collect_existing_metrics.py
@@ -44,14 +44,14 @@ if args.coverage_h5 is not None:
     cvg_h5_histogram.to_hdf(args.output_h5,key="histogram_coverage", mode="a")
 
 if args.short_report_h5 is not None:
-    with pd.HDFStore(args.short_report_h5) as hdf:
-        hdf_keys=hfd.keys()
+    with pd.HDFStore(args.short_report_h5,'r') as hdf:
+        hdf_keys = hdf.keys()
         for report_key in hdf_keys:
             short_report_h5_pd = pd.read_hdf(args.short_report_h5,key= report_key)
             short_report_h5_pd.to_hdf(args.output_h5,key=report_key, mode="a")
 if args.extended_report_h5 is not None:
-    with pd.HDFStore(args.extended_report_h5) as hdf:
-        hdf_keys=hfd.keys()
+    with pd.HDFStore(args.extended_report_h5,'r') as hdf:
+        hdf_keys = hdf.keys()
         for report_key in hdf_keys:
             extended_report_h5_pd = pd.read_hdf(args.extended_report_h5,key= report_key)
-            extended_report_h5.to_hdf(args.output_h5,key="short_report_" + key, mode="a")
+            extended_report_h5_pd.to_hdf(args.output_h5, key="short_report_" + report_key, mode="a")
