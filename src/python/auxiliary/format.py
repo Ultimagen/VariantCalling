5e4a18f7565726eaf13f94781916d1c678b8c222
Itai Rusinek <itai.rusinek@ultimagen.com>
2021-05-09 09:41:20 +0300

    BIOIN-107 added chrY and chrM to CHROM_DTYPE

--- src/python/auxiliary/format.py
+++ src/python/auxiliary/format.py
@@ -11,7 +11,7 @@ from .utils import run_shell_command, get_tagged_output_file
 from pandas.api.types import CategoricalDtype
 
 CHROM_DTYPE = CategoricalDtype(
-    categories=[f"chr{j}" for j in range(1, 23)] + ["chrX"], ordered=True
+    categories=[f"chr{j}" for j in range(1, 23)] + ["chrX", "chrY", "chrM"], ordered=True
 )
 
 
