5acf3233fc6bfe33f3f080722d9a46d379050747
Itai Rusinek <itai.rusinek@ultimagen.com>
2021-05-19 09:19:49 +0300

    BIOIN-107 added an output test (percentiles)

--- src/python_tests/test_coverage_analysis.py
+++ src/python_tests/test_coverage_analysis.py
@@ -1,4 +1,6 @@
 import os
+import numpy as np
+import pandas as pd
 from os.path import join as pjoin, isfile
 from tempfile import TemporaryDirectory
 from pathmagic import PYTHON_TESTS_PATH
@@ -10,10 +12,25 @@ f_in = pjoin(
     "coverage_analysis",
     "170201-BC23.chr9_1000000_2001000.aligned.unsorted.duplicates_marked.bam",
 )
+f_ref = pjoin(
+    PYTHON_TESTS_PATH,
+    "coverage_analysis",
+    "170201-BC23.chr9_1000000_2001000.coverage_percentiles.parquet",
+)
 
 
 def test_coverage_analysis():
-    with TemporaryDirectory(prefix="/data/tmp" if os.path.isdir("/data/tmp") else "") as tmpdir:
+    with TemporaryDirectory(
+        prefix="/data/tmp" if os.path.isdir("/data/tmp") else ""
+    ) as tmpdir:
         run_full_coverage_analysis(
-            bam_file=f_in, out_path=tmpdir, regions=["chr9:1000000-2001000"], windows=[100_000]
+            bam_file=f_in,
+            out_path=tmpdir,
+            regions=["chr9:1000000-2001000"],
+            windows=[100_000],
         )
+        df = pd.read_hdf(
+            pjoin(tmpdir, "170201-BC23.coverage_stats.q0.Q0.l0.h5"), "percentiles"
+        )
+        df_ref = pd.read_parquet(f_ref)
+        assert np.allclose(df.fillna(-1), df_ref.fillna(-1))
