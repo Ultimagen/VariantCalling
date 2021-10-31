a16244efa8cf4e9d5515fc9521248d8048e0343a
Itai Rusinek <itai.rusinek@ultimagen.com>
2021-10-31 11:10:15 +0200

    BIOIN-203 added output sorting step in create_control_signature

--- src/python/pipelines/mrd_utils.py
+++ src/python/pipelines/mrd_utils.py
@@ -7,6 +7,8 @@ import argparse
 import pyfaidx
 import logging
 from tqdm import tqdm
+from tempfile import TemporaryDirectory
+import subprocess
 
 
 logger = logging.getLogger("mrd_utils")
@@ -78,53 +80,57 @@ def create_control_signature(
             f"Output file {control_signature_file_output} already exists and force_overwrite flag set to False"
         )
 
-    with pysam.VariantFile(signature_file) as f_in:
-        header = f_in.header
-        header.info.add("ORIG_POS", 1, "Integer", "Original position of the variant")
-        if append_python_call_to_header is not None:
-            header.add_line(
-                f"##python_cmd:create_control_signature=python {' '.join(sys.argv)}"
-            )
-        with pysam.VariantFile(
-            control_signature_file_output, "w", header=header
-        ) as f_out:
-            for rec in tqdm(
-                f_in, disable=not progress_bar, desc=f"Processing {signature_file}"
-            ):
-                is_snv = (
-                    len(rec.ref) == 1 and len(rec.alts) == 1 and len(rec.alts[0]) == 1
+    with TemporaryDirectory(prefix=control_signature_file_output) as tmpdir:
+        tmp_file = pjoin(tmpdir, control_signature_file_output)
+        with pysam.VariantFile(signature_file) as f_in:
+            header = f_in.header
+            header.info.add("ORIG_POS", 1, "Integer", "Original position of the variant")
+            if append_python_call_to_header is not None:
+                header.add_line(
+                    f"##python_cmd:create_control_signature=python {' '.join(sys.argv)}"
                 )
-                if not is_snv:
-                    continue
-
-                chrom = rec.chrom
-                pos = rec.pos
-                motif = ref[chrom][pos - 2 : pos + 1].seq
-                assert (
-                    rec.ref == motif[1]
-                ), f"Inconsistency in reference found!\n{rec.chrom} {rec.pos} ref={rec.ref} motif={motif}"
-
-                new_pos = pos + min_distance  # starting position
-                delta = int(delta)
-                while True:
-                    new_motif = ref[chrom][new_pos - 2 : new_pos + 1].seq
-                    if (
-                        len(new_motif) < 3 or new_motif == "NNN"
-                    ):  # reached the end of the chromosome or a reference gap
-                        if (
-                            delta < 0
-                        ):  # if we already looked in the other direction, stop and give up this entry (very rare)
-                            break
-                        # start looking in the other direction
-                        delta = -1 * delta
-                        new_pos = pos - min_distance
+            with pysam.VariantFile(
+                tmp_file, "w", header=header
+            ) as f_out:
+                for rec in tqdm(
+                    f_in, disable=not progress_bar, desc=f"Processing {signature_file}"
+                ):
+                    is_snv = (
+                        len(rec.ref) == 1 and len(rec.alts) == 1 and len(rec.alts[0]) == 1
+                    )
+                    if not is_snv:
                         continue
-                    if motif == new_motif:  # motif matches
-                        rec.pos = new_pos
-                        rec.info["ORIG_POS"] = pos
-                        f_out.write(rec)
-                        break
-                    new_pos += delta
+
+                    chrom = rec.chrom
+                    pos = rec.pos
+                    motif = ref[chrom][pos - 2 : pos + 1].seq
+                    assert (
+                        rec.ref == motif[1]
+                    ), f"Inconsistency in reference found!\n{rec.chrom} {rec.pos} ref={rec.ref} motif={motif}"
+
+                    new_pos = pos + min_distance  # starting position
+                    delta = int(delta)
+                    while True:
+                        new_motif = ref[chrom][new_pos - 2 : new_pos + 1].seq
+                        if (
+                            len(new_motif) < 3 or new_motif == "NNN"
+                        ):  # reached the end of the chromosome or a reference gap
+                            if (
+                                delta < 0
+                            ):  # if we already looked in the other direction, stop and give up this entry (very rare)
+                                break
+                            # start looking in the other direction
+                            delta = -1 * delta
+                            new_pos = pos - min_distance
+                            continue
+                        if motif == new_motif:  # motif matches
+                            rec.pos = new_pos
+                            rec.info["ORIG_POS"] = pos
+                            f_out.write(rec)
+                            break
+                        new_pos += delta
+        # sort because output can become unsorted and then cannot be indexed
+        subprocess.call(f"bcftools sort -Oz -o {control_signature_file_output} {tmp_file}".split())
     # index output
     pysam.tabix_index(
         control_signature_file_output, preset="vcf", force=force_overwrite
