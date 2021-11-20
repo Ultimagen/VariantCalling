c302bae6100eea6821953be95a8813eaebe198c3
Ilya Soifer <ilya.soifer@ultimagen.com>
2021-11-20 09:17:31 +0000

    Also removing nonstandard characters from the reference

--- src/python/vcftools.py
+++ src/python/vcftools.py
@@ -40,7 +40,7 @@ def get_vcf_df(variant_calls: str, sample_id: int = 0, chromosome: str = None) -
                'AS_SOR', 'AS_SORP', 'FS', 'VQR_VAL', 'QD',
                'GQ', 'PGT', 'PID', 'PS',
                'AC', 'AN', 'BaseQRankSum', 'ExcessHet', 'MLEAC', 'MLEAF', 'MQRankSum', 'ReadPosRankSum', 'XC', 'ID',
-               'GNOMAD_AF','NLOD','NALOD']
+               'GNOMAD_AF','NLOD','NALOD', 'AS_HmerLength']
 
     concordance_df = pd.DataFrame([[x[y] for y in columns] for x in vfi], columns=[x.lower() for x in columns])
 
