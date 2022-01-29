05dab687036c64d5b1e86b33f8db46c23b9e92a9
Doron Shem-Tov <doron.shemtov@ultimagen.com>
2022-01-29 16:06:12 +0000

    add logs, and get runfile from command lint

--- ugvc/concordance/assess_concordance_with_exclusion_h5.py
+++ ugvc/concordance/assess_concordance_with_exclusion_h5.py
@@ -8,9 +8,9 @@ import pandas as pd
 from pandas import DataFrame
 
 import python.pipelines.vcf_pipeline_utils as vcf_pipeline_utils
-from readwrite.read_from_gcs import read_hdf
+from ugvc.readwrite.read_from_gcs import read_hdf
 from ugvc.concordance.concordance_utils import classify_variants, calculate_results
-
+from ugvc import logger
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -32,6 +32,7 @@ def parse_args():
     # 'common_vars_hapmap_2_1_whole_genome_allele_1_again_filtered_002850_UGAv3_2_0.85-bwa.novel.bed'
     parser.add_argument(
         '--chr', help='chromosome name, in case h5 contains multiple datasets per chromosome', required=True)
+    parser.add_argument('--hcr', help='bed file describing high confidance regions (runs.conservative.bed)', required=True)
     parser.add_argument(
         '--output_prefix', help='prefix to output files containing stats and info about errors', required=True)
     return parser.parse_args()
@@ -64,21 +65,25 @@ def main():
     annotation_intervals = args.annotate_intervals.split(',')
     exclude_lists_beds = args.exclude_lists.split(',')
     out_pref = args.output_prefix
-
+    logger.info(f'read_hdf: {chrom}')
     df: DataFrame = read_hdf(input_file, key=chrom)
 
+    logger.info(f'annotate concordance with annotation_intervals')
     df_annot, annots = vcf_pipeline_utils.annotate_concordance(df, ref_genome_file,
-                                                               runfile='/data/runs.conservative.bed',
+                                                               runfile=args.hcr,
                                                                flow_order='TGCA',
                                                                annotate_intervals=annotation_intervals)
 
+    logger.info('classify variants')
     df_annot = classify_variants(df_annot, ignore_gt=True)
     ddf_annot = ddf.from_pandas(df_annot, npartitions=30)
+    logger.info('extract alleles')
     called_alleles = ddf_annot.apply(extract_alleles, meta=('str'), axis=1).compute(scheduler='multiprocessing')
     df_annot['alleles_base'] = called_alleles
 
     for exclude_list_bed_file in exclude_lists_beds:
         exclude_list_name = splitext(basename(exclude_list_bed_file))[0]
+        logger.info(f'exclude calls from {exclude_list_name}')
         exclude_list_annot_df = df_annot.copy()
         exclude_list_df = pd.read_csv(exclude_list_bed_file, sep='\t', names=['chrom', 'pos', 'pos_1', 'alleles'])
         exclude_list_df['alleles'] = exclude_list_df['alleles'].apply(lambda x: np.array(ast.literal_eval(x)))
