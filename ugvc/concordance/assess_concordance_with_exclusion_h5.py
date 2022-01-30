270072f3dfc5c9e714c3cf54f8bc6491d7b0c9f4
doron <doron.shemtov@ultimagen.com>
2022-01-30 10:44:18 +0200

    move bed tracks creation to be part of assess_concordance_with_exclusions_h5.py

--- ugvc/concordance/assess_concordance_with_exclusion_h5.py
+++ ugvc/concordance/assess_concordance_with_exclusion_h5.py
@@ -8,8 +8,9 @@ import pandas as pd
 from pandas import DataFrame
 
 from python.pipelines.vcf_pipeline_utils import annotate_concordance
-from ugvc.concordance.concordance_utils import classify_variants, calculate_results, read_hdf
 from ugvc import logger
+from ugvc.concordance.concordance_utils import classify_variants, calculate_results, read_hdf
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -19,8 +20,10 @@ def parse_args():
     parser.add_argument(
         '--genome_fasta', help='path to fasta file of reference genome', required=True)
     parser.add_argument(
-        '--exclude_lists', help='bed file to use as allele-specific exclude lists', required=True)
+        '--initial_exclude_list', help='bed file of initial allele-specific exclude-list', required=True)
     # 'common_vars_hapmap_2_1_whole_genome_allele_1_again.bed',
+    parser.add_argument(
+        '--refined_exclude_lists', help='csv list of bed corrected exclude-lists by SEC', required=True)
     # 'common_vars_hapmap_2_1_whole_genome_allele_1_again_filtered_002850_UGAv3_2_0.85-bwa.bed',
     # 'common_vars_hapmap_2_1_whole_genome_allele_1_again_filtered_002850_UGAv3_2_0.85-bwa.novel.bed'
     parser.add_argument(
@@ -36,6 +39,7 @@ def extract_alleles(x):
         return [x['alleles'][0]]
     return np.asarray(pd.Series(x['alleles']).loc[pd.Series(x['gt_ultima']).apply(lambda x: 0 if np.isnan(x) else x)])
 
+
 def get_is_excluded_series(df_annot: DataFrame, exclude_list_df: DataFrame, exclude_list_name: str):
     is_in_bl = []
     for ind, variant in df_annot.iterrows():
@@ -50,6 +54,38 @@ def get_is_excluded_series(df_annot: DataFrame, exclude_list_df: DataFrame, excl
     is_in_bl.index = df_annot.index
     return is_in_bl
 
+
+def write_exclusions_delta_bed_files(df: DataFrame,
+                                     output_prefix: str,
+                                     initial_exclude_list_name: str,
+                                     refined_exclude_list_name: str):
+    df['pos-1'] = df['pos'] - 1
+    df['description'] = df['variant_type'] + '_' + df['hmer_indel_length'].astype(str)
+
+    # was removed from initial exclude_list by refinement
+    df['exclude_list_delta'] = ((df[initial_exclude_list_name]) & ~(df[refined_exclude_list_name]))
+
+    initial_fn = df[df['classify'] == 'fn']
+    initial_fp = df[df['classify'] == 'fp']
+    initial_tp = df[df['classify'] == 'tp']
+    initial_fn_indel = initial_fn[~initial_fn['indel_classify'].isna()]
+    initial_fp_indel = initial_fp[~initial_fp['indel_classify'].isna()]
+    initial_tp_indel = initial_tp[~initial_tp['indel_classify'].isna()]
+    delta_fn_indel = initial_fn_indel[initial_fn_indel[refined_exclude_list_name] & (~initial_fn_indel['call'].isna())]
+    delta_fp_indel = initial_fp_indel[initial_fp_indel['exclude_list_delta']]
+    delta_tp_indel = initial_tp_indel[initial_tp_indel['exclude_list_delta']]
+    unread_fn_indel = initial_fn_indel[initial_fn_indel['call'].isna()]
+
+    write_bed(delta_fn_indel, f'{output_prefix}_delta_fn.bed')
+    write_bed(delta_fp_indel, f'{output_prefix}_delta_fp.bed')
+    write_bed(delta_tp_indel, f'{output_prefix}_delta_tp.bed')
+    write_bed(unread_fn_indel, f'{output_prefix}_unread_fn.bed')
+
+
+def write_bed(df: DataFrame, bed_path: str):
+    df[['chrom', 'pos-1', 'pos', 'description']].to_csv(bed_path, index=False, sep='\t', header=None)
+
+
 def main():
     args = parse_args()
     input_file = args.concordance_h5_input  # '/data/mutect2/data_simulation/002850-UGAv3-2_40x.hcr_wgs.h5'
@@ -58,12 +94,9 @@ def main():
         key = args.chr
     else:
         key = 'all'
-    exclude_lists_beds = args.exclude_lists.split(',')
+    exclude_lists_beds = [args.initial_exclude_list] + args.refined_exclude_lists.split(',')
     out_pref = args.output_prefix
-<<<<<<< HEAD
-=======
-    
->>>>>>> make assess_concordance_with_exclusion_h5 work on real data
+
     logger.info(f'read_hdf: {key}')
     df: DataFrame = read_hdf(input_file, key=key)
 
@@ -81,8 +114,10 @@ def main():
     called_alleles = ddf_annot.apply(extract_alleles, meta='str', axis=1).compute(scheduler='multiprocessing')
     df_annot['alleles_base'] = called_alleles
 
+    exclude_list_names = []
     for exclude_list_bed_file in exclude_lists_beds:
         exclude_list_name = splitext(basename(exclude_list_bed_file))[0]
+        exclude_list_names.append(exclude_list_name)
         logger.info(f'exclude calls from {exclude_list_name}')
         exclude_list_annot_df = df_annot.copy()
         exclude_list_df = pd.read_csv(exclude_list_bed_file, sep='\t', names=['chrom', 'pos', 'pos_1', 'alleles'])
@@ -93,13 +128,19 @@ def main():
 
         exclude_list_annot_df.loc[(exclude_list_annot_df['classify'] == 'tp') & (is_in_bl == True), 'classify'] = 'fn'
         exclude_list_annot_df = exclude_list_annot_df.loc[(exclude_list_annot_df['classify'] == 'fn') |
-                                                                (is_in_bl == False)]
+                                                          (is_in_bl == False)]
         exclude_list_annot_df = classify_variants(exclude_list_annot_df, ignore_gt=True)
         stats_table = calculate_results(exclude_list_annot_df)
         print(stats_table)
 
     exclude_list_annot_df.to_hdf(f'{out_pref}.h5', key)
+    initial_exclusion_list_name = exclude_list_names[0]
+    for refined_exclude_list_name in exclude_list_names[1:]:
+        write_exclusions_delta_bed_files(df=exclude_list_annot_df,
+                                         output_prefix=f'{out_pref}.{refined_exclude_list_name}',
+                                         initial_exclude_list_name=initial_exclusion_list_name,
+                                         refined_exclude_list_name=refined_exclude_list_name)
 
 
-if __name__ == '__main__':
-    main()
+        if __name__ == '__main__':
+            main()
