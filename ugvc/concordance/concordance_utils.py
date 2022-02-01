e687c9ccc0dd86081fbfecefa9ccfa74ccc335b5
doron <doron.shemtov@ultimagen.com>
2022-02-01 19:23:21 +0200

    first attempt refactoring and enriching evaluate_concordance, to avoid duplication with assess_concordancewith_exclusions_h5

--- ugvc/concordance/concordance_utils.py
+++ ugvc/concordance/concordance_utils.py
@@ -1,126 +1,93 @@
+import h5py
 import pandas as pd
 import numpy as np
 from pandas import DataFrame
 
+from python.modules.stats.precision_recall import get_recall, get_precision, get_f1
+from python.pipelines import variant_filtering_utils as variant_filtering_utils
+from ugvc import logger
 
-def read_hdf(fname: str, key='all') -> DataFrame:
+
+def read_hdf(file_name: str, key='all') -> DataFrame:
     if key == 'all':
-        dfs = [pd.read_hdf(fname, key=f"chr{x}") for x in list(range(1, 23)) + ['X']]
+        f = h5py.File(file_name, 'r')
+        keys = list(f.keys())
+        print(f'keys in h5 file : {keys}')
+        dfs = [pd.read_hdf(file_name, key=key) for key in keys]
         return pd.concat(dfs)
     else:
-        return pd.read_hdf(fname, key=key)
-
-
-def classify_variants(vcf_concordance_df: pd.DataFrame, ignore_gt=True) -> pd.DataFrame:
-    vcf_concordance_df['cat'] = None
-    vcf_concordance_df.loc[vcf_concordance_df['indel'] == False, 'cat'] = 'SNP'
-    vcf_concordance_df.loc[
-        (vcf_concordance_df['indel'] == True) & (vcf_concordance_df['hmer_indel_length'] == 0) & (vcf_concordance_df['indel_length'] > 1), 'cat'] = 'NON-HMER'
-    vcf_concordance_df.loc[
-        (vcf_concordance_df['indel'] == True) & (vcf_concordance_df['hmer_indel_length'] == 0) & (vcf_concordance_df['indel_length'] == 1), 'cat'] = 'HMER 0/1'
-
-    vcf_concordance_df.loc[(vcf_concordance_df['indel'] == True) & (vcf_concordance_df['hmer_indel_length'] > 0) & (
-            vcf_concordance_df['hmer_indel_length'] <= 4), 'cat'] = 'HMER 2-4'
-    vcf_concordance_df.loc[(vcf_concordance_df['indel'] == True) & (vcf_concordance_df['hmer_indel_length'] > 4) & (
-            vcf_concordance_df['hmer_indel_length'] <= 8), 'cat'] = 'HMER 5-8'
-    vcf_concordance_df.loc[(vcf_concordance_df['indel'] == True) & (vcf_concordance_df['hmer_indel_length'] > 8), 'cat'] = 'HMER >8'
-
-    if ignore_gt:
-        vcf_concordance_df['score'] = np.where(vcf_concordance_df['classify'] == 'fn', -1, vcf_concordance_df['tree_score'])
-        vcf_concordance_df['label'] = np.where(vcf_concordance_df['classify'] == 'fp', 0, 1)
-    else:
-        vcf_concordance_df['score'] = np.where(vcf_concordance_df['classify_gt'] == 'fn', -1, vcf_concordance_df['tree_score'])
-        vcf_concordance_df['label'] = np.where(vcf_concordance_df['classify_gt'] == 'fp', 0, 1)
-    return vcf_concordance_df
-
-
-def calc_performance(vcf_concordance_df: pd.DataFrame, cat: str):
-    d = vcf_concordance_df[(vcf_concordance_df['cat'] == cat)].copy()
-    d = d[['classify', 'score', 'label']].sort_values(by=['score'])
-    num = len(d)
-    numPos = sum(d['label'])
-    numNeg = num - numPos
-    d['fn'] = np.cumsum(d['label'])
-    d['tp'] = numPos - (d['fn'])
-    d['fp'] = numNeg - np.cumsum(1 - d['label'])
-    d['recall'] = d['tp'] / (d['tp'] + d['fn'])
-    d['precision'] = d['tp'] / (d['tp'] + d['fp'])
-    d.loc[[d.index[-1]], 'precision'] = 1.0
-    d['tresh'] = d.score.eq(d.score.shift(periods=-1))
-    d = d[~d['tresh']]
-    d['dist'] = ((1 - d['recall']) ** 2 + (1 - d['precision']) ** 2) ** (0.5)
-    minDist = min(d['dist'])
-    opt = d[d['dist'] == minDist]
-    return d, opt, numPos, numNeg
-
-
-def calculate_results(df):
-    categories = ['SNP', 'NON-HMER', 'HMER 0/1', 'HMER 2-4', 'HMER 5-8', 'HMER >8']
-    optRes = pd.DataFrame()
-    for i, cat in enumerate(categories):
-        d, opt, p, n = calc_performance(df, cat)
-        row = pd.DataFrame({'pos': p,
-                            'neg': n,
-                            'tp': opt.tp[0],
-                            'fp': opt.fp[0],
-                            'fn': opt.fn[0],
-                            'max recall': np.nan if d.empty else max(d.recall),
-                            'initial_fp': np.nan if d.empty else max(d.fp),
-                            'initial_tp': np.nan if d.empty else max(d.tp),
-                            'recall': np.nan if opt.empty else opt.recall[0],
-                            'precision': np.nan if opt.empty else opt.precision[0],
-                            'thresh': '{:,.4f}'.format(opt.score[0])
-                            }, index=[cat])
-        optRes = pd.concat([optRes, row])
-
-    # create INDEL ALL category
-    indel_cats = ['NON-HMER', 'HMER 0/1', 'HMER 2-4', 'HMER 5-8', 'HMER >8']
-    pos = 0
-    neg = 0
-    tp = 0
-    fp = 0
-    fn = 0
-    initial_fp = 0
-    initial_tp = 0
-    for indel_cat in indel_cats:
-        pos = pos + optRes.loc[indel_cat, 'pos']
-        neg = neg + optRes.loc[indel_cat, 'neg']
-        tp = tp + optRes.loc[indel_cat, 'tp']
-        fp = fp + optRes.loc[indel_cat, 'fp']
-        fn = fn + optRes.loc[indel_cat, 'fn']
-        initial_fp = initial_fp + optRes.loc[indel_cat, 'initial_fp']
-        initial_tp = initial_tp + optRes.loc[indel_cat, 'initial_tp']
-
-    row = pd.DataFrame({'pos': pos,
-                        'neg': neg,
-                        'tp': tp,
-                        'fp': fp,
-                        'fn': fn,
-                        'max recall': initial_tp / pos,
-                        'initial_fp': initial_fp,
-                        'initial_tp': initial_tp,
-                        'recall': tp / pos,
-                        'precision': tp / (fp + tp),
-                        'thresh': np.nan
-                        }, index=['INDEL_ALL'])
-    optRes = pd.concat([optRes, row])
-    return optRes
-
-
-def apply_thresholds(df, thresholds):
-    result_df = df.copy()
-    blacklist_loc = result_df['filter'].str.contains('BLACKLIST')
-
-    result_df['filter'] = 'PASS'
-    result_df.loc[blacklist_loc, 'filter'] = 'BLACKLIST'
-    thresholds = thresholds.loc[list(result_df['cat']), 'thresh']
-    result_df['filter'] = result_df['filter'].where(
-        pd.isnull(result_df['tree_score']) | (result_df['tree_score'] > np.array(thresholds.astype(np.float))),
-        "LOW_SCORE")
-    result_df.drop('tree_score', inplace=True, axis=1)
-    return result_df
-
+        return pd.read_hdf(file_name, key=key)
 
 def filter_df(df):
     df = df[~((df.classify == "fp") & (df['filter'] == 'LOW_SCORE'))]
     return df
+
+
+def calc_accuracy_metrics(df: DataFrame, classify_column: str) -> DataFrame:
+    """
+    Calculate tp, fp, fn, recall, precision, f1, before and after applying filter column
+    :param df: concordance dataframe
+    :param classify_column: column name which contains tp,fp,fn status before applying filter
+    :return: data-frame with variant types and their scores
+    """
+    trivial_classifier_set = validate_and_preprocess_concordance_df(df)
+    # calc recall,precision, f1 per variant category
+    accuracy_df = variant_filtering_utils.test_decision_tree_model(df,
+                                                                   trivial_classifier_set,
+                                                                   classify_column)
+    all_indels = summarize_indel_stats(accuracy_df)
+    accuracy_df = accuracy_df.append(all_indels, ignore_index=True)
+    return accuracy_df
+
+def calc_recall_precision_curve(df: DataFrame, classify_column: str) -> DataFrame:
+    """
+    calc recall/precision curve
+    :param df: concordance dataframe
+    :param classify_column:  column name which contains tp,fp,fn status before applying filter
+    :return:data-frame with variant types and their recall-precision curves
+    """
+    trivial_classifier_set = validate_and_preprocess_concordance_df(df)
+    recall_precision_curve_dict = \
+        variant_filtering_utils.get_decision_tree_precision_recall_curve(df, trivial_classifier_set, classify_column)
+    recall_precision_curve_df = convert_recall_precision_dict_to_df({'analysis': recall_precision_curve_dict})
+    return recall_precision_curve_df
+
+
+def validate_and_preprocess_concordance_df(df):
+    assert 'tree_score' in df.columns, "Input concordance file should be after applying a model"
+    df.loc[pd.isnull(df['hmer_indel_nuc']), "hmer_indel_nuc"] = 'N'
+    if np.any(pd.isnull(df['tree_score'])):
+        logger.warning("Null values in concordance dataframe tree_score. Setting them as zero, but it is suspicious")
+        df.loc[pd.isnull(df['tree_score']), "tree_score"] = 0
+    # set for compatability with test_decision_tree_model
+    df['group'] = 'all'
+    df['test_train_split'] = False
+    trivial_classifier = variant_filtering_utils.SingleTrivialClassifierModel()
+    trivial_classifier_set = variant_filtering_utils.MaskedHierarchicalModel(_name='classifier',
+                                                                             _group_column='group',
+                                                                             _models_dict={'all': trivial_classifier})
+    return trivial_classifier_set
+
+def convert_recall_precision_dict_to_df(recall_precision_dict):
+    results_vals = pd.DataFrame(recall_precision_dict).unstack().reset_index()
+    results_vals.columns = ['model', 'category', 'tmp']
+    results_vals.loc[pd.isnull(results_vals['tmp']), 'tmp'] = [(np.nan, np.nan, np.nan, np.nan)]
+    results_vals['recall'] = results_vals['tmp'].apply(lambda x: x[0])
+    results_vals['precision'] = results_vals['tmp'].apply(lambda x: x[1])
+    results_vals['f1'] = results_vals['tmp'].apply(lambda x: x[2])
+    if len(results_vals['tmp'].values[0]) > 3:
+        results_vals['predictions'] = results_vals['tmp'].apply(lambda x: x[:, 3])
+    results_vals.drop('tmp', axis=1, inplace=True)
+    return results_vals
+
+def summarize_indel_stats(accuracy_df):
+    indel_df = accuracy_df[accuracy_df['group'].str.contains('indel') | accuracy_df['group'].str.contains('INDEL')]
+    all_indels = indel_df.sum()
+    all_indels['group'] = 'INDELS'
+    all_indels['recall'] = get_recall(all_indels['fn'], all_indels['tp'])
+    all_indels['precision'] = get_precision(all_indels['fp'], all_indels['tp'])
+    all_indels['f1'] = get_f1(all_indels['precision'], all_indels['recall'])
+    all_indels['initial_recall'] = get_recall(all_indels['initial_fn'], all_indels['initial_tp'])
+    all_indels['initial_precision'] = get_precision(all_indels['initial_fp'], all_indels['initial_tp'])
+    all_indels['initial_f1'] = get_f1(all_indels['initial_precision'], all_indels['initial_recall'])
+    return all_indels
