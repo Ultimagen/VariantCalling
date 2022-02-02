fix bugs in test_decision_tree_model, sort variant-types in tables, enable masking HPOL_RN filter

src/python/pipelines/variant_filtering_utils.py
@@ -11,6 +11,7 @@ import numpy as np
 import tqdm
 import logging
 from typing import Optional, Tuple, Callable, Union
+from collections import OrderedDict
 from enum import Enum
 import python.utils as utils
 
@@ -792,25 +793,16 @@ def tree_score_to_fpr(df: pd.DataFrame, prediction_score: pd.Series, tree_score_
     return fpr_values
 
 
-def get_testing_selection_functions() -> dict:
-    sfs = []
-    sfs.append(('SNP', lambda x: np.logical_not(x.indel)))
-    sfs.append(("Non-hmer INDEL", lambda x: x.indel &
-                                            (x.hmer_indel_length == 0)))
-    sfs.append(("HMER indel <= 4", (lambda x: x.indel & (x.hmer_indel_length > 0) &
-                                              (x.hmer_indel_length < 5))))
-    sfs.append(("HMER indel (4,8)", lambda x: x.indel & (x.hmer_indel_length >= 5) &
-                                              (x.hmer_indel_length < 8)))
-    sfs.append(("HMER indel [8,10]", lambda x: x.indel & (x.hmer_indel_length >= 8) &
-                                               (x.hmer_indel_length <= 10)))
-
-    sfs.append(("HMER indel 11,12", lambda x: x.indel & (x.hmer_indel_length >= 11) &
-                                              (x.hmer_indel_length <= 12)))
-
-    sfs.append(("HMER indel > 12", lambda x: x.indel &
-                                             (x.hmer_indel_length > 12)))
-
-    return dict(sfs)
+def get_testing_selection_functions() -> OrderedDict:
+    sfs = OrderedDict()
+    sfs['SNP'] = lambda x: np.logical_not(x.indel)
+    sfs["Non-hmer INDEL"] = lambda x: x.indel & (x.hmer_indel_length == 0)
+    sfs["HMER indel <= 4"] = (lambda x: x.indel & (x.hmer_indel_length > 0) & (x.hmer_indel_length < 5))
+    sfs["HMER indel (4,8)"] = lambda x: x.indel & (x.hmer_indel_length >= 5) & (x.hmer_indel_length < 8)
+    sfs["HMER indel [8,10]"] = lambda x: x.indel & (x.hmer_indel_length >= 8) & (x.hmer_indel_length <= 10)
+    sfs["HMER indel 11,12"] = lambda x: x.indel & (x.hmer_indel_length >= 11) & (x.hmer_indel_length <= 12)
+    sfs["HMER indel > 12"] =  lambda x: x.indel & (x.hmer_indel_length > 12)
+    return sfs
 
 
 def add_testing_train_split_column(concordance: pd.DataFrame,
@@ -973,10 +965,10 @@ def test_decision_tree_model(concordance: pd.DataFrame,
     concordance = add_grouping_column(
         concordance, get_testing_selection_functions(), "group_testing")
 
-    # get concordance status after applying variant filtering
+    # get filter status as True='tp' False='fp'
     predictions = model.predict(concordance, classify_column)
 
-    groups = set(concordance['group_testing'])
+    groups = list(get_testing_selection_functions().keys())
     accuracy_df = pd.DataFrame(columns=['group',
                                         'tp',
                                         'fp',
@@ -993,9 +985,12 @@ def test_decision_tree_model(concordance: pd.DataFrame,
     for g in groups:
         select = (concordance["group_testing"] == g) & (~concordance["test_train_split"])
         group_df = concordance[select]
-        group_ground_truth = group_df[classify_column]
-        group_ground_truth[group_ground_truth == 'fn'] = 'tp'
-        group_post_filtering_classification = predictions[select]
+
+        # apply filters on classification
+        is_filtered = predictions[select] != 'tp'
+        post_filtering_classification = group_df[classify_column].copy()
+        post_filtering_classification.iloc[is_filtered & (post_filtering_classification == 'fp')] = 'tn'
+        post_filtering_classification.iloc[is_filtered & (post_filtering_classification == 'tp')] = 'fn'
 
         pre_filtering_tp = int((group_df[classify_column] == 'tp').sum())
         pre_filtering_fp = int((group_df[classify_column] == 'fp').sum())
@@ -1003,26 +998,21 @@ def test_decision_tree_model(concordance: pd.DataFrame,
         pre_filtering_precision = get_precision(pre_filtering_fp, pre_filtering_tp)
         pre_filtering_recall = get_recall(pre_filtering_fn, pre_filtering_tp)
         pre_filtering_f1 = get_f1(pre_filtering_precision, pre_filtering_recall)
-        post_filtering_tp = int((group_post_filtering_classification == 'tp').sum())
-        post_filtering_fp = int((group_post_filtering_classification == 'fp').sum())
-        post_filtering_fn = int((group_post_filtering_classification == 'fn').sum())
+
+        post_filtering_tp = int((post_filtering_classification == 'tp').sum())
+        post_filtering_fp = int((post_filtering_classification == 'fp').sum())
+        post_filtering_fn = int((post_filtering_classification == 'fn').sum())
         post_filtering_precision = get_precision(post_filtering_fp, post_filtering_tp)
         post_filtering_recall = get_recall(post_filtering_fn, post_filtering_tp)
         post_filtering_f1 = get_f1(post_filtering_precision, post_filtering_recall)
 
-        recall = metrics.recall_score(
-            group_ground_truth, group_post_filtering_classification, labels=["tp"], average=None)[0]
-        precision = metrics.precision_score(
-            group_ground_truth, group_post_filtering_classification, labels=["tp"], average=None)[0]
-        f1 = metrics.f1_score(
-            group_ground_truth, group_post_filtering_classification, labels=["tp"], average=None)[0]
         accuracy_df = accuracy_df.append({'group': g,
                                           'tp': post_filtering_tp,
                                           'fp': post_filtering_fp,
                                           'fn': post_filtering_fn,
-                                          'precision': precision,
-                                          'recall': recall,
-                                          'f1': f1,
+                                          'precision': post_filtering_precision,
+                                          'recall': post_filtering_recall,
+                                          'f1': post_filtering_f1,
                                           'initial_tp': pre_filtering_tp,
                                           'initial_fp': pre_filtering_fp,
                                           'initial_fn': pre_filtering_fn,
@@ -1030,7 +1020,6 @@ def test_decision_tree_model(concordance: pd.DataFrame,
                                           'initial_recall': pre_filtering_recall,
                                           'initial_f1': pre_filtering_f1
                                           }, ignore_index=True)
-    accuracy_df = accuracy_df.round(5)
     return accuracy_df
 
 

ugvc/concordance/concordance_utils.py
@@ -23,42 +23,50 @@ def filter_df(df):
     return df
 
 
-def calc_accuracy_metrics(df: DataFrame, classify_column: str) -> DataFrame:
+def calc_accuracy_metrics(df: DataFrame, classify_column: str, filter_hpol_run: bool = False) -> DataFrame:
     """
     Calculate tp, fp, fn, recall, precision, f1, before and after applying filter column
     :param df: concordance dataframe
     :param classify_column: column name which contains tp,fp,fn status before applying filter
+    :param filter_hpol_run: filter variants with HPOL_RUN filter
     :return: data-frame with variant types and their scores
     """
-    trivial_classifier_set = validate_and_preprocess_concordance_df(df)
+    trivial_classifier_set = validate_and_preprocess_concordance_df(df, filter_hpol_run)
     # calc recall,precision, f1 per variant category
     accuracy_df = variant_filtering_utils.test_decision_tree_model(df,
                                                                    trivial_classifier_set,
                                                                    classify_column)
     all_indels = summarize_indel_stats(accuracy_df)
     accuracy_df = accuracy_df.append(all_indels, ignore_index=True)
+    accuracy_df = accuracy_df.round(5)
     return accuracy_df
 
-def calc_recall_precision_curve(df: DataFrame, classify_column: str) -> DataFrame:
+def calc_recall_precision_curve(df: DataFrame, classify_column: str, filter_hpol_run: bool = False) -> DataFrame:
     """
     calc recall/precision curve
     :param df: concordance dataframe
     :param classify_column:  column name which contains tp,fp,fn status before applying filter
+    :param filter_hpol_run: filter variants with HPOL_RUN filter
     :return:data-frame with variant types and their recall-precision curves
     """
-    trivial_classifier_set = validate_and_preprocess_concordance_df(df)
+    trivial_classifier_set = validate_and_preprocess_concordance_df(df, filter_hpol_run)
     recall_precision_curve_dict = \
         variant_filtering_utils.get_decision_tree_precision_recall_curve(df, trivial_classifier_set, classify_column)
     recall_precision_curve_df = convert_recall_precision_dict_to_df({'analysis': recall_precision_curve_dict})
     return recall_precision_curve_df
 
 
-def validate_and_preprocess_concordance_df(df):
+def validate_and_preprocess_concordance_df(df: DataFrame, filter_hpol_run: bool = False):
     assert 'tree_score' in df.columns, "Input concordance file should be after applying a model"
     df.loc[pd.isnull(df['hmer_indel_nuc']), "hmer_indel_nuc"] = 'N'
     if np.any(pd.isnull(df['tree_score'])):
         logger.warning("Null values in concordance dataframe tree_score. Setting them as zero, but it is suspicious")
         df.loc[pd.isnull(df['tree_score']), "tree_score"] = 0
+    print(df['filter'].value_counts())
+    if not filter_hpol_run:
+        df.loc[df[df['filter'] == 'HPOL_RUN'].index, 'filter'] = 'PASS'
+
+    print(df['filter'].value_counts())
     # set for compatability with test_decision_tree_model
     df['group'] = 'all'
     df['test_train_split'] = False