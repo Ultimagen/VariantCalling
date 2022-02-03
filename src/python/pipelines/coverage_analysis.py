1f7fff72927b867f124099c34330ec2717c0b8e4
doron <doron.shemtov@ultimagen.com>
2022-02-03 14:38:00 +0200

    define git-lfs tracking, and add sec test files

--- src/python/pipelines/coverage_analysis.py
+++ src/python/pipelines/coverage_analysis.py
@@ -1,3 +1,5 @@
+from typing import List, Union, Optional
+
 import pandas as pd
 import numpy as np
 import os
@@ -669,8 +671,8 @@ def run_full_coverage_analysis(
     out_path: str,
     ref_fasta: str,
     coverage_intervals_dict: str,
-    regions: str = None,
-    windows: int = None,
+    regions: Optional[Union[str, List[str]]] = None,
+    windows: Optional[Union[int, List[int]]] = None,
     min_bq: int = 0,
     min_mapq: int = 0,
     min_read_length: int = 0,
