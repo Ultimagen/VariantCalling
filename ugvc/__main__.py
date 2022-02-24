81740708c64a5e4a9cc1d04052e951ed43bfdf91
doron <doron.shemtov@ultimagen.com>
2022-02-24 09:32:10 +0200

    add paths to main to enable running ugvc from anywhere without env-var

--- ugvc/__main__.py
+++ ugvc/__main__.py
@@ -1,10 +1,21 @@
+from os.path import dirname
+
 from simppl import cli
 import sys
+
+# add package paths to PYTHONPATH
+paths = [f'{dirname(dirname(__file__))}/src', f'{dirname(dirname(__file__))}']
+for path in paths:
+    if path not in sys.path:
+        sys.path.append(path)
+
+# import pipeline modules implementing run(argv) method
 from ugvc.pipelines.sec import sec_training
 from ugvc.pipelines.sec import correct_systematic_errors
 from ugvc.pipelines.sec import assess_sec_concordance
 from python.pipelines import evaluate_concordance
 
+# create a list of imported pipeline modules
 modules = [
     sec_training,
     correct_systematic_errors,
@@ -13,16 +24,16 @@ modules = [
 ]
 
 logo = \
-"""
-  __    __    ___________    ____  ______ 
-|  |  |  |  /  _____\   \  /   / /      |
-|  |  |  | |  |  __  \   \/   / |  ,----'
-|  |  |  | |  | |_ |  \      /  |  |     
-|  `--'  | |  |__| |   \    /   |  `----.
- \______/   \______|    \__/     \______|
-                                        
-Ultima genomics variant calling toolkit
-"""
+    """
+      __    __    ___________    ____  ______ 
+    |  |  |  |  /  _____\   \  /   / /      |
+    |  |  |  | |  |  __  \   \/   / |  ,----'
+    |  |  |  | |  | |_ |  \      /  |  |     
+    |  `--'  | |  |__| |   \    /   |  `----.
+     \______/   \______|    \__/     \______|
+                                            
+    Ultima genomics variant calling toolkit
+    """
 
 # initialize cli
 ugvc_cli = cli.CommandLineInterface(__file__, logo, modules)
