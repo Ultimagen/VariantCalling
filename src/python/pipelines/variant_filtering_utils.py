Fixed a bug that got somehow in recall_precision curve calculation (#161)
* Fixed a bug that got somehow in recall_precision curve calculation

src/python/pipelines/variant_filtering_utils.py
@@ -335,7 +335,7 @@ def get_all_precision_recalls(results: pd.DataFrame) -> dict:
 def tuple_break(x):
     '''Returns the first element in the tuple
     '''
-    if type(x) == tuple:
+    if isinstance(x, tuple):
         return x[0]
     return 0 if (x is None or np.isnan(x)) else x
 
@@ -343,7 +343,7 @@ def tuple_break(x):
 def tuple_break_second(x):
     '''Returns the second element in the tuple
     '''
-    if type(x) == tuple:
+    if isinstance(x, tuple):
         return x[1]
     return 0 if (x is None or np.isnan(x)) else x
 
@@ -570,8 +570,8 @@ def train_model(concordance: pd.DataFrame, test_train_split: np.ndarray,
         model.fit(train_data, labels)
 
     tree_scores = model.predict_proba(train_data)[:, 1]
-    curve = precision_recall_curve(labels.apply(
-        lambda x: 1 if (x == 'tp') else 0), tree_scores)
+    curve = precision_recall_curve(labels, tree_scores,
+                                   fn_mask=(labels == 'fn'), pos_label='tp')
     precision, recall, f1, preditions = curve
     # get the best f1 threshold
     threshold = preditions[np.argmax(f1)]
@@ -918,7 +918,6 @@ def train_model_wrapper(concordance: pd.DataFrame, classify_column: str, interva
     transformer.fit(concordance)
     groups = set(concordance["group"])
     classifier_models: dict = {}
-    regressor_models: dict = {}
     fpr_values: dict = {}
     thresholds: dict = {}
     for g in groups:
@@ -955,7 +954,7 @@ def test_decision_tree_model(concordance: pd.DataFrame,
     classify_column: str
         Ground truth labels
     add_testing_group_column: bool
-        Should default testing grouping be added (default: True), 
+        Should default testing grouping be added (default: True),
         if False will look for grouping in group_testing
 
     Returns
@@ -1054,12 +1053,19 @@ def get_empty_recall_precision(category: str) -> dict:
             }
 
 
-
 def apply_filter(pre_filtering_classification: pd.Series, is_filtered: pd.Series) -> pd.Series:
     """
-    @param pre_filtering_classification: classification to 'tp', 'fp', 'fn' before applying filter
-    @param is_filtered:  boolean series denoting which rows where filtered
-    @return: classification to 'tp', 'fp', 'fn', 'tn' after applying filter
+    Parameters
+    ----------
+    pre_filtering_classification : pd.Series
+        classification to 'tp', 'fp', 'fn' before applying filter
+    is_filtered : pd.Series
+        boolean series denoting which rows where filtered
+
+    Returns
+    -------
+    pd.Series
+        classification to 'tp', 'fp', 'fn', 'tn' after applying filter
     """
     post_filtering_classification = pre_filtering_classification.copy()
     post_filtering_classification.iloc[is_filtered & (
@@ -1085,7 +1091,7 @@ def get_decision_tree_precision_recall_curve(concordance: pd.DataFrame,
     classify_column: str
         Ground truth labels
     add_testing_group_column: bool
-        Should default testing grouping be added (default: True), 
+        Should default testing grouping be added (default: True),
         if False will look for grouping in group_testing
 
     Returns
@@ -1104,7 +1110,6 @@ def get_decision_tree_precision_recall_curve(concordance: pd.DataFrame,
         groups = list(set(concordance['group_testing']))
 
     predictions = model.predict(concordance, classify_column, get_numbers=True)
-    recalls_precisions = {}
     accuracy_df = pd.DataFrame(columns=['group',
                                         'predictions',
                                         'precision',
@@ -1131,11 +1136,10 @@ def get_decision_tree_precision_recall_curve(concordance: pd.DataFrame,
 
         group_ground_truth = classification.copy()
         group_ground_truth[classification == 'fn'] = 'tp'
-
         curve = precision_recall_curve(np.array(group_ground_truth),
                                        np.array(group_predictions),
-                                       pos_label="tp",
-                                       fn_score=-1)
+                                       fn_mask=(classification == 'fn'),
+                                       pos_label="tp")
 
         precision, recall, f1, prediction_values = curve
 
@@ -1191,11 +1195,11 @@ def calculate_unfiltered_model(concordance: pd.DataFrame, classify_column: str) 
 class VariantSelectionFunctions(Enum):
     """Collecton of variant selection functions - all get DF as input and return boolean np.array"""
 
-    def ALL(
-        df: pd.DataFrame) -> np.ndarray: return np.ones(df.shape[0], dtype=np.bool)
+    def ALL(df: pd.DataFrame) -> np.ndarray:
+        return np.ones(df.shape[0], dtype=np.bool)
 
-    def HMER_INDEL(
-        df: pd.DataFrame) -> np.ndarray: return np.array(df.hmer_indel_length > 0)
+    def HMER_INDEL(df: pd.DataFrame) -> np.ndarray:
+        return np.array(df.hmer_indel_length > 0)
 
-    def ALL_except_HMER_INDEL_greater_than_or_equal_5(
-        df: pd.DataFrame) -> np.ndarray: return np.array(~ ((df.hmer_indel_length >= 5)))
+    def ALL_except_HMER_INDEL_greater_than_or_equal_5(df: pd.DataFrame) -> np.ndarray:
+        return np.array(~ ((df.hmer_indel_length >= 5)))

test/unit/utils/test_stats_utils.py
@@ -37,42 +37,62 @@ class TestStatsUtils(unittest.TestCase):
                                       get_corrected_multinomial_frequencies([10, 10, 0]))
 
     def test_multinomial_likelihood(self):
-        self.assertAlmostEqual(0.0652, multinomial_likelihood([4, 4, 4], [4, 4, 4]), places=3)
-        self.assertAlmostEqual(0.0652, multinomial_likelihood([4, 4, 4], [40, 40, 40]), places=3)
-        self.assertAlmostEqual(0.0068, multinomial_likelihood([40, 40, 40], [40, 40, 40]), places=3)
+        self.assertAlmostEqual(0.0652, multinomial_likelihood(
+            [4, 4, 4], [4, 4, 4]), places=3)
+        self.assertAlmostEqual(0.0652, multinomial_likelihood(
+            [4, 4, 4], [40, 40, 40]), places=3)
+        self.assertAlmostEqual(0.0068, multinomial_likelihood(
+            [40, 40, 40], [40, 40, 40]), places=3)
 
-        self.assertAlmostEqual(3.3 * 10 ** -13, multinomial_likelihood([4, 4, 40], [4, 4, 4]), places=10)
-        self.assertAlmostEqual(3.3 * 10 ** -13, multinomial_likelihood([4, 4, 40], [40, 40, 40]), places=10)
+        self.assertAlmostEqual(
+            3.3 * 10 ** -13, multinomial_likelihood([4, 4, 40], [4, 4, 4]), places=10)
+        self.assertAlmostEqual(
+            3.3 * 10 ** -13, multinomial_likelihood([4, 4, 40], [40, 40, 40]), places=10)
 
         # Get expected improvement as actual becomes fitter to expected
-        self.assertAlmostEqual(2.1 * 10 ** -10, multinomial_likelihood([10, 10, 10], [1, 10, 40]), places=10)
-        self.assertAlmostEqual(2.7 * 10 ** -53, multinomial_likelihood([40, 10, 1], [1, 10, 40]), places=40)
-        self.assertAlmostEqual(0.039, multinomial_likelihood([1, 10, 40], [1, 10, 40]), places=3)
+        self.assertAlmostEqual(
+            2.1 * 10 ** -10, multinomial_likelihood([10, 10, 10], [1, 10, 40]), places=10)
+        self.assertAlmostEqual(
+            2.7 * 10 ** -53, multinomial_likelihood([40, 10, 1], [1, 10, 40]), places=40)
+        self.assertAlmostEqual(0.039, multinomial_likelihood(
+            [1, 10, 40], [1, 10, 40]), places=3)
 
         # 0 expected does not cause 0 likelihood, because of add-one correction
-        self.assertAlmostEqual(0.0043, multinomial_likelihood([4, 4, 4], [4, 4, 0]), places=3)
+        self.assertAlmostEqual(0.0043, multinomial_likelihood(
+            [4, 4, 4], [4, 4, 0]), places=3)
 
         # need to watch out from this edge-case
-        self.assertAlmostEqual(3.3 * 10 ** -13, multinomial_likelihood([4, 4, 40], [0, 0, 0]), places=3)
+        self.assertAlmostEqual(
+            3.3 * 10 ** -13, multinomial_likelihood([4, 4, 40], [0, 0, 0]), places=3)
 
     def test_multinomial_likelihood_ratio(self):
-        self.assertAlmostEqual(1, multinomial_likelihood_ratio([4, 4, 4], [4, 4, 4])[1], places=3)
-        self.assertAlmostEqual(1, multinomial_likelihood_ratio([4, 4, 4], [40, 40, 40])[1], places=3)
-        self.assertAlmostEqual(1, multinomial_likelihood_ratio([40, 40, 40], [40, 40, 40])[1], places=3)
+        self.assertAlmostEqual(1, multinomial_likelihood_ratio(
+            [4, 4, 4], [4, 4, 4])[1], places=3)
+        self.assertAlmostEqual(1, multinomial_likelihood_ratio(
+            [4, 4, 4], [40, 40, 40])[1], places=3)
+        self.assertAlmostEqual(1, multinomial_likelihood_ratio(
+            [40, 40, 40], [40, 40, 40])[1], places=3)
 
-        self.assertAlmostEqual(3.3 * 10 ** -13, multinomial_likelihood_ratio([4, 4, 40], [4, 4, 4])[1], places=10)
-        self.assertAlmostEqual(3.3 * 10 ** -13, multinomial_likelihood_ratio([4, 4, 40], [40, 40, 40])[1], places=10)
+        self.assertAlmostEqual(
+            3.3 * 10 ** -13, multinomial_likelihood_ratio([4, 4, 40], [4, 4, 4])[1], places=10)
+        self.assertAlmostEqual(
+            3.3 * 10 ** -13, multinomial_likelihood_ratio([4, 4, 40], [40, 40, 40])[1], places=10)
 
         # Get expected improvement as actual becomes fitter to expected
-        self.assertAlmostEqual(7.8 * 10 ** -9, multinomial_likelihood_ratio([10, 10, 10], [1, 10, 40])[1], places=10)
-        self.assertAlmostEqual(6.9 * 10 ** -52, multinomial_likelihood_ratio([40, 10, 1], [1, 10, 40])[1], places=40)
-        self.assertAlmostEqual(1, multinomial_likelihood_ratio([1, 10, 40], [1, 10, 40])[1], places=3)
+        self.assertAlmostEqual(
+            7.8 * 10 ** -9, multinomial_likelihood_ratio([10, 10, 10], [1, 10, 40])[1], places=10)
+        self.assertAlmostEqual(
+            6.9 * 10 ** -52, multinomial_likelihood_ratio([40, 10, 1], [1, 10, 40])[1], places=40)
+        self.assertAlmostEqual(1, multinomial_likelihood_ratio(
+            [1, 10, 40], [1, 10, 40])[1], places=3)
 
         # 0 expected does not cause 0 likelihood, because of add-one correction
-        self.assertAlmostEqual(0.0661, multinomial_likelihood_ratio([4, 4, 4], [4, 4, 0])[1], places=3)
+        self.assertAlmostEqual(0.0661, multinomial_likelihood_ratio(
+            [4, 4, 4], [4, 4, 0])[1], places=3)
 
         # need to watch out from this edge-case
-        self.assertAlmostEqual(9.1 * 10 ** -12, multinomial_likelihood_ratio([4, 4, 40], [0, 0, 0])[1], places=10)
+        self.assertAlmostEqual(
+            9.1 * 10 ** -12, multinomial_likelihood_ratio([4, 4, 40], [0, 0, 0])[1], places=10)
 
     def test_get_precision(self):
         self.assertAlmostEqual(get_precision(fp=100, tp=900), 0.9)
@@ -80,9 +100,31 @@ class TestStatsUtils(unittest.TestCase):
 
     def test_get_recall(self):
         self.assertAlmostEqual(0.9, get_recall(fn=100, tp=900))
-        self.assertAlmostEqual(0.99889,get_recall(fn=1, tp=900),  places=5)
+        self.assertAlmostEqual(0.99889, get_recall(fn=1, tp=900), places=5)
 
     def test_get_f1(self):
-        self.assertAlmostEqual(0.942857, get_f1(recall=0.99, precision=0.9),  places=5)
-        self.assertAlmostEqual(0.642857, get_f1(recall=0.5, precision=0.9), places=5)
+        self.assertAlmostEqual(0.942857, get_f1(
+            recall=0.99, precision=0.9), places=5)
+        self.assertAlmostEqual(0.642857, get_f1(
+            recall=0.5, precision=0.9), places=5)
 
+    def test_recall_precision_curve(self):
+        labels = np.array([0, 1] * 50)
+        scores = np.array([0.1, .8] * 50)
+        precision, recalls, f1, predictions = precision_recall_curve(
+            labels, scores, fn_mask=np.zeros_like(scores, dtype=bool),
+            pos_label=1, min_class_counts_to_output=1)
+        self.assertEqual(len(precision), 1)
+        self.assertEqual(len(f1), 1)
+        self.assertAlmostEqual(max(f1), 1)
+
+        labels = np.array([0, 1] * 50 + [1] * 10)
+        scores = np.array([0.1, .8] * 50 + [-1] * 10)
+        precision, recalls, f1, predictions = precision_recall_curve(
+            labels, scores,
+            np.concatenate((np.zeros(100, dtype=bool),
+                            np.ones(10, dtype=bool))),
+            pos_label=1, min_class_counts_to_output=1)
+        self.assertEqual(len(precision), 1)
+        self.assertEqual(len(f1), 1)
+        self.assertAlmostEqual(max(f1), 0.909090909)