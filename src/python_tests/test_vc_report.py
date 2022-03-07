19513464da0159ae12b16b57113538400b96c2dc
Tammy_Biniashvili <tammy.biniashvili@ultimagen.com>
2022-03-07 11:40:48 +0200

    add_sec_test_data_to_short_report

--- src/python_tests/test_vc_report.py
+++ src/python_tests/test_vc_report.py
@@ -4,7 +4,6 @@ from os.path import join as pjoin
 import shutil
 import subprocess
 
-
 def test_vc_report(tmpdir):
     path = pathmagic.vc_path
     datadir = pjoin(pathmagic.PYTHON_TESTS_PATH, "reports")
@@ -15,22 +14,54 @@ def test_vc_report(tmpdir):
     shutil.copy(pjoin(datadir, "test.trained.h5"), tmpdir)
     with open(pjoin(tmpdir, "var_report.config"), 'w') as config_file:
         strg = '''
-        [VarReport]
-        run_id = 140479-BC21
-        pipeline_version = 2.4.1
-        h5_concordance_file = ./test.untrained.h5
-        h5_model_file = ./test.trained.h5
+            [VarReport]
+            run_id = 140479-BC21
+            pipeline_version = 2.4.1
+            h5_concordance_file = ./test.untrained.h5
+            h5_model_file = ./test.trained.h5
+            const_model = untrained_ignore_gt_excl_hpol_runs
+            trained_model = threshold_model_recall_precision_ignore_gt_excl_hpol_runs
+            h5_output = test.var_report.h5
+            model_name_with_gt = untrained_ignore_gt_excl_hpol_runs
+            model_name_without_gt = threshold_model_recall_precision_ignore_gt_excl_hpol_runs
+            model_pkl_with_gt = dummy1.pkl
+            model_pkl_without_gt = dummy2.pkl
 
-        const_model = untrained_ignore_gt_excl_hpol_runs
+    	'''
+        config_file.write(strg)
 
-        trained_model = threshold_model_recall_precision_ignore_gt_excl_hpol_runs
-        h5_output = test.var_report.h5
-        model_name_with_gt = untrained_ignore_gt_excl_hpol_runs
-        model_name_without_gt = threshold_model_recall_precision_ignore_gt_excl_hpol_runs
-        model_pkl_with_gt = dummy1.pkl
-        model_pkl_without_gt = dummy2.pkl
-        
-	'''
+    cmd = ['jupyter', 'nbconvert', '--to', 'notebook',
+           '--execute', 'createVarReport.ipynb']
+    assert (subprocess.check_call(cmd, cwd=tmpdir) == 0)
+
+    cmd = ['jupyter', 'nbconvert', '--to', 'html', 'createVarReport.nbconvert.ipynb', '--template', 'full',
+           '--no-input', '--output', 'varReport.html']
+    assert (subprocess.check_call(cmd, cwd=tmpdir) == 0)
+
+def test_vc_report_with_sec(tmpdir):
+    path = pathmagic.vc_path
+    datadir = pjoin(pathmagic.PYTHON_TESTS_PATH, "reports")
+    report_path = pjoin(path, "python", "reports")
+    shutil.copy(pjoin(report_path, "createVarReport.ipynb"), tmpdir)
+    shutil.copy(pjoin(report_path, "nexusplt.py"), tmpdir)
+    shutil.copy(pjoin(datadir, "test.trained.h5"), tmpdir)
+    shutil.copy(pjoin(datadir, "test.ug_hcr.sec.comp.h5"), tmpdir)
+    with open(pjoin(tmpdir, "var_report.config"), 'w') as config_file:
+        strg = '''
+            [VarReport]
+            run_id = 140479-BC21
+            pipeline_version = 2.4.1
+            h5_concordance_file = ./test.ug_hcr.sec.comp.h5
+            h5_model_file = ./test.trained.h5
+            const_model = untrained_ignore_gt_excl_hpol_runs
+            trained_model = threshold_model_recall_precision_ignore_gt_excl_hpol_runs
+            h5_output = test.var_report.h5
+            model_name_with_gt = untrained_ignore_gt_excl_hpol_runs
+            model_name_without_gt = threshold_model_recall_precision_ignore_gt_excl_hpol_runs
+            model_pkl_with_gt = dummy1.pkl
+            model_pkl_without_gt = dummy2.pkl
+
+    	'''
         config_file.write(strg)
 
     cmd = ['jupyter', 'nbconvert', '--to', 'notebook',
