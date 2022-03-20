04701a0f82ede79c26d5cebc64c760ef5a846805
Tammy_Biniashvili <tammy.biniashvili@ultimagen.com>
2022-03-20 10:51:55 +0200

    add_integration_test_for_evaluate_concordance

---
+++ test/system/concordance/test_evaluate_concordance.py
@@ -0,0 +1,27 @@
+import os
+import unittest
+from os.path import dirname
+import filecmp
+
+from python.pipelines import evaluate_concordance
+from test import get_resource_dir, make_test_outputs_dir
+
+
+class TestEvaluateConcordance(unittest.TestCase):
+    inputs_dir = get_resource_dir(__name__)
+    test_outputs_dir = make_test_outputs_dir(__name__)
+
+    def test_evaluate_concordance(self):
+        input_file = f'{self.inputs_dir}/test.untrained.h5'
+        output_prefix = f'{self.test_outputs_dir}/test'
+        os.makedirs(dirname(output_prefix), exist_ok=True)
+
+        evaluate_concordance.run(
+            ['--input_file', input_file,
+             '--output_prefix', f'{self.test_outputs_dir}/test'
+             ])
+
+        expected_stats_file = f'{self.inputs_dir}/expected.out.stats.csv'
+        out_stats_file = f'{self.test_outputs_dir}/test.stats.csv'
+        self.assertTrue(filecmp.cmp(expected_stats_file, out_stats_file, shallow=False),
+                        'stats files are not identical')
