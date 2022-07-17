2b347afc5cbac4bf71c1f40372153d1f445e548e
Tammy Biniashvili <tammy.biniashvili@ultimagen.com>
2022-07-17 22:49:29 +0300

    adding argparse to R scripts

--- ugvc/cnv/get_reads_count_from_bam.R
+++ ugvc/cnv/get_reads_count_from_bam.R
@@ -12,10 +12,10 @@ parser$add_argument("-refseq", "--refSeqNames_string",
                     default="chr1,chr2,chr3,chr4,chr5,chr6,chr7,chr8,chr9,chr10,chr11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19,chr20,chr21,chr22,chrX,chrY")
 parser$add_argument("-wl", "--window_length",
                    help="window length (#bp) for which reads count is calculated for",
-                   type="int", default=1000)
+                   type="integer", default=1000)
 parser$add_argument("-p", "--parallel",
                     help="number of parallel process",
-                    type="int", default=30)
+                    type="integer", default=30)
 parser$add_argument("-o", "--base_file_name",
                     help="out base file name")
 
