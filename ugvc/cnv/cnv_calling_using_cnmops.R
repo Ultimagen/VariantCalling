803687ca807b77988ab7afb46e44d125f8438998
Tammy Biniashvili <tammy.biniashvili@ultimagen.com>
2022-07-20 12:06:57 +0300

    use rds/hdf5 instead of rdata/csv and update conda env

--- ugvc/cnv/cnv_calling_using_cnmops.R
+++ ugvc/cnv/cnv_calling_using_cnmops.R
@@ -19,15 +19,15 @@
 suppressPackageStartupMessages(library(cn.mops))
 suppressPackageStartupMessages(library(magrittr))
 suppressPackageStartupMessages(library(argparse))
+suppressPackageStartupMessages(library(rhdf5))
 
 parser <- ArgumentParser()
 parser$add_argument("-cohort_rc", "--cohort_reads_count_file",
                     help="input cohort reads count file in Rdata format")
 parser$add_argument("-minWidth", "--min_width_val",
-                    help="min width parameter value",
                     type="integer",default=2)
 parser$add_argument("-p", "--parallel",
-                    help="number of parallel process",
+                    help="number of parallel processes",
                     type="integer", default=30)
 args <- parser$parse_args()
 
@@ -35,24 +35,25 @@ cohort_reads_count_file <- args$cohort_reads_count_file
 min_width_val <- args$min_width_val
 cores <- args$parallel
 
-load(cohort_reads_count_file) #variable name : merged_cohort_reads_count
+#load(cohort_reads_count_file)
+readRDS(file = cohort_reads_count_file) #variable name : merged_cohort_reads_count
 
 resCNMOPS <- cn.mops(merged_cohort_reads_count,parallel=cores,minWidth = as.integer(min_width_val) )
 resCNMOPS_Int <-calcIntegerCopyNumbers(resCNMOPS)
 
-save(resCNMOPS_Int,file="cohort.cnmops.RData")
+saveRDS(resCNMOPS_Int,file="cohort.cnmops.rds")
 
 df_cnvs<-as.data.frame(cnvs(resCNMOPS_Int))
 write.csv(df_cnvs,"cohort.cnmops.cnvs.csv", row.names = FALSE,quote=FALSE)
 
-df_cnvr<-as.data.frame(cnvr(resCNMOPS_Int))
-write.csv(df_cnvr,"cohort.cnmops.cnvr.csv", row.names = FALSE,quote=FALSE)
-
-individualCall_df<-as.data.frame(individualCall(resCNMOPS_Int))
-write.csv(individualCall_df,"cohort.cnmops.individualCall.csv",row.names = FALSE, quote=FALSE)
-
-inicall_df<-as.data.frame(iniCall(resCNMOPS_Int))
-write.csv(inicall_df,"cohort.cnmops.inicall.csv",row.names = FALSE, quote=FALSE)
-
-integerCopyNumber_df<-as.data.frame(integerCopyNumber(resCNMOPS_Int))
-write.csv(integerCopyNumber_df,"cohort.cnmops.integerCopyNumber.csv",row.names = FALSE, quote=FALSE)
+# df_cnvr<-as.data.frame(cnvr(resCNMOPS_Int))
+# write.csv(df_cnvr,"cohort.cnmops.cnvr.csv", row.names = FALSE,quote=FALSE)
+#
+# individualCall_df<-as.data.frame(individualCall(resCNMOPS_Int))
+# write.csv(individualCall_df,"cohort.cnmops.individualCall.csv",row.names = FALSE, quote=FALSE)
+#
+# inicall_df<-as.data.frame(iniCall(resCNMOPS_Int))
+# write.csv(inicall_df,"cohort.cnmops.inicall.csv",row.names = FALSE, quote=FALSE)
+#
+# integerCopyNumber_df<-as.data.frame(integerCopyNumber(resCNMOPS_Int))
+# write.csv(integerCopyNumber_df,"cohort.cnmops.integerCopyNumber.csv",row.names = FALSE, quote=FALSE)
