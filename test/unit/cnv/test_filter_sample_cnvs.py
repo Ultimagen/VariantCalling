8502d156b4dbab4c97d4b5395767871e1c1040eb
Tammy Biniashvili <tammy.biniashvili@ultimagen.com>
2022-07-24 11:20:36 +0300

    subsample test data

--- test/unit/cnv/test_filter_sample_cnvs.py
+++ test/unit/cnv/test_filter_sample_cnvs.py
@@ -1,4 +1,5 @@
 import filecmp
+import os
 import unittest
 from os.path import join as pjoin
 from test import get_resource_dir
@@ -6,15 +7,15 @@ from ugvc.cnv.filter_sample_cnvs import annotate_bed
 
 inputs_dir = get_resource_dir(__file__)
 
-
 class TestFilterSampleCnvs(unittest.TestCase):
-    def test_annotate_bed(self):
+    def test_annotate_bed(self, tmpdir):
         input_bed_file = pjoin(inputs_dir, "unfiltered_cnvs.bed")
         expected_out_filtered_bed_file = pjoin(inputs_dir, "filtered_cnvs.bed")
         coverage_lcr_file = pjoin(inputs_dir, "UG-CNV-LCR.bed")
         blocklist = pjoin(inputs_dir, "blocklist.bed")
         intersection_cutoff = 0.5
         min_cnv_length = 10000
+        os.chdir(tmpdir)
         [out_annotate_file, out_filtered_file] = annotate_bed(input_bed_file, intersection_cutoff,
                                                               coverage_lcr_file, intersection_cutoff,
                                                               blocklist, min_cnv_length)
