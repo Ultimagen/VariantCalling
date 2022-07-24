c319de5b818dfe42ce8ecaddfeff6dde51e274ce
Tammy Biniashvili <tammy.biniashvili@ultimagen.com>
2022-07-24 11:41:24 +0000

    fix test

--- test/unit/cnv/test_filter_sample_cnvs.py
+++ test/unit/cnv/test_filter_sample_cnvs.py
@@ -2,31 +2,34 @@ import filecmp
 import os
 import unittest
 from os.path import join as pjoin
-from test import get_resource_dir
+from test import get_resource_dir, test_dir
 from ugvc.cnv.filter_sample_cnvs import annotate_bed
 
+#class TestFilterSampleCnvs(unittest.TestCase):
 
-class TestFilterSampleCnvs(unittest.TestCase):
+class TestFilterSampleCnvs:
     inputs_dir = get_resource_dir(__file__)
 
     def test_annotate_bed(self, tmpdir):
-        input_bed_file = pjoin(inputs_dir, "unfiltered_cnvs.bed")
-        expected_out_filtered_bed_file = pjoin(inputs_dir, "filtered_cnvs.bed")
-        expected_out_annotate_bed_file = pjoin(inputs_dir, "annotate_cnv.bed")
-        coverage_lcr_file = pjoin(inputs_dir, "UG-CNV-LCR.bed")
-        blocklist = pjoin(inputs_dir, "blocklist.bed")
+        input_bed_file = pjoin(self.inputs_dir, "unfiltered_cnvs.bed")
+        expected_out_filtered_bed_file = pjoin(self.inputs_dir, "filtered_cnvs.bed")
+        expected_out_annotate_bed_file = pjoin(self.inputs_dir, "annotate_cnv.bed")
+        coverage_lcr_file = pjoin(self.inputs_dir, "UG-CNV-LCR.bed")
+        blocklist = pjoin(self.inputs_dir, "blocklist.bed")
         intersection_cutoff = 0.5
         min_cnv_length = 10000
-        prefix = tmpdir
+        prefix = f"{tmpdir}/"
         #os.chdir(tmpdir)
         print(tmpdir)
         [out_annotate_file, out_filtered_file] = annotate_bed(input_bed_file, intersection_cutoff,
                                                               coverage_lcr_file, intersection_cutoff,
                                                               blocklist, prefix, min_cnv_length)
 
-        self.assertTrue(filecmp.cmp(out_filtered_file, expected_out_filtered_bed_file),
-                        'filtered cnvs list is not identical to expected list')
-        self.assertTrue(filecmp.cmp(out_annotate_file, expected_out_annotate_bed_file),
-                        'annotate cnvs list is not identical to expected list')
+        assert filecmp.cmp(out_filtered_file, expected_out_filtered_bed_file)
+        assert filecmp.cmp(out_annotate_file, expected_out_annotate_bed_file)
+#        self.assertTrue(filecmp.cmp(out_filtered_file, expected_out_filtered_bed_file),
+#                        'filtered cnvs list is not identical to expected list')
+#        self.assertTrue(filecmp.cmp(out_annotate_file, expected_out_annotate_bed_file),
+#                        'annotate cnvs list is not identical to expected list')
 
         
