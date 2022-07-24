7857f59822668e291cce129e2c81c17bc84716a6
Tammy Biniashvili <tammy.biniashvili@ultimagen.com>
2022-07-24 11:20:36 +0300

    subsample test data

--- ugvc/cnv/filter_sample_cnvs.py
+++ ugvc/cnv/filter_sample_cnvs.py
@@ -8,8 +8,8 @@ bedtools = "/home/ubuntu/miniconda3/envs/genomics.py3/bin/bedtools"
 bedmap = "/home/ubuntu/miniconda3/envs/genomics.py3/bin/bedmap"
 
 
-def filter_by_lcr(bed_file, lcr_cutoff, lcr_file):
-
+def filter_by_lcr(bed_file, lcr_cutoff, lcr_file, prefix):
+    out_lcr_file = prefix + '/' + bed_file.rstrip(".bed") + ".lcr.bed"
     cmd = (
         bedtools
         + " subtract -N -f "
@@ -19,10 +19,10 @@ def filter_by_lcr(bed_file, lcr_cutoff, lcr_file):
         + " -b "
         + lcr_file
         + " > "
-        + bed_file.rstrip(".bed")
-        + ".lcr.bed"
+        + out_lcr_file
     )
     os.system(cmd)
+    out_lcr_filtered_out_file = prefix + '/' + bed_file.rstrip(".bed") + ".lcr.filtered_out.bed"
     cmd = (
         bedtools
         + " subtract -N -f "
@@ -30,28 +30,25 @@ def filter_by_lcr(bed_file, lcr_cutoff, lcr_file):
         + " -a "
         + bed_file
         + " -b "
-        + bed_file.rstrip(".bed")
-        + ".lcr.bed"
+        + out_lcr_file
         + " > "
-        + bed_file.rstrip(".bed")
-        + ".lcr_filtered_out.bed"
+        + out_lcr_filtered_out_file
     )
     os.system(cmd)
+    out_lcr_annotate_file = prefix + '/' + bed_file.rstrip(".bed") + ".lcr.annotate.bed"
     cmd = (
         "cat "
-        + bed_file.rstrip(".bed")
-        + ".lcr_filtered_out.bed"
+        + out_lcr_filtered_out_file
         + ' | awk \'{print $1"\t"$2"\t"$3"\tUG-CNV-LCR"}\' > '
-        + bed_file.rstrip(".bed")
-        + ".lcr.annotate.bed"
+        + out_lcr_annotate_file
     )
     os.system(cmd)
 
-    out_file = bed_file.rstrip(".bed") + ".lcr.annotate.bed"
-    return out_file
+    return out_lcr_annotate_file
 
 
-def filter_by_blocklist(bed_file, diff_cutoff, diff_bed_file):
+def filter_by_blocklist(bed_file, diff_cutoff, diff_bed_file, prefix):
+    out_blocklist_file = prefix + '/' + bed_file.rstrip(".bed") + ".blocklist.bed"
     cmd = (
         bedtools
         + " subtract -N -f "
@@ -61,9 +58,9 @@ def filter_by_blocklist(bed_file, diff_cutoff, diff_bed_file):
         + " -b "
         + diff_bed_file
         + " > "
-        + bed_file.rstrip(".bed")
-        + ".blocklist.bed"
+        + out_blocklist_file
     )
+    out_blocklist_filtered_out_file = prefix + '/' + bed_file.rstrip(".bed") + ".blocklist.filtered_out.bed"
     os.system(cmd)
     cmd = (
         bedtools
@@ -72,49 +69,44 @@ def filter_by_blocklist(bed_file, diff_cutoff, diff_bed_file):
         + " -a "
         + bed_file
         + " -b "
-        + bed_file.rstrip(".bed")
-        + ".blocklist.bed"
+        + out_blocklist_file
         + " > "
-        + bed_file.rstrip(".bed")
-        + ".blocklist.filtered_out.bed"
+        + out_blocklist_filtered_out_file
     )
     os.system(cmd)
+    out_blocklist_annotate_file = prefix + '/' + bed_file.rstrip(".bed") + ".blocklist.annotate.bed"
     cmd = (
         "cat "
-        + bed_file.rstrip(".bed")
-        + ".blocklist.filtered_out.bed"
+        + out_blocklist_filtered_out_file
         + ' | awk \'{print $1"\t"$2"\t"$3"\tBLOCKLIST"}\' > '
-        + bed_file.rstrip(".bed")
-        + ".blocklist.annotate.bed"
+        + out_blocklist_annotate_file
     )
     os.system(cmd)
 
-    out_file = bed_file.rstrip(".bed") + ".blocklist.annotate.bed"
-    return out_file
+    return out_blocklist_annotate_file
 
 
-def filter_by_length(bed_file, length_cutoff):
+def filter_by_length(bed_file, length_cutoff, prefix):
 
-    cmd = "awk '$3-$2<" + str(length_cutoff) + "' " + bed_file + " > " + bed_file.rstrip(".bed") + ".len.bed"
+    out_len_file = prefix + '/' + bed_file.rstrip(".bed") + ".len.bed"
+    cmd = "awk '$3-$2<" + str(length_cutoff) + "' " + bed_file + " > " + out_len_file
     os.system(cmd)
+    out_len_annotate_file = prefix + '/' + bed_file.rstrip(".bed") + ".len.annotate.bed"
     cmd = (
         "cat "
-        + bed_file.rstrip(".bed")
-        + ".len.bed"
+        + out_len_file
         + ' | awk \'{print $1"\t"$2"\t"$3"\tLEN"}\' > '
-        + bed_file.rstrip(".bed")
-        + ".len.annotate.bed"
+        + out_len_annotate_file
     )
     os.system(cmd)
-    out_file = bed_file.rstrip(".bed") + ".len.annotate.bed"
-    return out_file
+    return out_len_annotate_file
 
 
-def annotate_bed(bed_file, lcr_cutoff, lcr_file, diff_cutoff, diff_bed_file, length_cutoff=10000):
+def annotate_bed(bed_file, lcr_cutoff, lcr_file, diff_cutoff, diff_bed_file, prefix, length_cutoff=10000):
     # get filters regions
-    lcr_bed_file = filter_by_lcr(bed_file, lcr_cutoff, lcr_file)
-    black_bed_file = filter_by_blocklist(bed_file, diff_cutoff, diff_bed_file)
-    length_bed_file = filter_by_length(bed_file, length_cutoff)
+    lcr_bed_file = filter_by_lcr(bed_file, lcr_cutoff, lcr_file, prefix)
+    black_bed_file = filter_by_blocklist(bed_file, diff_cutoff, diff_bed_file, prefix)
+    length_bed_file = filter_by_length(bed_file, length_cutoff, prefix)
 
     # merge all filters and sort
     cmd = "cat " + lcr_bed_file + " " + black_bed_file + " " + length_bed_file + " > filters.annotate.unsorted.bed"
@@ -169,17 +161,25 @@ def main():
     args.add_argument("--coverage_lcr_file", help="UG-CNV-LCR bed file", required=True, type=str)
     args.add_argument("--blocklist", help="blocklist bed file", required=True, type=str)
     args.add_argument("--min_cnv_length", required=True, type=int, default=10000)
+    args.add_argument("--out_directory", help="out directory where intermediate and output files will be saved."
+                                               " if not supplied all files will be written to current directory",
+                      required=False, type=str)
 
     args = args.parse_args()
 
+    prefix = ""
+    if args.out_prefix:
+        prefix = args.out_prefix
     [out_annotate_bed_file, out_filtered_bed_file] = annotate_bed(
         args.input_bed_file,
         args.intersection_cutoff,
         args.coverage_lcr_file,
         args.intersection_cutoff,
         args.blocklist,
-        args.min_cnv_length,
+        prefix,
+        args.min_cnv_length
     )
+
     print("output files:")
     print(out_annotate_bed_file)
     print(out_filtered_bed_file)
