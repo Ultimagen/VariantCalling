837434061e3a0cd6f5b245c2761e075c9f9cc7c9
Tammy Biniashvili <tammy.biniashvili@ultimagen.com>
2022-12-13 08:32:03 +0000

    update filter_variants_pipeline for optional input model

--- test/system/test_filter_variants_pipeline.py
+++ test/system/test_filter_variants_pipeline.py
@@ -43,3 +43,34 @@ class TestFilterVariantPipeline:
 
         df = vcftools.get_vcf_df(output_file)
         assert {"LOW_SCORE": 104, "PASS": 804} == dict(df["filter"].value_counts())
+
+    def test_filter_variants_pipeline_blacklist_only(self, tmpdir):
+        output_file = f"{tmpdir}/004777-X0024.annotated.AF_chr1_1_1000000_filtered.blacklist_only.vcf.gz"
+        filter_variants_pipeline.run(
+            [
+                "--input_file",
+                f"{self.inputs_dir}/004777-X0024.annotated.AF_chr1_1_1000000.vcf.gz",
+                "--runs_file",
+                f"{self.general_inputs_dir}/hg38_runs.conservative.bed",
+
+                "--reference_file",
+                f"{self.general_inputs_dir}/Homo_sapiens_assembly38.fasta",
+                "--blacklist_cg_insertions",
+                "--annotate_intervals",
+                f"{self.general_inputs_dir}/LCR-hs38.bed",
+                "--annotate_intervals",
+                f"{self.general_inputs_dir}/exome.twist.bed",
+                "--annotate_intervals",
+                f"{self.general_inputs_dir}/mappability.0.bed",
+                "--annotate_intervals",
+                f"{self.general_inputs_dir}/hmers_7_and_higher.bed",
+                "--output_file",
+                output_file,
+                "--blacklist",
+                f"{self.inputs_dir}/blacklist_example.chr1_1_1000000.pkl",
+            ]
+        )
+
+        df = vcftools.get_vcf_df(output_file)
+        assert 4 == df[df['blacklst'].notna()]['blacklst'].count()
+
