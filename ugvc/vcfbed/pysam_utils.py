9ef0a676f48d1fdc18a7dcd8c7305edc365f4c63
Ilya Soifer <ilya.soifer@ultimagen.com>
2023-01-31 15:13:31 +0200

    [BIOIN-803] Variant pysam services

--- ugvc/vcfbed/pysam_utils.py
+++ ugvc/vcfbed/pysam_utils.py
@@ -133,3 +133,86 @@ def is_snp(alleles: list[str]):
     """
     __alleles = [a for a in alleles if a != "<NON_REF>"]
     return len(__alleles) >= 2 and all(len(a) == 1 for a in __alleles)
+
+
+DNA_SYMBOLS = ["A", "T", "G", "C"]
+
+
+def is_symbolic(allele: str) -> bool:
+    """Check if allele is symbolic
+
+    Parameters
+    ----------
+    allele : str
+        Input allele
+
+    Returns
+    -------
+    bool
+        T/F
+    """
+    return len([x for x in allele if x not in DNA_SYMBOLS]) > 0
+
+
+def is_indel(rec: VariantRecord) -> list[bool]:
+    """Checks for each alt allele of the variant if it is indel
+
+    Parameters
+    ----------
+    rec : pysam.VariantRecord
+        Input variant
+
+    Returns
+    -------
+    List[bool]
+        list of T/F
+    """
+    return [len(x) != len(rec.alleles[0]) and not is_symbolic(x) for x in rec.alleles]
+
+
+def is_deletion(rec: VariantRecord) -> list[bool]:
+    """For every allele in the variant record - report if the allele is deletion
+
+    Parameters
+    ----------
+    rec : pysam.VariantRecord
+        Report if the variant is deletion for each allele
+
+    Returns
+    -------
+    List[bool]
+        List of T/F
+    """
+    return [len(x) < len(rec.alleles[0]) and not is_symbolic(x) for x in rec.alleles]
+
+
+def is_insertion(rec: VariantRecord) -> list[bool]:
+    """For every allele in the variant record - report if the allele is insertion
+
+    Parameters
+    ----------
+    rec : pysam.VariantRecord
+        Report if the variant is deletion for each allele
+
+    Returns
+    -------
+    List[bool]
+        List of T/F
+    """
+    return [len(x) > len(rec.alleles[0]) and not is_symbolic(x) for x in rec.alleles]
+
+
+def indel_length(rec: VariantRecord) -> list[int]:
+    """Length of all indel alleles
+
+    Parameters
+    ----------
+    rec : pysam.VariantRecord
+        Input record
+
+    Returns
+    -------
+    List[int]
+        List of length differences between the allele and the reference allele
+    """
+    return [abs(len(x) - len(rec.alleles[0])) if not is_symbolic(x) else 0 for x in rec.alleles]
