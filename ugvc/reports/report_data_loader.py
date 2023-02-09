cc86efbbb2c5d91039e55ea6d21697a6987c3b8c
doronst <doron.shemtov@ultimagen.com>
2023-02-09 16:46:22 +0000

    report: refactor report_data_loader, use vcfeval penalty==2 fp/fn counts

---
+++ ugvc/reports/report_data_loader.py
@@ -0,0 +1,91 @@
+import pandas as pd
+
+from ugvc.comparison.concordance_utils import read_hdf
+
+
+class ReportDataLoader:
+    def __init__(self, concordance_file: str, reference_version: str):
+        self.concordance_file = concordance_file
+        self.reference_version = reference_version
+        self.columns_to_select, self.rename_dict = self.__choose_relevant_df_columns()
+        self.sources = {}
+
+    def load_concordance_df(self):
+        # TODO add description what this table represents?
+        self.sources["Trained wo gt"] = (self.concordance_file, "concordance")
+        df = read_hdf(self.concordance_file, key="concordance")
+        df.rename(columns=self.rename_dict, inplace=True)
+        return df
+
+    def load_whole_genome_concordance_df(self):
+        """Load the concordance data for the entire genome"""
+        self.sources["whole genome"] = (self.concordance_file, "all")
+        with pd.HDFStore(self.concordance_file) as hdf:
+            keys = hdf.keys()
+            wg_dfs = []
+            for k in keys:
+                if k in {"/concordance", "/input_args"}:
+                    continue
+                tmp: pd.DataFrame = pd.read_hdf(hdf, key=k)
+                tmp = tmp[[x for x in self.columns_to_select if x in tmp.columns]]
+                wg_dfs.append(tmp)
+            wg_df = pd.concat(wg_dfs)
+            wg_df.rename(columns=self.rename_dict, inplace=True)
+
+            return wg_df
+
+    def load_trained_without_gt_concordance_df(self, trained_with_gt):
+        self.sources["Trained with gt"] = (trained_with_gt, "scored_concordance")
+        df = read_hdf(trained_with_gt, key="scored_concordance")
+        df.rename(columns=self.rename_dict, inplace=True)
+        return df
+
+    def __choose_relevant_df_columns(self):
+        columns_to_select, rename_dict = None, None
+        if self.reference_version == "hg38":
+            columns_to_select = [
+                "call",
+                "base",
+                "indel",
+                "hmer_indel_length",
+                "tree_score",
+                "filter",
+                "blacklst",
+                "classify",
+                "classify_gt",
+                "indel_length",
+                "hmer_indel_nuc",
+                "ref",
+                "gt_ground_truth",
+                "well_mapped_coverage",
+                "mappability.0",
+                "ug_hcr",
+                "LCR-hs38",
+            ]
+            rename_dict = {"LCR-hs38": "LCR"}
+        elif self.reference_version == "hg19":
+            columns_to_select = [
+                "call",
+                "base",
+                "indel",
+                "hmer_indel_length",
+                "tree_score",
+                "filter",
+                "blacklst",
+                "classify",
+                "classify_gt",
+                "indel_length",
+                "hmer_indel_nuc",
+                "ref",
+                "gt_ground_truth",
+                "well_mapped_coverage",
+                "LCR-hg19_tab_no_chr",
+                "mappability.hg19.0_tab_no_chr",
+                "ug_hcr_hg19_no_chr",
+            ]
+            rename_dict = {
+                "LCR-hg19_tab_no_chr": "LCR",
+                "mappability.hg19.0_tab_no_chr": "mappability.0",
+                "ug_hcr_hg19_no_chr": "ug_hcr",
+            }
+        return columns_to_select, rename_dict
--- ugvc/reports/report_utils.py
+++ ugvc/reports/report_utils.py
@@ -1,55 +1,60 @@
-import pandas as pd
-import numpy as np
+from configparser import ConfigParser
+
 import matplotlib.pyplot as plt
 import nexusplt as nxp
-from configparser import ConfigParser
+import numpy as np
+import pandas as pd
 
 
 def parse_config(config_file):
     parser = ConfigParser()
     parser.read(config_file)
-    param_names = ['run_id',
-                   'pipeline_version',
-                   'h5_concordance_file',
-                   'model_name_with_gt',
-                   'model_name_without_gt',
-                   'model_pkl_with_gt',
-                   'model_pkl_without_gt',
-                   'model_name'
-                   ]
-    parameters = {p: parser.get('VarReport', p) for p in param_names}
-    parameters['reference_version'] = parser.get('VarReport', 'reference_version', fallback='hg38')
+    param_names = [
+        "run_id",
+        "pipeline_version",
+        "h5_concordance_file",
+    ]
+    parameters = {p: parser.get("VarReport", p) for p in param_names}
 
     # Optional parameters
-    parameters['truth_sample_name'] = parser.get('VarReport', 'truth_sample_name', fallback='NA')
-    parameters['sources'] = {'Trained wo gt': (parameters['h5_concordance_file'], "concordance")}
-    parameters['image_prefix'] = parser.get('VarReport', 'image_output_prefix',
-                                            fallback=parameters['run_id'] + '.vars') + '.'
-    parameters['h5outfile'] = parser.get('VarReport', 'h5_output', fallback='var_report.h5')
-    parameters['trained_w_gt'] = parser.get('VarReport', 'h5_model_file', fallback=None)
+    parameters["reference_version"] = parser.get("VarReport", "reference_version", fallback="hg38")
+    parameters["truth_sample_name"] = parser.get("VarReport", "truth_sample_name", fallback="NA")
+    parameters["image_prefix"] = (
+        parser.get("VarReport", "image_output_prefix", fallback=parameters["run_id"] + ".vars") + "."
+    )
+    parameters["h5outfile"] = parser.get("VarReport", "h5_output", fallback="var_report.h5")
+    parameters["trained_w_gt"] = parser.get("VarReport", "h5_model_file", fallback=None)
+
+    if parameters["truth_sample_name"]:
+        param_names.append("truth_sample_name")
+
+    for opt_param_name in (
+        "model_name_with_gt",
+        "model_name_without_gt",
+        "model_pkl_with_gt",
+        "model_pkl_without_gt",
+        "model_name",
+    ):
+        opt_param = parser.get("VarReport", opt_param_name, fallback=None)
+        if opt_param:
+            parameters[opt_param_name] = opt_param
+            param_names.append(opt_param_name)
 
     return parameters, param_names
 
 
 class ShortReportUtils:
-
     def __init__(self, image_dir, image_prefix):
 
         self.image_dir = image_dir
         self.image_prefix = image_prefix
 
-    def plot_performance(self,
-                         perf_curve,
-                         opt_res,
-                         categories,
-                         sources: dict,
-                         ext=None,
-                         img=None,
-                         legend=None,
-                         opt_res_sec=None):
+    def plot_performance(
+        self, perf_curve, opt_res, categories, sources: dict, ext=None, img=None, legend=None, opt_res_sec=None
+    ):
         n = len(categories)
         fig, ax = plt.subplots(1, n, figsize=(4 * n, 4))
-        col = ['r', 'b', 'g', 'm', 'k']
+        col = ["r", "b", "g", "m", "k"]
 
         for i, cat in enumerate(categories):
             for j, s in enumerate(sources):
@@ -58,11 +63,11 @@ class ShortReportUtils:
                 if opt_res_sec is not None:
                     opt_sec = opt_res_sec[s][cat]
                 if not perf.empty:
-                    ax[i].plot(perf.recall, perf.precision, '-', label=s, color=col[j])
-                    ax[i].plot(opt.get('recall'), opt.get('precision'), 'o', color=col[j])
+                    ax[i].plot(perf.recall, perf.precision, "-", label=s, color=col[j])
+                    ax[i].plot(opt.get("recall"), opt.get("precision"), "o", color=col[j])
                     if opt_res_sec is not None:
-                        ax[i].plot(opt_sec.get('recall'), opt_sec.get('precision'), 'o', color="black")
-                title = cat if ext is None else '{0} ({1})'.format(cat, ext)
+                        ax[i].plot(opt_sec.get("recall"), opt_sec.get("precision"), "o", color="black")
+                title = cat if ext is None else "{0} ({1})".format(cat, ext)
                 ax[i].set_title(title)
                 ax[i].set_xlabel("Recall")
                 ax[i].set_xlim([0.6, 1])
@@ -71,9 +76,9 @@ class ShortReportUtils:
 
         ax[0].set_ylabel("Precision")
         if legend:
-            ax[0].legend(loc='lower left')
+            ax[0].legend(loc="lower left")
         if img:
-            nxp.save(fig, self.image_prefix + img, 'png', outdir=self.image_dir)
+            nxp.save(fig, self.image_prefix + img, "png", outdir=self.image_dir)
 
     def get_performance(self, data, categories, sources):
         opt_tab = {}
@@ -84,19 +89,25 @@ class ShortReportUtils:
             opt_res[s] = {}
             perf_curve[s] = {}
 
-            for i, cat in enumerate(categories):
+            for cat in categories:
                 d = self.__filter_by_category(data[s], cat)
-                perf, opt, pos, neg = self.__calc_performance(d)
-                perf_curve[s][cat] = perf
-                opt_res[s][cat] = opt
-
-                row = pd.DataFrame({'# pos': pos,
-                                    '# neg': neg,
-                                    'max recall': np.nan if perf.empty else max(perf.recall),
-                                    'recall': np.nan if perf.empty else opt.get('recall'),
-                                    'precision': np.nan if perf.empty else opt.get('precision'),
-                                    'F1': np.nan if perf.empty else opt.get('f1')
-                                    }, index=[cat])
+                performance_dict = self.__calc_performance(d)
+                pr_curve = performance_dict["pr_curve"]
+                perf_curve[s][cat] = pr_curve
+                opt_res[s][cat] = performance_dict
+                # Report table columns
+                row = pd.DataFrame(
+                    {
+                        "# pos": performance_dict["initial_tp"] + performance_dict["initial_fn"],
+                        "# neg": performance_dict["initial_fp"],
+                        "# init fn": performance_dict["initial_fn"],
+                        "max recall": performance_dict["max_recall"],
+                        "recall": performance_dict["recall"],
+                        "precision": performance_dict["precision"],
+                        "F1": performance_dict["f1"],
+                    },
+                    index=[cat],
+                )
                 opt_tab[s] = pd.concat([opt_tab[s], row])
 
         return opt_tab, opt_res, perf_curve
@@ -104,93 +115,111 @@ class ShortReportUtils:
     @staticmethod
     def has_sec(x):
         res = False
-        if x is not None:
-            if x == x:
-                if "SEC" in x:
-                    res = True
+        if x is not None and not pd.isna(x):
+            if "SEC" in x:
+                res = True
         return res
 
     @staticmethod
-    def __calc_performance(data: pd.DataFrame):
-        classify_col = 'classify_gt'
+    def __calc_performance(data: pd.DataFrame) -> dict:
+        score_name = "tree_score"
         d = data.copy()
-
+        d = d[["call", "base", score_name, "filter"]]
+        d.loc[d["call"].isna(), "call"] = "NA"
+        d.loc[d["base"].isna(), "base"] = "NA"
         # Change tree_score such that PASS will get high score values. FNs will be the lowest
-        score_pass = d.query('filter=="PASS" & tree_score==tree_score').head(20)['tree_score'].mean()
-        score_not_pass = d.query('filter!="PASS" & tree_score==tree_score').head(20)['tree_score'].mean()
+        # score_pass = d[(d['filter']=="PASS") & (d[score_name].isna())].head(20)[score_name].mean()
+        # score_not_pass =  d[(d['filter']!="PASS") & (d[score_name].isna())].head(20)[score_name].mean()
+        score_pass = d.query('filter=="PASS" & tree_score==tree_score').head(20)["tree_score"].mean()
+        score_not_pass = d.query('filter!="PASS" & tree_score==tree_score').head(20)["tree_score"].mean()
         dir_switch = 1 if score_pass > score_not_pass else -1
-        score = d['tree_score'] * dir_switch
+        score = d[score_name] * dir_switch
         score = score - score.min()
-        d['tree_score'] = np.where(d[classify_col] == 'fn', -1, score)
 
-        # Calculate precision and recall continuously along the tree_score values
-        d = d[[classify_col, 'tree_score', 'filter']].sort_values(by=['tree_score'])
+        d["fp"] = (d["call"] == "FP") | (d["call"] == "FP_CA")
+        d["fn"] = (d["base"] == "FN") | (d["base"] == "FN_CA")
+        d["tp"] = (d["base"] == "TP") & (d["call"] == "TP")
 
-        d['label'] = np.where(d[classify_col] == 'fp', 0, 1)
-
-        d.loc[d[classify_col] == 'fn', 'filter'] = 'MISS'
-
-        num = len(d)
-        num_pos = sum(d['label'])
-        num_neg = num - num_pos
-        if num < 10:
-            return pd.DataFrame(), None, num_pos, num_neg
-
-        d['fn'] = np.cumsum(d['label'])
-        d['tp'] = num_pos - (d['fn'])
-        d['fp'] = num_neg - np.cumsum(1 - d['label'])
-
-        d['recall'] = d['tp'] / (d['fn'] + d['tp'])
-        d['precision'] = d['tp'] / (d['fp'] + d['tp'])
-        d['f1'] = d['tp'] / (d['tp'] + 0.5 * d['fn'] + 0.5 * d['fp'])
-
-        d['mask'] = ((d['tp'] + d['fn']) >= 20) & ((d['tp'] + d['fp']) >= 20) & (d['tree_score'] >= 0)
-        if len(d[d['mask']]) == 0:
-            return pd.DataFrame(), None, num_pos, num_neg
+        # define variants which didn't have any candidate
+        missing_candiddates_index = (d["base"] == "FN") & (d["call"] == "NA")
+        missing_candidates = missing_candiddates_index.sum()
+        # Give missing candidates score of -1 (order them at the top for pr_curve)
+        d[score_name] = np.where(missing_candiddates_index, -1, score)
 
         # Calculate the precision and recall as ouputted by the model (based on the FILTER column)
-        d['class'] = np.where(d['label'] == 0, 'FP', 'FN')
-        d.loc[(d['label'] == 1) & (d['filter'] == 'PASS'), 'class'] = 'TP'
-        d.loc[(d['label'] == 0) & (d['filter'] != 'PASS'), 'class'] = 'TN'
-
-        fn = len(d[d['class'] == 'FN'])
-        tp = len(d[d['class'] == 'TP'])
-        fp = len(d[d['class'] == 'FP'])
+        filtered_tp = len(d[d["tp"] & (d["filter"] != "PASS")])
+        filtered_fp = len(d[d["fp"] & (d["filter"] != "PASS")])
+        initial_fp = d["fp"].sum()
+        initial_tp = d["tp"].sum()
+        initial_fn = d["fn"].sum()
+        fp = initial_fp - filtered_fp
+        fn = initial_fn + filtered_tp
+        tp = initial_tp - filtered_tp
 
         recall = tp / (tp + fn) if (tp + fn > 0) else np.nan
         precision = tp / (tp + fp) if (tp + fp > 0) else np.nan
-        max_recall = 1 - len(d[d['filter'] == 'MISS']) / num_pos
-
         f1 = tp / (tp + 0.5 * fn + 0.5 * fp)
+        max_recall = (tp + fn - missing_candidates) / (tp + fn)
+        result_dict = {
+            "pr_curve": pd.DataFrame(),
+            "recall": recall,
+            "precision": precision,
+            "f1": f1,
+            "max_recall": max_recall,
+            "initial_tp": initial_tp,
+            "initial_fp": initial_fp,
+            "initial_fn": initial_fn,
+            "tp": tp,
+            "fp": fp,
+            "fn": fn,
+        }
+        if len(d) < 10:
+            return result_dict
 
-        return (d[['recall', 'precision']][d['mask']],
-                dict({'recall': recall, 'precision': precision, 'f1': f1}),
-                num_pos, num_neg)
+        # Sort by score to get precision/recall curve
+        d = d.sort_values(by=[score_name])
+
+        # Calculate precision and recall continuously
+
+        # how many true variants are either initial FNs (missing/filtered) or below score threshold
+        d["fn"] = initial_fn + np.cumsum(d["tp"])
+        d["tp"] = tp - np.cumsum(d["tp"])  # how many tps pass score threshold
+        d["fp"] = fp - np.cumsum(d["fp"])  # how many fps pass score threshold
+        d["recall"] = d["tp"] / (d["fn"] + d["tp"])
+        d["precision"] = d["tp"] / (d["fp"] + d["tp"])
+
+        # Select for pr_curve variants which are not missed candidates / first 20 pos/neg variants
+        d["mask"] = ((d["tp"] + d["fn"]) >= 20) & ((d["tp"] + d["fp"]) >= 20) & (d[score_name] >= 0)
+        if len(d[d["mask"]]) > 0:
+            result_dict["pr_curve"] = d[["fp", "fn", "tp", "recall", "precision"]][d["mask"]]
+        return result_dict
 
     @staticmethod
-    def __filter_by_category(data, cat):
-        if cat == 'SNP':
-            return data[data['indel'] == False]
-        if cat == 'Indel':
-            return data[data['indel'] == True]
-        elif cat == 'non-hmer Indel':
-            return data[(data['indel'] == True) & (data['hmer_indel_length'] == 0) & (data['indel_length'] > 0)]
-        elif cat == 'non-hmer Indel w/o LCR':
-            return data[(data['indel'] == True) & (data['hmer_indel_length'] == 0) & (data['indel_length'] > 0) &
-                        (~data['LCR'])]
-        elif cat == 'hmer Indel <=4':
-            return data[(data['indel'] == True) & (data['hmer_indel_length'] > 0) & (data['hmer_indel_length'] <= 4)]
-        elif cat == 'hmer Indel >4,<=8':
-            return data[(data['indel'] == True) & (data['hmer_indel_length'] > 4) & (data['hmer_indel_length'] <= 8)]
-        elif cat == 'hmer Indel >8,<=10':
-            return data[(data['indel'] == True) & (data['hmer_indel_length'] > 8) & (data['hmer_indel_length'] <= 10)]
-        elif cat == 'hmer Indel >10,<=14':
-            return data[(data['indel'] == True) & (data['hmer_indel_length'] > 10) & (data['hmer_indel_length'] <= 14)]
-        elif cat == 'hmer Indel >15,<=19':
-            return data[(data['indel'] == True) & (data['hmer_indel_length'] > 14) & (data['hmer_indel_length'] <= 19)]
-        elif cat == 'hmer Indel >=20':
-            return data[(data['indel'] == True) & (data['hmer_indel_length'] >= 20)]
+    def __filter_by_category(data, cat) -> pd.DataFrame:
+        result = None
+        if cat == "SNP":
+            result = data[~data["indel"]]
+        elif cat == "Indel":
+            result = data[data["indel"]]
+        elif cat == "non-hmer Indel":
+            result = data[(data["indel"]) & (data["hmer_indel_length"] == 0) & (data["indel_length"] > 0)]
+        elif cat == "non-hmer Indel w/o LCR":
+            result = data[
+                (data["indel"]) & (data["hmer_indel_length"] == 0) & (data["indel_length"] > 0) & (~data["LCR"])
+            ]
+        elif cat == "hmer Indel <=4":
+            result = data[(data["indel"]) & (data["hmer_indel_length"] > 0) & (data["hmer_indel_length"] <= 4)]
+        elif cat == "hmer Indel >4,<=8":
+            result = data[(data["indel"]) & (data["hmer_indel_length"] > 4) & (data["hmer_indel_length"] <= 8)]
+        elif cat == "hmer Indel >8,<=10":
+            result = data[(data["indel"]) & (data["hmer_indel_length"] > 8) & (data["hmer_indel_length"] <= 10)]
+        elif cat == "hmer Indel >10,<=14":
+            result = data[(data["indel"]) & (data["hmer_indel_length"] > 10) & (data["hmer_indel_length"] <= 14)]
+        elif cat == "hmer Indel >15,<=19":
+            result = data[(data["indel"]) & (data["hmer_indel_length"] > 14) & (data["hmer_indel_length"] <= 19)]
+        elif cat == "hmer Indel >=20":
+            result = data[(data["indel"]) & (data["hmer_indel_length"] >= 20)]
         for i in range(1, 10):
-            if cat == 'hmer Indel {0:d}'.format(i):
-                return data[(data['indel'] == True) & (data['hmer_indel_length'] == i)]
-        return None
+            if cat == "hmer Indel {0:d}".format(i):
+                result = data[(data["indel"]) & (data["hmer_indel_length"] == i)]
+        return result
