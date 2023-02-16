af2a35c082a5d8ae02e6767a01259cbe360acb5b
doron <doron.shemtov@ultimagen.com>
2023-02-16 13:11:48 +0200

    report: get rid of with_gt/wo_gt, sources, image saving, and introduce papermill

--- test/system/test_vc_report.py
+++ test/system/test_vc_report.py
@@ -1,164 +1,68 @@
-import shutil
 import subprocess
 from os.path import join as pjoin
 from test import get_resource_dir, test_dir
 
+jupyter_convert_cmd = (
+    "jupyter nbconvert --to html createVarReport.nbconvert.ipynb "
+    "--template full --no-input --output varReport.html".split()
+)
+data_dir = get_resource_dir(__file__)
+path = test_dir
+report_path = pjoin(path, "..", "ugvc", "reports")
+
 
 def test_vc_report(tmpdir):
-    path = test_dir
-    datadir = get_resource_dir(__file__)
-    report_path = pjoin(path, "..", "ugvc", "reports")
-
-    shutil.copy(pjoin(report_path, "createVarReport.ipynb"), tmpdir)
-    shutil.copy(pjoin(report_path, "nexusplt.py"), tmpdir)
-    shutil.copy(pjoin(datadir, "test.untrained.h5"), tmpdir)
-    shutil.copy(pjoin(datadir, "test.trained.h5"), tmpdir)
-    with open(pjoin(tmpdir, "var_report.config"), "w") as config_file:
-        strg = """
-            [VarReport]
-            verbosity = 5
-            run_id = 140479-BC21
-            pipeline_version = 2.4.1
-            reference_version = hg38
-            h5_concordance_file = ./test.untrained.h5
-            h5_model_file = ./test.trained.h5
-            const_model = untrained_ignore_gt_excl_hpol_runs
-            trained_model = threshold_model_recall_precision_ignore_gt_excl_hpol_runs
-            h5_output = test.var_report.h5
-            model_name_with_gt = untrained_ignore_gt_excl_hpol_runs
-            model_name_without_gt = threshold_model_recall_precision_ignore_gt_excl_hpol_runs
-            model_pkl_with_gt = dummy1.pkl
-            model_pkl_without_gt = dummy2.pkl
-
-        """
-        config_file.write(strg)
-
-    cmd = [
-        "jupyter",
-        "nbconvert",
-        "--to",
-        "notebook",
-        "--execute",
-        "createVarReport.ipynb",
-    ]
+    cmd = (
+        f"papermill {report_path}/createVarReport.ipynb createVarReport.nbconvert.ipynb "
+        "-p verbosity 5 "
+        "-p run_id 140479-BC21 "
+        "-p pipeline_version 2.4.1 "
+        f"-p h5_concordance_file {data_dir}/test.untrained.h5 "
+        "-p h5_output h5_output_file "
+        "-p model_name untrained_ignore_gt_excl_hpol_runs ".split()
+    )
     assert subprocess.check_call(cmd, cwd=tmpdir) == 0
+    assert subprocess.check_call(jupyter_convert_cmd, cwd=tmpdir) == 0
 
-    cmd = [
-        "jupyter",
-        "nbconvert",
-        "--to",
-        "html",
-        "createVarReport.nbconvert.ipynb",
-        "--template",
-        "full",
-        "--no-input",
-        "--output",
-        "varReport.html",
-    ]
+
+def test_vc_report_verbosity_1(tmpdir):
+    cmd = (
+        f"papermill {report_path}/createVarReport.ipynb createVarReport.nbconvert.ipynb "
+        "-p verbosity 5 "
+        "-p run_id 140479-BC21 "
+        "-p pipeline_version 2.4.1 "
+        f"-p h5_concordance_file {data_dir}/test.untrained.h5 "
+        "-p h5_output h5_output_file "
+        "-p model_name untrained_ignore_gt_excl_hpol_runs ".split()
+    )
     assert subprocess.check_call(cmd, cwd=tmpdir) == 0
+    assert subprocess.check_call(jupyter_convert_cmd, cwd=tmpdir) == 0
 
 
 def test_vc_report_wo_ref(tmpdir):
-    path = test_dir
-    datadir = get_resource_dir(__file__)
-    report_path = pjoin(path, "..", "ugvc", "reports")
-
-    shutil.copy(pjoin(report_path, "createVarReport.ipynb"), tmpdir)
-    shutil.copy(pjoin(report_path, "nexusplt.py"), tmpdir)
-    shutil.copy(pjoin(datadir, "test.untrained.h5"), tmpdir)
-    shutil.copy(pjoin(datadir, "test.trained.h5"), tmpdir)
-    with open(pjoin(tmpdir, "var_report.config"), "w") as config_file:
-        strg = """
-            [VarReport]
-            verbosity = 5
-            run_id = 140479-BC21
-            pipeline_version = 2.4.1
-            h5_concordance_file = ./test.untrained.h5
-            h5_model_file = ./test.trained.h5
-            const_model = untrained_ignore_gt_excl_hpol_runs
-            trained_model = threshold_model_recall_precision_ignore_gt_excl_hpol_runs
-            h5_output = test.var_report.h5
-            model_name_with_gt = untrained_ignore_gt_excl_hpol_runs
-            model_name_without_gt = threshold_model_recall_precision_ignore_gt_excl_hpol_runs
-            model_pkl_with_gt = dummy1.pkl
-            model_pkl_without_gt = dummy2.pkl
-
-        """
-        config_file.write(strg)
-
-    cmd = [
-        "jupyter",
-        "nbconvert",
-        "--to",
-        "notebook",
-        "--execute",
-        "createVarReport.ipynb",
-    ]
+    cmd = (
+        f"papermill {report_path}/createVarReport.ipynb createVarReport.nbconvert.ipynb "
+        "-p verbosity 5 "
+        "-p run_id 140479-BC21 "
+        "-p pipeline_version 2.4.1 "
+        f"-p h5_concordance_file {data_dir}/test.untrained.h5 "
+        "-p h5_output h5_output_file "
+        "-p model_name untrained_ignore_gt_excl_hpol_runs ".split()
+    )
     assert subprocess.check_call(cmd, cwd=tmpdir) == 0
-
-    cmd = [
-        "jupyter",
-        "nbconvert",
-        "--to",
-        "html",
-        "createVarReport.nbconvert.ipynb",
-        "--template",
-        "full",
-        "--no-input",
-        "--output",
-        "varReport.html",
-    ]
-    assert subprocess.check_call(cmd, cwd=tmpdir) == 0
+    assert subprocess.check_call(jupyter_convert_cmd, cwd=tmpdir) == 0
 
 
 def test_vc_report_with_sec(tmpdir):
-    path = test_dir
-    datadir = get_resource_dir(__file__)
-    report_path = pjoin(path, "..", "ugvc", "reports")
-    shutil.copy(pjoin(report_path, "createVarReport.ipynb"), tmpdir)
-    shutil.copy(pjoin(report_path, "nexusplt.py"), tmpdir)
-    shutil.copy(pjoin(datadir, "test.trained.h5"), tmpdir)
-    shutil.copy(pjoin(datadir, "test.ug_hcr.sec.comp.h5"), tmpdir)
-    with open(pjoin(tmpdir, "var_report.config"), "w") as config_file:
-        strg = """
-            [VarReport]
-            verbosity = 5
-            run_id = 140479-BC21
-            pipeline_version = 2.4.1
-            reference_version = hg38
-            h5_concordance_file = ./test.ug_hcr.sec.comp.h5
-            h5_model_file = ./test.trained.h5
-            const_model = untrained_ignore_gt_excl_hpol_runs
-            trained_model = threshold_model_recall_precision_ignore_gt_excl_hpol_runs
-            h5_output = test.var_report.h5
-            model_name_with_gt = untrained_ignore_gt_excl_hpol_runs
-            model_name_without_gt = threshold_model_recall_precision_ignore_gt_excl_hpol_runs
-            model_pkl_with_gt = dummy1.pkl
-            model_pkl_without_gt = dummy2.pkl
+    cmd = (
+        f"papermill {report_path}/createVarReport.ipynb createVarReport.nbconvert.ipynb "
+        "-p verbosity 5 "
+        "-p run_id 140479-BC21 "
+        "-p pipeline_version 2.4.1 "
+        f"-p h5_concordance_file {data_dir}/test.ug_hcr.sec.comp.h5 "
+        "-p h5_output h5_output_file "
+        "-p model_name threshold_model_recall_precision_ignore_gt_excl_hpol_runs ".split()
+    )
 
-        """
-        config_file.write(strg)
-
-    cmd = [
-        "jupyter",
-        "nbconvert",
-        "--to",
-        "notebook",
-        "--execute",
-        "createVarReport.ipynb",
-    ]
     assert subprocess.check_call(cmd, cwd=tmpdir) == 0
-
-    cmd = [
-        "jupyter",
-        "nbconvert",
-        "--to",
-        "html",
-        "createVarReport.nbconvert.ipynb",
-        "--template",
-        "full",
-        "--no-input",
-        "--output",
-        "varReport.html",
-    ]
-    assert subprocess.check_call(cmd, cwd=tmpdir) == 0
+    assert subprocess.check_call(jupyter_convert_cmd, cwd=tmpdir) == 0
