960b92aa516b772d0c8febbd2295edbb5ccbb86e
mayalevy <maya.levy@ultimagen.com>
2023-03-23 12:30:57 +0200

    bioin-918 rearrage code and added tests

---
+++ test/unit/joint/test_compress_gvcf.py
@@ -0,0 +1,25 @@
+
+
+import ugvc.joint.compress_gvcf as compress_gvcf
+from os.path import join as pjoin
+from test import get_resource_dir
+import pysam
+
+def test_compress_gvcf(tmpdir):
+    inputs_dir = get_resource_dir(__file__)
+    input_file = pjoin(inputs_dir, "input.g.vcf.gz")
+    output_file = pjoin(tmpdir, "compressed.g.vcf.gz")
+    result = compress_gvcf.compressGVCF(input_file, output_file)
+    assert result == (4438, 1075)
+
+
+def test_get_compressed_pl_into_3_values():
+
+    result = compress_gvcf.get_compressed_pl_into_3_values((1,2,3))
+    assert result == (1,2,3)
+
+    result = compress_gvcf.get_compressed_pl_into_3_values((0,54,57,990, 2,990))
+    assert result == (0,54,2)
+
+    result = compress_gvcf.get_compressed_pl_into_3_values((0,54,57,990, 990,990,6,7,8,9))
+    assert result == (0,6,7)
--- ugvc/joint/compress_gvcf.py
+++ ugvc/joint/compress_gvcf.py
@@ -9,6 +9,23 @@ def get_sample_value(record, key, sample_name):
 
 
 def get_compressed_pl_into_3_values(record, sample_name):
+    '''
+    This function takes out PL values out of record, sample_name
+    PL is tuple which is size is decided by the number of alts.
+    This function makes it a 3 tuple for the merged record in the following manner:
+    The input PL is of the form of
+    (0,0),(0,1)(1,1),(0,2),(1,2),(2,2),(0,3),(1,3),(2,3),(3,3)..
+    so this function makes a 3 tuple by:
+    ((0,0), min((0,1),(0,2),(0,3)..),min(all the others which are not 0/*)
+    Parameters
+    ----------
+    record
+    sample_name
+
+    Returns
+    -------
+
+    '''
     compressed_pl = []
     pl = get_sample_value(record, 'PL', sample_name)
     if len(pl) == 3:
@@ -27,6 +44,43 @@ def get_compressed_pl_into_3_values(record, sample_name):
                     compressed_pl[min(i + 1, 2)] = min(compressed_pl[min(i + 1, 2)], cur_compressed_pl[i])
         return tuple(compressed_pl)
 
+
+
+def get_compressed_pl_into_3_values(pl):
+    '''
+    PL is tuple which its size is decided by the number of alts.
+    This function makes it a 3 tuple for the merged record in the following manner:
+    The input PL is of the form of
+    (0,0),(0,1)(1,1),(0,2),(1,2),(2,2),(0,3),(1,3),(2,3),(3,3)..
+    so this function makes a 3 tuple by:
+    ((0,0), min((0,1),(0,2),(0,3)..),min(all the others which are not 0/*)
+    Parameters
+    ----------
+    pl
+
+    Returns
+    -------
+    3-values tuple
+    '''
+    if len(pl) == 3:
+        return pl
+    else:
+        compressed_pl = []
+        n = 0
+        sum_n = 0
+        while sum_n < len(pl):
+            cur_compressed_pl = pl[sum_n:sum_n + n + 1]
+            n += 1
+            sum_n += n
+            for i in range(len(cur_compressed_pl)):
+                if i + 1 >= len(compressed_pl) and i <= 1:
+                    compressed_pl.append(cur_compressed_pl[i])
+                else:
+                    compressed_pl[min(i + 1, 2)] = min(compressed_pl[min(i + 1, 2)], cur_compressed_pl[i])
+        return tuple(compressed_pl)
+
+
+
 def get_parser() -> argparse.ArgumentParser:
     ap_var = argparse.ArgumentParser(prog="compress_gvcf.py", description="Compress GVCF file by merging similar rows")
     ap_var.add_argument("--input_path", help="Input gvcf file path", required=True, type=str)
@@ -34,17 +88,49 @@ def get_parser() -> argparse.ArgumentParser:
     return ap_var
 
 
-def run(argv: List[str]):
-    parser = get_parser()
-    args = parser.parse_args(argv[1:])
-    gvcf_in = pysam.VariantFile(args.input_path, "r")
-    gvcf_out = pysam.VariantFile(args.output_path, "w", header=gvcf_in.header)
+def compressGVCF(input_path, output_path):
+    """Makes the g.vcf file smaller by merging similar sequential records
+    The mergining is doe in the following way:
+    We merge sequential records as long as there GQ value of all the records in this merge is not different
+    from each other by more than 10
+    In addition, we want to keep RefCall records with GQ<22 and not merge them.
+    When merging we create a new record with the following parameters:
+    - chrom, pos, ref: are of the first record in this group of merged records
+    - id: '.'
+    - alts: ["<*>"]
+    - qual: 0
+    - GT: (0, 0)
+    - GQ: is the avg GQ of all the merged records
+    - MIN_DP: is the minimum MIN_DP of all the merged records
+    - END: is the end of the last record in the merged records
+    - PL: it is always of size 3, and each value is the minimum value in this index among all the records,
+        see get_compressed_pl_into_3_values for more details
+
+    Parameters
+    ----------
+    input_path : str
+        input gvcf
+    output_path : str
+        compressed gvcf
+
+    Returns
+    -------
+    tuple(int,int)
+        Number of records in input g.vcf, number of records in compressed g.vcf
+
+    """
+
+    gvcf_in = pysam.VariantFile(input_path, "r")
+    gvcf_out = pysam.VariantFile(output_path, "w", header=gvcf_in.header)
     fetch_gvcf = gvcf_in.fetch()
     sample_name = list(gvcf_in.header.samples)[0]
     merge_size = 0
     prev_record = None
     first_record = None
+    count_in = 0
+    count_out = 0
     for index, record in enumerate(fetch_gvcf):
+        count_in += 1
         if prev_record is None:
             # first record
             prev_record = record
@@ -54,7 +140,7 @@ def run(argv: List[str]):
             gq_sum = min_gq
             min_dp = get_sample_value(record, 'MIN_DP', sample_name)
             merge_size += 1
-            pl = get_compressed_pl_into_3_values(record, sample_name)
+            pl = get_compressed_pl_into_3_values(get_sample_value(record, 'PL', sample_name))
             continue
 
         gq_value = get_sample_value(record, 'GQ', sample_name)
@@ -68,6 +154,7 @@ def run(argv: List[str]):
             # if we don't merge - just add the record to the gvcf
             if merge_size == 1:
                 gvcf_out.write(prev_record)
+                count_out += 1
             else:
                 # create merged record
                 new_record = pysam.VariantHeader.new_record(gvcf_in.header)
@@ -84,6 +171,7 @@ def run(argv: List[str]):
                     new_record.samples[sample_name]['MIN_DP'] = min_dp
                 new_record.samples[sample_name]['PL'] = pl
                 gvcf_out.write(new_record)
+                count_out += 1
             # get ready for the next iteration
             prev_record = record
             first_record = record
@@ -92,7 +180,7 @@ def run(argv: List[str]):
             max_gq = min_gq
             gq_sum = min_gq
             min_dp = get_sample_value(record, 'MIN_DP', sample_name)
-            pl = get_compressed_pl_into_3_values(record, sample_name)
+            pl = get_compressed_pl_into_3_values(get_sample_value(record, 'PL', sample_name))
         else:
             # just continue with the same set of lines
             prev_record = record
@@ -105,13 +193,19 @@ def run(argv: List[str]):
                     min_dp = min(min_dp, cur_min_dp)
                 else:
                     min_dp = cur_min_dp
-            cur_pl = get_compressed_pl_into_3_values(record, sample_name)
+            cur_pl = get_compressed_pl_into_3_values(get_sample_value(record, 'PL', sample_name))
             pl = (min(cur_pl[0], pl[0]), min(cur_pl[1], pl[1]), min(cur_pl[2], pl[2]))
             gq_sum += gq_value
 
     gvcf_in.close()
     gvcf_out.close()
+    pysam.tabix_index(output_path, preset='vcf')
+
+    return count_in, count_out
 
 
 if __name__ == "__main__":
-    run(sys.argv)
+    parser = get_parser()
+    args = parser.parse_args(sys.argv[1:])
+    result = compressGVCF(args.input_path, args.output_path)
+    sys.stderr.write(f"Compressed {result[0]} into {result[1]} records")
