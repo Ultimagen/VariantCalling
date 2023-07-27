9711333295bb03800dfb73e97a7fa60d8a80aa84
Itai Rusinek <itai.rusinek@ultimagen.com>
2023-07-27 11:54:18 +0000

    refactored ugvc/mrd/balanced_epcr_utils.py to work with enumerations

--- ugvc/mrd/balanced_epcr_utils.py
+++ ugvc/mrd/balanced_epcr_utils.py
@@ -1,44 +1,61 @@
+from enum import Enum
+
 import pandas as pd
 
-from ugvc.utils.misc_utils import set_pyplot_defaults
 
-set_pyplot_defaults()
+# Trimmer segment labels and tags
+class TrimmerSegmentLabels(Enum):
+    T_HMER_START = "T_hmer_start"
+    T_HMER_END = "T_hmer_end"
+    A_HMER_START = "A_hmer_start"
+    A_HMER_END = "A_hmer_end"
+    NATIVE_ADAPTER = "native_adapter_with_leading_C"
+    STEM_END = "Stem_end"  # when native adapter trimming was done on-tool a modified format is used
 
 
-# Trimmer segment labels
-T_HMER_START = "T_hmer_start"
-T_HMER_END = "T_hmer_end"
-A_HMER_START = "A_hmer_start"
-A_HMER_END = "A_hmer_end"
-NATIVE_ADAPTER = "native_adapter_with_leading_C"
-STEM_END = "Stem_end"  # when native adapter trimming was done on-tool a modified format is used
-# Category names
-END_UNREACHED = "END_UNREACHED"
-HYB = "HYB"
-LIG = "LIG"
-MIXED = "MIXED"
-UNDETERMINED = "UNDETERMINED"
-# Internal names
-COUNT = "count"
-STRAND_RATIO_START = "strand_ratio_start"
-STRAND_RATIO_END = "strand_ratio_end"
-STRAND_RATIO_CATEGORY_START = "strand_ratio_category_start"
-STRAND_RATIO_CATEGORY_END = "strand_ratio_category_end"
-# Input parameters
+class TrimmerSegmentTags(Enum):
+    T_HMER_START = "ts"
+    T_HMER_END = "te"
+    A_HMER_START = "as"
+    A_HMER_END = "te"
+    NATIVE_ADAPTER = "a3"
+    STEM_END = "s2"  # when native adapter trimming was done on-tool a modified format is used
+
+
+class BalancedCategories(Enum):
+    # Category names
+    END_UNREACHED = "END_UNREACHED"
+    HYB = "HYB"
+    LIG = "LIG"
+    MIXED = "MIXED"
+    UNDETERMINED = "UNDETERMINED"
+
+
+class HistogramColumnNames(Enum):
+    # Internal names
+    COUNT = "count"
+    STRAND_RATIO_START = "strand_ratio_start"
+    STRAND_RATIO_END = "strand_ratio_end"
+    STRAND_RATIO_CATEGORY_START = "strand_ratio_category_start"
+    STRAND_RATIO_CATEGORY_END = "strand_ratio_category_end"
+
+
+# Input parameter defaults
 STRAND_RATIO_LOWER_THRESH = 0.27
 STRAND_RATIO_UPPER_THRESH = 0.73
 MIN_TOTAL_HMER_LENGTHS_IN_TAGS = 4
 MAX_TOTAL_HMER_LENGTHS_IN_TAGS = 8
+MIN_STEM_END_MATCHED_LENGTH = 11  # the stem is 12bp, 1 indel allowed as tolerance
 
 
 def get_annotation(x, sr_lower, sr_upper):
     if x == 0:
-        return HYB
+        return BalancedCategories.HYB.value
     if x == 1:
-        return LIG
+        return BalancedCategories.LIG.value
     if sr_lower <= x <= sr_upper:
-        return MIXED
-    return UNDETERMINED
+        return BalancedCategories.MIXED.value
+    return BalancedCategories.UNDETERMINED.value
 
 
 def read_balanced_epcr_trimmer_histogram(
@@ -47,6 +64,7 @@ def read_balanced_epcr_trimmer_histogram(
     sr_upper=STRAND_RATIO_UPPER_THRESH,
     min_total_hmer_lengths_in_tags=MIN_TOTAL_HMER_LENGTHS_IN_TAGS,
     max_total_hmer_lengths_in_tags=MAX_TOTAL_HMER_LENGTHS_IN_TAGS,
+    min_stem_end_matched_length=MIN_STEM_END_MATCHED_LENGTH,
     sample_name="",
     output_filename=None,
 ):
@@ -69,6 +87,8 @@ def read_balanced_epcr_trimmer_histogram(
     max_total_hmer_lengths_in_tags : int, optional
         maximum total hmer lengths in tags for determining strand ratio category
         default 8
+    min_stem_end_matched_length : int, optional
+        minimum length of stem end matched to determine the read end was reached
     sample_name : str, optional
         sample name to use as index, by default ""
     output_filename : str, optional
@@ -82,71 +102,98 @@ def read_balanced_epcr_trimmer_histogram(
     Raises
     ------
     ValueError
-        If required columns are missing ({COUNT}, {T_HMER_START}, {A_HMER_START})
+        If required columns are missing ("count", "T_hmer_start", "A_hmer_start")
     """
     # read histogram
     df_trimmer_histogram = pd.read_csv(trimmer_histogram)
     # change legacy segment names
     df_trimmer_histogram = df_trimmer_histogram.rename(
         columns={
-            "T hmer": T_HMER_START,
-            "A hmer": A_HMER_START,
-            "A_hmer_5": A_HMER_START,
-            "T_hmer_5": T_HMER_START,
-            "A_hmer_3": A_HMER_END,
-            "T_hmer_3": T_HMER_END,
+            "T hmer": TrimmerSegmentLabels.T_HMER_START.value,
+            "A hmer": TrimmerSegmentLabels.A_HMER_START.value,
+            "A_hmer_5": TrimmerSegmentLabels.A_HMER_START.value,
+            "T_hmer_5": TrimmerSegmentLabels.T_HMER_START.value,
+            "A_hmer_3": TrimmerSegmentLabels.A_HMER_END.value,
+            "T_hmer_3": TrimmerSegmentLabels.T_HMER_END.value,
         }
     )
     # make sure expected columns exist
-    for col in (COUNT, T_HMER_START, A_HMER_START):
+    for col in (
+        HistogramColumnNames.COUNT.value,
+        TrimmerSegmentLabels.T_HMER_START.value,
+        TrimmerSegmentLabels.A_HMER_START.value,
+    ):
         if col not in df_trimmer_histogram.columns:
             raise ValueError(f"Missing expected column {col} in {trimmer_histogram}")
-    if (A_HMER_END in df_trimmer_histogram.columns or T_HMER_END in df_trimmer_histogram.columns) and (
-        NATIVE_ADAPTER not in df_trimmer_histogram.columns and STEM_END not in df_trimmer_histogram.columns
+    if (
+        TrimmerSegmentLabels.A_HMER_END.value in df_trimmer_histogram.columns
+        or TrimmerSegmentLabels.T_HMER_END.value in df_trimmer_histogram.columns
+    ) and (
+        TrimmerSegmentLabels.NATIVE_ADAPTER.value not in df_trimmer_histogram.columns
+        and TrimmerSegmentLabels.STEM_END.value not in df_trimmer_histogram.columns
     ):
         # If an end tag exists (LA-v6)
-        raise ValueError(f"Missing expected column {NATIVE_ADAPTER} or {STEM_END} in {trimmer_histogram}")
+        raise ValueError(
+            f"Missing expected column {TrimmerSegmentLabels.NATIVE_ADAPTER.value} "
+            f"or {TrimmerSegmentLabels.STEM_END.value} in {trimmer_histogram}"
+        )
 
     df_trimmer_histogram.index.name = sample_name
 
     # add normalized count column
     df_trimmer_histogram = df_trimmer_histogram.assign(
-        count_norm=df_trimmer_histogram[COUNT] / df_trimmer_histogram[COUNT].sum()
+        count_norm=df_trimmer_histogram[HistogramColumnNames.COUNT.value]
+        / df_trimmer_histogram[HistogramColumnNames.COUNT.value].sum()
     )
     # add strand ratio columns and determine categories
-    tags_sum_5 = df_trimmer_histogram[T_HMER_START] + df_trimmer_histogram[A_HMER_START]
-    df_trimmer_histogram.loc[:, STRAND_RATIO_START] = (
-        (df_trimmer_histogram[T_HMER_START] / (tags_sum_5))
+    tags_sum_5 = (
+        df_trimmer_histogram[TrimmerSegmentLabels.T_HMER_START.value]
+        + df_trimmer_histogram[TrimmerSegmentLabels.A_HMER_START.value]
+    )
+    df_trimmer_histogram.loc[:, HistogramColumnNames.STRAND_RATIO_START.value] = (
+        (df_trimmer_histogram[TrimmerSegmentLabels.T_HMER_START.value] / (tags_sum_5))
         .where((tags_sum_5 >= min_total_hmer_lengths_in_tags) & (tags_sum_5 <= max_total_hmer_lengths_in_tags))
         .round(2)
     )
     # determine strand ratio category
-    df_trimmer_histogram.loc[:, STRAND_RATIO_CATEGORY_START] = df_trimmer_histogram[STRAND_RATIO_START].apply(
-        lambda x: get_annotation(x, sr_lower, sr_upper)
-    )
-    if A_HMER_END in df_trimmer_histogram.columns or T_HMER_END in df_trimmer_histogram.columns:
+    df_trimmer_histogram.loc[:, HistogramColumnNames.STRAND_RATIO_CATEGORY_START.value] = df_trimmer_histogram[
+        HistogramColumnNames.STRAND_RATIO_START.value
+    ].apply(lambda x: get_annotation(x, sr_lower, sr_upper))
+    if (
+        TrimmerSegmentLabels.A_HMER_END.value in df_trimmer_histogram.columns
+        or TrimmerSegmentLabels.T_HMER_END.value in df_trimmer_histogram.columns
+    ):
         # if only one of the end tags exists (maybe a small subsample) assign the other to 0
-        for c in (A_HMER_END, T_HMER_END):
+        for c in (TrimmerSegmentLabels.A_HMER_END.value, TrimmerSegmentLabels.T_HMER_END.value):
             if c not in df_trimmer_histogram.columns:
                 df_trimmer_histogram.loc[:, c] = 0
 
-        tags_sum_3 = df_trimmer_histogram[T_HMER_END] + df_trimmer_histogram[A_HMER_END]
-        df_trimmer_histogram.loc[:, STRAND_RATIO_END] = (
-            (df_trimmer_histogram[T_HMER_END] / (df_trimmer_histogram[T_HMER_END] + df_trimmer_histogram[A_HMER_END]))
+        tags_sum_3 = (
+            df_trimmer_histogram[TrimmerSegmentLabels.T_HMER_END.value]
+            + df_trimmer_histogram[TrimmerSegmentLabels.A_HMER_END.value]
+        )
+        df_trimmer_histogram.loc[:, HistogramColumnNames.STRAND_RATIO_END.value] = (
+            (
+                df_trimmer_histogram[TrimmerSegmentLabels.T_HMER_END.value]
+                / (
+                    df_trimmer_histogram[TrimmerSegmentLabels.T_HMER_END.value]
+                    + df_trimmer_histogram[TrimmerSegmentLabels.A_HMER_END.value]
+                )
+            )
             .where((tags_sum_3 >= min_total_hmer_lengths_in_tags) & (tags_sum_3 <= max_total_hmer_lengths_in_tags))
             .round(2)
         )
         # determine if end was reached - at least 1bp native adapter or all of the end stem were found
         is_end_reached = (
-            df_trimmer_histogram[NATIVE_ADAPTER] >= 1
-            if NATIVE_ADAPTER in df_trimmer_histogram.columns
-            else df_trimmer_histogram[STEM_END] >= 11
+            df_trimmer_histogram[TrimmerSegmentLabels.NATIVE_ADAPTER.value] >= 1
+            if TrimmerSegmentLabels.NATIVE_ADAPTER.value in df_trimmer_histogram.columns
+            else df_trimmer_histogram[TrimmerSegmentLabels.STEM_END.value] >= min_stem_end_matched_length
         )
         # determine strand ratio category
-        df_trimmer_histogram.loc[:, STRAND_RATIO_CATEGORY_END] = (
-            df_trimmer_histogram[STRAND_RATIO_END]
+        df_trimmer_histogram.loc[:, HistogramColumnNames.STRAND_RATIO_CATEGORY_END.value] = (
+            df_trimmer_histogram[HistogramColumnNames.STRAND_RATIO_END.value]
             .apply(lambda x: get_annotation(x, sr_lower, sr_upper))
-            .where(is_end_reached, END_UNREACHED)
+            .where(is_end_reached, BalancedCategories.END_UNREACHED.value)
         )
 
     # save to parquet
