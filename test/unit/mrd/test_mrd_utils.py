e52cdf31e2f8d6efe52f52e29554d3dc280d2777
doron <doron.shemtov@ultimagen.com>
2023-08-28 15:11:23 +0300

    trinuc_context_with_alt

--- test/unit/mrd/test_mrd_utils.py
+++ test/unit/mrd/test_mrd_utils.py
@@ -3,11 +3,13 @@ import subprocess
 import tempfile
 from os.path import join as pjoin
 from test import get_resource_dir, test_dir
+import pysam
+
 
 import pandas as pd
 from pandas.testing import assert_frame_equal
 
-from ugvc.mrd.featuremap_utils import FeaturemapAnnotator
+from ugvc.mrd.featuremap_utils import FeaturemapAnnotator, FeatureMapFields
 from ugvc.mrd.mrd_utils import (
     featuremap_to_dataframe,
     generate_synthetic_signatures,
@@ -23,18 +25,31 @@ reference_fasta = pjoin(general_inputs_dir, "chr1_head", "Homo_sapiens_assembly3
 intersection_file_basename = "MRD_test_subsample.MRD_test_subsample_annotated_AF_vcf_gz_mrd_quality_snvs.intersection"
 
 
-def test_FeaturemapAnnotator():
+def test_featuremap_annotator(tmpdir):
     input_featuremap = pjoin(inputs_dir, "Pa_46.bsDNA.chr20_sample.vcf.gz")
     expected_featuremap = pjoin(inputs_dir, "Pa_46.bsDNA.chr20_sample.annotated.vcf.gz")
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        tmp_out_path = pjoin(tmpdirname, "tmp_out.vcf.gz")
-        VcfAnnotator.process_vcf(
-            input_path=input_featuremap,
-            output_path=tmp_out_path,
-            annotators=[FeaturemapAnnotator()],
-        )
-        assert filecmp.cmp(tmp_out_path, expected_featuremap)
-
+    tmpfile = f'{tmpdir}/output_featuremap.vcf.gz'
+    VcfAnnotator.process_vcf(
+        input_path=input_featuremap,
+        output_path=tmpfile,
+        annotators=[FeaturemapAnnotator()],
+    )
+    output_variants = pysam.VariantFile(tmpfile)
+    forward_events = 0
+    reverse_events = 0
+    total_max_softclip_bases = 0
+    for v in output_variants:
+        assert FeatureMapFields.MAX_SOFTCLIP_LENGTH.value in v.info
+        total_max_softclip_bases += int(v.info[FeatureMapFields.MAX_SOFTCLIP_LENGTH.value])
+        if FeatureMapFields.MAX_SOFTCLIP_LENGTH.IS_FORWARD.value in v.info:
+            forward_events += 1
+        else:
+            reverse_events += 1
+    assert FeatureMapFields.IS_DUPLICATE.value in output_variants.header.info, \
+    f'{FeatureMapFields.IS_DUPLICATE.value} is not in info header {output_variants.header.info}'
+    assert forward_events == 9
+    assert reverse_events == 22
+    assert total_max_softclip_bases == 81
 
 def test_read_signature_ug_mutect():
     signature = read_signature(pjoin(inputs_dir, "mutect_mrd_signature_test.vcf.gz"))
@@ -62,21 +77,20 @@ def test_read_signature_external():
     assert_frame_equal(signature, expected_output)
 
 
-def test_intersect_featuremap_with_signature():
+def test_intersect_featuremap_with_signature(tmpdir):
     signature_file = pjoin(inputs_dir, "Pa_46.FreshFrozen.chr20.70039_70995.vcf.gz")
     featuremap_file = pjoin(inputs_dir, "Pa_46.bsDNA.chr20_sample.vcf.gz")
     test_file = pjoin(inputs_dir, "intersected_featuremap.vcf.gz")
 
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        output_intersection_file = pjoin(tmpdirname, "intersected.vcf.gz")
-        intersect_featuremap_with_signature(
-            featuremap_file,
-            signature_file,
-            output_intersection_file=output_intersection_file,
-        )
-        cmd1 = f"bcftools view -H {output_intersection_file}"
-        cmd2 = f"bcftools view -H {test_file}"
-        assert subprocess.check_output(cmd1, shell=True) == subprocess.check_output(cmd2, shell=True)
+    output_intersection_file = pjoin(tmpdir, "intersected.vcf.gz")
+    intersect_featuremap_with_signature(
+        featuremap_file,
+        signature_file,
+        output_intersection_file=output_intersection_file,
+    )
+    cmd1 = f"bcftools view -H {output_intersection_file}"
+    cmd2 = f"bcftools view -H {test_file}"
+    assert subprocess.check_output(cmd1, shell=True) == subprocess.check_output(cmd2, shell=True)
 
 
 def test_featuremap_to_dataframe():
@@ -93,13 +107,16 @@ def test_featuremap_to_dataframe():
 
 def test_read_intersection_dataframes():
     parsed_intersection_dataframe = read_intersection_dataframes(
-        pjoin(inputs_dir, f"{intersection_file_basename}.expected_output.parquet")
+        pjoin(inputs_dir, f"{intersection_file_basename}.expected_output.parquet"),
+        read_dataframes=True,
     )
     parsed_intersection_dataframe_expected = pd.read_parquet(
-        pjoin(inputs_dir, f"{intersection_file_basename}.parsed.expected_output.parquet")
+        pjoin(inputs_dir, f"{intersection_file_basename}.parsed.expected_output.parquet"),
+        read_dataframes=True,
     )
     parsed_intersection_dataframe2 = read_intersection_dataframes(
-        [pjoin(inputs_dir, f"{intersection_file_basename}.expected_output.parquet")]
+        [pjoin(inputs_dir, f"{intersection_file_basename}.expected_output.parquet")],
+        read_dataframes=True,
     )
     assert_frame_equal(
         parsed_intersection_dataframe,
@@ -111,21 +128,20 @@ def test_read_intersection_dataframes():
     )
 
 
-def test_generate_synthetic_signatures():
+def test_generate_synthetic_signatures(tmpdir):
     signature_file = pjoin(inputs_dir, "mutect_mrd_signature_test.vcf.gz")
     db_file = pjoin(
         inputs_dir,
         "pancan_pcawg_2020.mutations_hg38_GNOMAD_dbsnp_beds.sorted.Annotated.HMER_LEN.edited.chr19.vcf.gz",
     )
     n_synthetic_signatures = 1
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        output_dir = tmpdirname
-        synthetic_signature_list = generate_synthetic_signatures(
-            signature_file, db_file, n_synthetic_signatures, output_dir
-        )
-        output_file = synthetic_signature_list[0]
-        test_signautre = pjoin(inputs_dir, "synthetic_signature_test.vcf.gz")
-        # assert similar number of lines in output file as in input file
-        cmd1 = f"bcftools view -H {output_file}"
-        cmd2 = f"bcftools view -H {test_signautre}"
-        assert subprocess.check_output(cmd1, shell=True) == subprocess.check_output(cmd2, shell=True)
+    output_dir = tmpdir
+    synthetic_signature_list = generate_synthetic_signatures(
+        signature_file, db_file, n_synthetic_signatures, output_dir
+    )
+    output_file = synthetic_signature_list[0]
+    test_signautre = pjoin(inputs_dir, "synthetic_signature_test.vcf.gz")
+    # assert similar number of lines in output file as in input file
+    cmd1 = f"bcftools view -H {output_file}"
+    cmd2 = f"bcftools view -H {test_signautre}"
+    assert subprocess.check_output(cmd1, shell=True) == subprocess.check_output(cmd2, shell=True)
--- ugvc/mrd/srsnv_inference_utils.py
+++ ugvc/mrd/srsnv_inference_utils.py
@@ -1,21 +1,14 @@
 from __future__ import annotations
 
-import os
-import subprocess
-from os.path import join as pjoin
+import json
 
 import joblib
-import json
 import numpy as np
 import pandas as pd
-from matplotlib import pyplot as plt
-from os.path import basename, dirname, isfile, join as pjoin
-import subprocess
-import random
+import pysam
 
-from ugvc.dna.utils import revcomp
-from ugvc.utils.misc_utils import set_pyplot_defaults
-from ugvc.vcfbed.variant_annotation import get_cycle_skip_dataframe, get_motif_around, VcfAnnotator
+from ugvc.vcfbed.variant_annotation import VcfAnnotator
+
 
 class MLQualAnnotator(VcfAnnotator):
     """
@@ -42,7 +35,13 @@ class MLQualAnnotator(VcfAnnotator):
 
         """
         header.add_meta(
-            "INFO", items=[("ID", "ML_QUAL"), ("Number", 1), ("Type", "Float"), ("Description", "single-read-SNV model phred score")]
+            "INFO",
+            items=[
+                ("ID", "ML_QUAL"),
+                ("Number", 1),
+                ("Type", "Float"),
+                ("Description", "single-read-SNV model phred score"),
+            ],
         )
 
         return header
@@ -60,37 +59,34 @@ class MLQualAnnotator(VcfAnnotator):
         list[pysam.VariantRecord]
             list of updated VCF records
         """
-         # Convert list of variant records to a pandas DataFrame
+        # Convert list of variant records to a pandas DataFrame
         data = []
         features = self.model.feature_names_in_
         for variant in records:
-            d_rec = { feature: variant.info[feature] for feature in features }
+            d_rec = {feature: variant.info[feature] for feature in features}
             data.append(d_rec)
-        df = pd.DataFrame(data)
+        df = pd.DataFrame(data)[features]
 
         # Mark categorical features
-        features = features.astype({col: "category" for col in self.categorical_features})
+        df = df.astype({col: "category" for col in self.categorical_features})
 
         # Apply the provided model to assign a new quality value
-        predicted_qualities = model.predict_proba(features)
+        predicted_qualities = self.model.predict_proba(df)
 
         # Assign the new quality value to each variant record
         for i, variant in enumerate(records):
             variant.info["ML_QUAL"] = -10 * np.log10(predicted_qualities[i][0])
         return records
 
-def single_read_snv_inference(featuremap_path: str,
-                   params_path: str,
-                   model_path: str,
-                   out_path: str) -> None:
+
+def single_read_snv_inference(featuremap_path: str, params_path: str, model_path: str, out_path: str) -> None:
 
     # os.makedirs(os.dirname(out_path), exist_ok=True)
     model = joblib.load(model_path)
-    with open(params_path, 'r') as params_path:
-        params = json.load(params_path)
-    categorical_features = params['model_params']['categorical_features'] + params['model_params']['motif_only_features']
+    with open(params_path, "r", encoding="UTF-8") as p_fh:
+        params = json.load(p_fh)
+    categorical_features = params["categorical_features"]
     ml_qual_annotator = MLQualAnnotator(model, categorical_features)
-    VcfAnnotator.process_vcf(annotators=[ml_qual_annotator],
-                             input_path=featuremap_path,
-                             output_path=out_path,
-                             multiprocess_contigs=False)
+    VcfAnnotator.process_vcf(
+        annotators=[ml_qual_annotator], input_path=featuremap_path, output_path=out_path, multiprocess_contigs=False
+    )
