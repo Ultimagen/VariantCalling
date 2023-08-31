26008d5161679141ade63d22186ceb650be3b26a
rinas <rina.schwartz@ultimagen.com>
2023-08-31 11:26:53 +0000

    fix tests

--- ugvc/pipelines/mrd/bqsr_train.py
+++ ugvc/pipelines/mrd/bqsr_train.py
@@ -13,7 +13,7 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 # DESCRIPTION
-#    Calculate error rate per motif
+#    Train a model for single read SNV quality recalibration
 # CHANGELOG in reverse chronological order
 from __future__ import annotations
 
@@ -94,6 +94,7 @@ def parse_args(argv: list[str]) -> argparse.Namespace:
 
 
 def run(argv: list[str]):
+    """Train a model for single read SNV quality recalibration"""
 
     args = parse_args(argv)
     simple_pipeline_args = (0, 10000, False)
--- ugvc/pipelines/mrd/featuremap_to_dataframe.py
+++ ugvc/pipelines/mrd/featuremap_to_dataframe.py
@@ -93,16 +93,5 @@ def run(argv: list[str]):
     featuremap_to_dataframe(
         featuremap_vcf=args_in.input,
         output_file=args_in.output,
-<<<<<<< HEAD
         input_info_fields=args_in.info_fields,
-=======
-        reference_fasta=args_in.reference_fasta,
-        motif_length=args_in.motif_length,
-        info_fields_override=info_fields,
-        format_fields=format_fields,
-        report_bases_in_synthesis_direction=not args_in.report_bases_in_reference_direction,
-        show_progress_bar=args_in.show_progress_bar,
-        flow_order=args_in.flow_order,
-        is_matched=is_matched,
->>>>>>> parent c6dcac32a6b39b8fe568e22876ef2aeebf785648
     )
