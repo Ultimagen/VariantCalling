9efb12cba4029575a0eb71bf5330d31912c5553a
Ilya Soifer <ilya.soifer@ultimagen.com>
2023-09-19 06:17:17 +0000

    WIP

--- test/unit/filtering/test_training_prep.py
+++ test/unit/filtering/test_training_prep.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import pytest
 
 import ugvc.filtering.training_prep as tprep
 
@@ -9,3 +10,57 @@ def test_select_overlapping_variants():
     df = pd.DataFrame({"alleles": alleles, "pos": positions})
     result = tprep.select_overlapping_variants(df)
     assert result == [[1], [4, 5]]
+
+
+testdata = [[(0, 1), (1, 2), (0, 0)], [(1, 1), (1, 2), (0, 0)], [(2, 2), (1, 2), (1, 1)], [(1, 2), (1, 2), (0, 1)]]
+
+
+@pytest.mark.parametrize("orig_gt,allele_indices,expected", testdata)
+def test_encode_gt_for_allele_subset(orig_gt, allele_indices, expected):
+    assert tprep.encode_gt_for_allele_subset(orig_gt, allele_indices) == expected
+
+
+def test_encode_gt_for_allele_subset_with_assertion():
+    orig_gt = (0, 3)
+    allele_indices = (1, 2)
+    with pytest.raises(AssertionError):
+        tprep.encode_gt_for_allele_subset(orig_gt, allele_indices)
+
+
+testdata = [
+    [(0, 10, 20), (0, 1), (0, 10, 20)],
+    [(0, 10, 20, 40, 50, 60), (1, 2), (0, 30, 40)],
+    [(0, 10, 20, 40, 50, 60, 100, 120, 130, 140), (0, 3), (0, 100, 140)],
+]
+
+
+@pytest.mark.parametrize("orig_pl,allele_indices,expected", testdata)
+def test_select_pl_for_allele_subset(orig_pl, allele_indices, expected):
+    assert tprep.select_pl_for_allele_subset(orig_pl, allele_indices) == expected
+
+
+testdata = [
+    [("A", "G", "C"), (0, 1), False],
+    [("A", "AG", "C"), (0, 1), True],
+    [("A", "AG", "AC"), (1, 2), False],
+    [("A", "C", "AC"), (0, 2), True],
+]
+
+
+@pytest.mark.parametrize("alleles,allele_indices,expected", testdata)
+def test_is_indel_subset(alleles, allele_indices, expected):
+    assert tprep.is_indel_subset(alleles, allele_indices) == expected
+
+
+testdata = [
+    [("A", "G", "C"), (0, 1), ((None,), (None,))],
+    [("A", "AG", "C"), (0, 1), (("ins",), (1,))],
+    [("A", "AG", "AC"), (1, 2), ((None,), (None,))],
+    [("A", "C", "AC"), (0, 2), (("ins",), (1,))],
+    [("A", "AC", "C"), (1, 2), (("del",), (1,))],
+]
+
+
+@pytest.mark.parametrize("alleles,allele_indices,expected", testdata)
+def test_indel_classify_subset(alleles, allele_indices, expected):
+    assert tprep.indel_classify_subset(alleles, allele_indices) == expected
--- ugvc/filtering/training_prep.py
+++ ugvc/filtering/training_prep.py
@@ -2,9 +2,12 @@ from __future__ import annotations
 
 import numpy as np
 import pandas as pd
+import pyfaidx
 import tqdm.auto as tqdm
 
+import ugvc.comparison.flow_based_concordance as fbc
 import ugvc.comparison.vcf_pipeline_utils as vpu
+import ugvc.flow_format.flow_based_read as fbr
 from ugvc import logger
 from ugvc.vcfbed import vcftools
 
@@ -132,7 +135,7 @@ def decode_label(label):
     return decode_dct[label]
 
 
-def select_overlapping_variants(df: pd.DataFrame) -> list[list[int]]:
+def select_overlapping_variants(df: pd.DataFrame) -> list:
     """Selects lists of overlapping variants that need to be genotyped together. This
     can be multiallelic variants or variants with spanning deletion
 
@@ -152,6 +155,8 @@ def select_overlapping_variants(df: pd.DataFrame) -> list[list[int]]:
     results = []
     cluster = []
     for i in range(df.shape[0]):
+        if len(cluster) == 0 and del_length[i] == 0:
+            continue
         if df.iloc[i]["pos"] > current_span:
             if len(cluster) > 1:
                 for c in cluster:
@@ -168,5 +173,194 @@ def select_overlapping_variants(df: pd.DataFrame) -> list[list[int]]:
         results.append(cluster[:])
     for m in multiallelic_locations:
         results.append([m])
-    results = sorted(results)
-    return results
+    sorted_results = sorted(results)
+    return sorted_results
+
+
+def split_multiallelic_variants(multiallelic_variant: pd.Series) -> pd.DataFrame:
+    """Splits multiallelic variants into multiple rows
+
+    Parameters
+    ----------
+    multiallelic_variant : pd.Series
+        A row from the training set dataframe
+    Returns
+    -------
+    pd.DataFrame
+        A dataframe with the same columns as the training set dataframe
+    """
+    # record_to_nbr_dict = vcftools.header_record_number(call_vcf_header)
+    alleles = multiallelic_variant["alleles"]
+    pos = multiallelic_variant["pos"]
+    ref = multiallelic_variant["ref"]
+    alt = multiallelic_variant["alt"]
+    result = pd.DataFrame()
+    for i, a in enumerate(alleles):
+        if a == ref:
+            continue
+        if a == alt:
+            continue
+        result = result.append(multiallelic_variant)
+        result.iloc[-1]["alleles"] = [ref, a]
+        result.iloc[-1]["pos"] = pos + i
+    return result
+
+
+def encode_gt_for_allele_subset(original_gt: tuple, allele_idcs: tuple) -> tuple:
+    """Encodes GT according to the subsampled allele indices.
+    If only first allele is present in the original genotype: 0,0
+    If only second allele is present in the original genotype: 1,1
+    If both alleles are present: 0,1
+    Requires: at least one allele is present
+
+    Parameters
+    ----------
+    original_gt : tuple
+        Original genotype tuple
+    allele_idcs : tuple
+        Indices of alleles that are being selected
+
+    Returns
+    -------
+    tuple
+        Resulting genotype, at least one allele should exist in the orginal genotype
+
+    Raises
+    ------
+    RuntimeError
+        if neither allele is present in the original genotype
+    """
+    assert (
+        allele_idcs[0] in original_gt or allele_idcs[1] in original_gt
+    ), "One of the alleles should be present in the GT"
+    if allele_idcs[0] in original_gt and allele_idcs[1] not in original_gt:
+        return (0, 0)
+    if allele_idcs[1] in original_gt and allele_idcs[0] not in original_gt:
+        return (1, 1)
+    if allele_idcs[1] in original_gt and allele_idcs[0] in original_gt:
+        return (0, 1)
+    raise RuntimeError("Neither allele found in the original genotype")
+
+
+def _get_pl_idx(tup: tuple) -> int:
+    """Returns the index of the PL value in the tuple
+
+    Parameters
+    ----------
+    tup : tuple
+        PL tuple
+
+    Returns
+    -------
+    int
+        Index of the PL value
+    """
+    offset: int = max(tup) * (max(tup) + 1) // 2
+    return offset + int(min(tup))
+
+
+def select_pl_for_allele_subset(original_pl: tuple, allele_idcs: tuple) -> tuple:
+    """Selects Pl values according to the subsampled allele indices. Normalizes to the minimum
+
+    Parameters
+    ----------
+    original_pl : tuple
+        Original PL tuple
+    allele_idcs : tuple
+        Indices of alleles that are being selected
+
+    Returns
+    -------
+    tuple
+        Resulting genotype, at least one allele should exist in the orginal genotype
+    """
+    allele_idcs = tuple(sorted(allele_idcs))
+    idcs = [
+        _get_pl_idx(x)
+        for x in ((allele_idcs[0], allele_idcs[0]), (allele_idcs[0], allele_idcs[1]), (allele_idcs[1], allele_idcs[1]))
+    ]
+    pltake = [original_pl[x] for x in idcs]
+    min_pl = min(pltake)
+    return tuple(x - min_pl for x in pltake)
+
+
+def is_indel_subset(alleles: tuple, allele_indices: tuple) -> bool:
+    """Checks if the variant is an indel
+
+    Parameters
+    ----------
+    alleles : tuple
+        Alleles tuple
+    allele_indices : tuple
+        Indices of alleles that are being selected
+
+    Returns
+    -------
+    bool
+        True if indel, False otherwise
+    """
+    return any(len(alleles[x]) != len(alleles[allele_indices[0]]) for x in allele_indices)
+
+
+def indel_classify_subset(alleles: tuple, allele_indices: tuple) -> tuple[tuple[str | None], tuple[int | None]]:
+    """Checks if the variant is insertion or deletion
+
+    Parameters
+    ----------
+    alleles : tuple
+        Alleles tuple
+    allele_indices : tuple
+        Indices of alleles that are being selected
+
+    Returns
+    -------
+    tuple[str|None]
+        ('ins',) or ('del',)
+    """
+    if not is_indel_subset(alleles, allele_indices):
+        return ((None,), (None,))
+    ref_allele = alleles[allele_indices[0]]
+    alt_allele = alleles[allele_indices[1]]
+    if len(ref_allele) > len(alt_allele):
+        return (("del",), (len(ref_allele) - len(alt_allele),))
+    return (("ins",), (len(alt_allele) - len(ref_allele),))
+
+
+def classify_hmer_indel_relative(
+    alleles: tuple, allele_indices: tuple, ref: pyfaidx.Fasta, pos: int, flow_order: str = "TGCA"
+) -> tuple:
+    """Checks if one allele is hmer indel relative to the other
+
+    Parameters
+    ----------
+    alleles : tuple
+        Tuple of all alleles
+    allele_indices : tuple
+        Pair of allele indices, the second one is measured relative to the first one
+    ref : pyfaidx.Fasta
+        Reference sequence
+    pos: int
+        Position of the variant (one-based)
+    flow_order: str
+        Flow order, default is TGCA
+    Returns
+    -------
+    tuple
+        Pair of hmer indel length and hmer indel nucleotide
+    """
+    refstr = fbc.get_reference_from_region(ref, (pos - 10, pos + 10))
+    refstr = refstr.upper()
+    fake_series = pd.DataFrame(pd.Series({"alleles": alleles, "gt": allele_indices, "pos": pos, "ref": alleles[0]})).T
+    haplotypes = fbc.apply_variants_to_reference(refstr, fake_series, pos - 10, genotype_col="gt", include_ref=False)
+    fhaplotypes = [fbr.generate_key_from_sequence(x, flow_order) for x in haplotypes]
+    compare = fbc.compare_haplotypes(fhaplotypes[0:1], fhaplotypes[1:2])
+    if compare[0] != 1:
+        return (0, None)
+    flow_location = np.nonzero(fhaplotypes[0] - fhaplotypes[1])[0]
+    nucleotide = flow_order[flow_location[0] % 4]
+    length = min(int(fhaplotypes[0][flow_location]), int(fhaplotypes[1][flow_location]))
+    return (nucleotide, length)
+
+
+# special treatment
+# rpa
