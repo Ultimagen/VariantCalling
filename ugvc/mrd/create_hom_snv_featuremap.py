d0e59fa899e78b9656dd6008d88cc2786447ab33
Itai Rusinek <itai.rusinek@ultimagen.com>
2023-09-21 13:42:22 +0000

    Created a create_hom_snv_featuremap python pipeline, it does the same as the previous wdl task except the minimum coverage requirement that is now dynamic (the lower between the requested coverage and the median coverage)

---
+++ ugvc/mrd/create_hom_snv_featuremap.py
@@ -0,0 +1,104 @@
+import os
+
+from simppl.simple_pipeline import SimplePipeline
+
+from ugvc import logger
+from ugvc.mrd.featuremap_utils import FeatureMapFields
+from ugvc.utils.exec_utils import print_and_execute
+from ugvc.utils.metrics_utils import read_effective_coverage_from_sorter_json
+
+
+def create_hom_snv_featuremap(
+    featuremap: str,
+    sorter_stats_json: str = None,
+    hom_snv_featuremap: str = None,
+    sp: SimplePipeline = None,
+    requested_min_coverage: int = 20,
+    min_af: float = 0.7,
+):
+    """Create a HOM SNV featuremap from a featuremap
+
+    Parameters
+    ----------
+    featuremap : str
+        Input featuremap.
+    sorter_stats_json : str
+        Path to Sorter statistics JSON file, used to extract the median coverage. If None (default), minimum coverage
+        will be set to requested_min_coverage even if the median coverage is lower, might yield an empty output.
+    hom_snv_featuremap : str, optional
+        Output featuremap with HOM SNVs reads to be used as True Positives. If None (default),
+        the hom_snv_featuremap will be the same as the input featuremap with a ".hom_snv.vcf.gz" suffix.
+    sp : SimplePipeline, optional
+        SimplePipeline object to use for running commands. If None (default), commands will be run using subprocess.
+    requested_min_coverage : int, optional
+        Minimum coverage requested for locus to be propagated to the output. If the median coverage is lower than this
+        value, the median coverage will be used as the minimum coverage instead.
+        Default 20
+    min_af : float, optional
+        Minimum allele fraction in the featuremap to be considered a HOM SNV
+        Default 0.7
+    """
+
+    # check inputs
+    assert os.path.isfile(featuremap), f"featuremap {featuremap} does not exist"
+    if sorter_stats_json:
+        assert os.path.isfile(sorter_stats_json), f"sorter_stats_json {sorter_stats_json} does not exist"
+    if hom_snv_featuremap is None:
+        if featuremap.endswith(".vcf.gz"):
+            hom_snv_featuremap = featuremap[: -len(".vcf.gz")]
+        hom_snv_featuremap = featuremap + ".hom_snv.vcf.gz"
+    hom_snv_bed = hom_snv_featuremap.replace(".vcf.gz", ".bed.gz")
+    logger.info(f"Writting HOM SNV featuremap to {hom_snv_featuremap}")
+
+    # get minimum coverage
+    if sorter_stats_json:
+        (
+            _,
+            _,
+            _,
+            min_coverage,
+            _,
+        ) = read_effective_coverage_from_sorter_json(sorter_stats_json, min_coverage_for_fp=requested_min_coverage)
+    else:
+        min_coverage = requested_min_coverage
+    logger.info(
+        f"Using a minimum coverage of {min_coverage} for HOM SNV featuremap (requested {requested_min_coverage})"
+    )
+
+    # Create commands to filter the featuremap for homozygous SNVs.
+    cmd_get_hom_snv_loci_bed_file = (
+        # Use bcftools to query specific fields in the vcf file. This includes the chromosome (CHROM),
+        # the 0-based start position (POS0), the 1-based start position (POS), and the number of reads
+        # in the locus (X_READ_COUNT) for the specified feature map.
+        f"bcftools query -f '%CHROM\t%POS0\t%POS\t%INFO/{FeatureMapFields.READ_COUNT.value}\n' {featuremap} |"
+        # Pipe the output to bedtools groupby command.
+        # Here, -c 3 means we are specifying the third column as the key to groupby.
+        # The '-full' option includes all columns from the input in the output.
+        # The '-o count' option is specifying to count the number of lines for each group.
+        f"bedtools groupby -c 3 -full -o count | "
+        # Pipe the result to an awk command, which filters the result based on minimum coverage and allele frequency.
+        # The '$4>=~{min_coverage}' part checks if the fourth column (which should be read count) is greater than or
+        # equal to the minimum coverage. The '$5/$4>=~{min_af}' part checks if the allele frequency (calculated as
+        # column 5 divided by column 4) is greater than or equal to the minimum allele frequency.
+        f"awk '($4>={min_coverage})&&($5/$4>={min_af})' | "
+        # The final output is then compressed and saved to the specified location in .bed.gz format.
+        f"gzip > {hom_snv_bed}"
+    )
+    cmd_intersect_bed_file_with_original_featuremap = (
+        f"bedtools intersect -a {featuremap} -b {hom_snv_bed} -u -header | bcftools view - -Oz -o {hom_snv_featuremap}"
+    )
+    cmd_index_hom_snv_featuremap = f"bcftools index -ft {hom_snv_featuremap}"
+
+    # Run the commands
+    try:
+        for command in (
+            cmd_get_hom_snv_loci_bed_file,
+            cmd_intersect_bed_file_with_original_featuremap,
+            cmd_index_hom_snv_featuremap,
+        ):
+            print_and_execute(command, simple_pipeline=sp, module_name=__name__)
+
+    finally:
+        # remove temp file
+        if os.path.isfile(hom_snv_bed):
+            os.remove(hom_snv_bed)
---
+++ ugvc/pipelines/mrd/create_hom_snv_featuremap.py
@@ -0,0 +1,81 @@
+#!/env/python
+# Copyright 2023 Ultima Genomics Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+# DESCRIPTION
+#    Create a HOM SNV featuremap from a featuremap
+# CHANGELOG in reverse chronological order
+from __future__ import annotations
+
+import argparse
+
+from simppl.simple_pipeline import SimplePipeline
+
+from ugvc.mrd.create_hom_snv_featuremap import create_hom_snv_featuremap
+
+
+def get_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(prog="create_hom_snv_featuremap.py", description=run.__doc__)
+    parser.add_argument(
+        "--featuremap",
+        type=str,
+        required=True,
+        help="""Input featuremap""",
+    )
+    parser.add_argument(
+        "--sorter_stats_json",
+        type=str,
+        default=None,
+        help="Path to Sorter statistics JSON file. If None, the minimum coverage "
+        "will be set to requested_min_coverage even if the median coverage is lower, might yield an empty output.",
+    )
+    parser.add_argument(
+        "--hom_snv_featuremap",
+        type=str,
+        default=None,
+        help="Output featuremap with HOM SNVs reads to be used as True Positives. If None (default), "
+        "the hom_snv_featuremap will be the same as the input featuremap with a '.hom_snv.vcf.gz' suffix.",
+    )
+    parser.add_argument(
+        "--requested_min_coverage",
+        type=int,
+        default=20,
+        help="Minimum coverage requested for locus to be propagated to the output. If the median coverage "
+        "is lower than this value, the median coverage will be used as the minimum coverage instead.",
+    )
+    parser.add_argument(
+        "--min_af",
+        type=float,
+        default=0.7,
+        help="""Minimum allele fraction in the featuremap to be considered a HOM SNV""",
+    )
+
+    return parser
+
+
+def run(argv: list[str]):
+    """Create a HOM SNV featuremap from a featuremap"""
+    print(argv)
+    parser = get_parser()
+    SimplePipeline.add_parse_args(parser)
+    args = parser.parse_args(argv[1:])
+    sp = SimplePipeline(args.fc, args.lc, debug=args.d, print_timing=True)
+
+    create_hom_snv_featuremap(
+        featuremap=args.featuremap,
+        sorter_stats_json=args.sorter_stats_json,
+        hom_snv_featuremap=args.hom_snv_featuremap,
+        sp=sp,
+        requested_min_coverage=args.requested_min_coverage,
+        min_af=args.min_af,
+    )
