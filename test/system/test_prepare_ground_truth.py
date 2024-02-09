5a75c9a759233086ccb7b4252d8dd3b82ada68c1
Ilya Soifer <ilya.soifer@ultimagen.com>
2024-02-09 21:17:37 +0000

    docs

--- test/system/test_prepare_ground_truth.py
+++ test/system/test_prepare_ground_truth.py
@@ -10,10 +10,12 @@ def test_prepare_ground_truth(mocker, tmpdir):
     inputs_dir = get_resource_dir(__file__)
     calls_file = str(pathlib.Path(inputs_dir, "input.vcf.gz"))
     vcfeval_output_file = pathlib.Path(inputs_dir, "vcfeval_output.vcf.gz")
+    # bypass well-tested run_vcfeval_concordance
     mocker.patch(
         "ugvc.comparison.vcf_pipeline_utils.VcfPipelineUtils.run_vcfeval_concordance", return_value=vcfeval_output_file
     )
     joint_df = pd.read_hdf(str(pathlib.Path(inputs_dir, "labeled_df.h5")), key="df")
+    # bypass creating of the joint vcf (good unit test)
     mocker.patch("ugvc.filtering.training_prep.calculate_labeled_vcf", return_value=joint_df)
     reference_path = str(pathlib.Path(inputs_dir, "ref_fragment.fa.gz"))
     output_file = str(pathlib.Path(tmpdir, "output.h5"))
