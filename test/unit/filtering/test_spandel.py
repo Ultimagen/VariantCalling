f34df1efedde7e4d08a17710980e46c6c196d06a
Ilya Soifer <ilya.soifer@ultimagen.com>
2024-02-09 06:55:37 +0000

    Another test_spandel added

--- test/unit/filtering/test_spandel.py
+++ test/unit/filtering/test_spandel.py
@@ -2,7 +2,9 @@ import pathlib
 import pickle
 from test import get_resource_dir
 
+import pandas as pd
 import pyfaidx
+import pysam
 
 import ugvc.filtering.spandel as spandel
 
@@ -25,4 +27,31 @@ def test_extract_allele_subset_from_multiallelic_spanning_deletion():
     ]
     assert len(results) == len(expected_results)
     for i in range(len(results)):
-        assert results[i].equals(expected_results[i])
+        pd.testing.assert_series_equal(results[i], expected_results[i])
+
+
+def test_split_multiallelic_variants_with_spandel():
+    inputs_dir = get_resource_dir(__file__)
+    reference = pyfaidx.Fasta(str(pathlib.Path(inputs_dir, "ref_fragment.fa.gz")))
+    inputs_file = pathlib.Path(inputs_dir, "spanning_deletions.pkl")
+    expected_result_file = pathlib.Path(inputs_dir, "expected_result_split_multiallelic.pkl")
+    vcf_header = pysam.VariantFile(str(pathlib.Path(inputs_dir, "test_header.vcf.gz"))).header
+    with open(inputs_file, "rb") as f:
+        inputs, _ = pickle.load(f)
+
+    with open(expected_result_file, "rb") as f:
+        expected_results = pickle.load(f)
+
+    results = [
+        spandel.split_multiallelic_variants_with_spandel(
+            input.iloc[1],
+            input.iloc[0],
+            vcf_header,
+            reference["chr21"],
+        )
+        for input in inputs
+    ]
+
+    assert len(results) == len(expected_results)
+    for i in range(len(results)):
+        pd.testing.assert_frame_equal(results[i], expected_results[i])
