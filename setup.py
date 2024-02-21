3031a0614833ad5ad26114d7e0c623bddff0a06d
Ilya Soifer <ilya.soifer@ultimagen.com>
2024-02-21 13:20:11 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="ugvc",
-    version="0.19",
+    version="0.20",
     packages=find_packages(),
     install_requires=[],
     scripts=[
