01dc9d0c1573e622334db53a61d5879f9fdf2875
Ilya Soifer <ilya.soifer@ultimagen.com>
2024-03-15 16:43:57 +0000

    (C) updated

--- ugvc/pipelines/training_prep_pipeline.py
+++ ugvc/pipelines/training_prep_pipeline.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright 2022 Ultima Genomics Inc.
+# Copyright 2024 Ultima Genomics Inc.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
