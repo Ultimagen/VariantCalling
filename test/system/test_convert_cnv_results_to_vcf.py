f92be08536bcbafc215d86b34ea93ea5ab5d5b48
TammushB <tammy.biniashvili@ultimagen.com>
2024-05-21 12:00:33 +0000

    add test

---
+++ test/system/test_convert_cnv_results_to_vcf.py
@@ -0,0 +1,38 @@
+import filecmp
+import os
+from os.path import join as pjoin
+from os.path import dirname
+
+import warnings
+warnings.filterwarnings('ignore')
+
+from test import get_resource_dir, test_dir
+
+from ugvc.cnv import convert_cnv_results_to_vcf
+
+class TestConvertCnvResultsToVcf:
+    inputs_dir = get_resource_dir(__file__)
+
+    def test_convert_cnv_results_to_vcf(self, tmpdir):
+        input_bed_file = pjoin(self.inputs_dir, "EL-0059.cnvs.annotate.bed")
+        genome_file = pjoin(self.inputs_dir, "Homo_sapiens_assembly38.chr1-24.genome")
+        expected_out_vcf = pjoin(self.inputs_dir,'expected_EL-0059.cnv.vcf')
+        
+        sample_name = 'EL-0059'
+        out_dir = f"{tmpdir}"
+        convert_cnv_results_to_vcf.run([
+            "convert_cnv_results_to_vcf",
+            "--cnv_annotated_bed_file",
+            input_bed_file,
+            "--out_directory",
+            out_dir,
+            "--sample_name",
+            sample_name,
+            "--ref_genome_file",
+            genome_file
+            ])
+        
+        out_vcf_file = pjoin(tmpdir,sample_name+'.cnv.vcf')
+        
+        assert filecmp.cmp(out_vcf_file, expected_out_vcf)
+        
