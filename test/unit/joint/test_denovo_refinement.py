c658044da4feb53ccfeeb87c09e2d66875c2f012
Ilya Soifer <ilya.soifer@ultimagen.com>
2024-06-21 18:36:20 +0000

    Unit tests done

--- test/unit/joint/test_denovo_refinement.py
+++ test/unit/joint/test_denovo_refinement.py
@@ -2,8 +2,13 @@ from os.path import join as pjoin
 from test import get_resource_dir
 
 import pandas as pd
+import pysam
 
-from ugvc.joint.denovo_refinement import add_paternal_qualities_to_denovo_vcf, get_parental_vcf_df
+from ugvc.joint.denovo_refinement import (
+    add_paternal_qualities_to_denovo_vcf,
+    get_parental_vcf_df,
+    write_recalibrated_vcf,
+)
 
 
 def test_get_parental_vcf_df():
@@ -25,3 +30,17 @@ def test_add_paternal_qualities_to_denovo_vcf():
     df = add_paternal_qualities_to_denovo_vcf(denovo_vcf, parental_vcf_df)
     expected = pd.read_hdf(pjoin(datadir, "denovo_vcf_with_qual.h5"), key="df")
     assert df.equals(expected)
+
+
+def test_write_recalibrated_vcf(tmpdir):
+    datadir = get_resource_dir(__file__)
+    recal_df = pd.DataFrame(pd.read_hdf(pjoin(datadir, "denovo_vcf_with_qual.h5"), key="df"))
+    denovo_vcf = pjoin(datadir, "denovo.vcf.gz")
+    output_fname = pjoin(tmpdir, "denovo.recal.vcf.gz")
+    write_recalibrated_vcf(denovo_vcf, output_fname, recal_df)
+    with pysam.VariantFile(pjoin(tmpdir, "denovo.recal.vcf.gz")) as infile:
+        count = 0
+        for r in infile:
+            if "DENOVO_QUAL" in r.info:
+                count += 1
+    assert count == 26
