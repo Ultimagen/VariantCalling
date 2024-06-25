5602c442c40e1aef05d8e99239f61a1197b44baf
GatKrieger <gat.krieger@ultimagen.com>
2024-06-25 14:09:26 +0000

    code revisit after Itai's comments

--- ugvc/mrd/featuremap_consensus_utils.py
+++ ugvc/mrd/featuremap_consensus_utils.py
@@ -1,58 +1,140 @@
 from __future__ import annotations
 
 from collections import defaultdict
-from os.path import basename
-from os.path import join as pjoin
 
 import numpy as np
 import pysam
 
+from ugvc.dna.format import AD, DP, GT, VAF
+from ugvc.mrd.featuremap_utils import FeatureMapFields
+
+fields_to_collect = {
+    "numeric_aggregated_fields": [FeatureMapFields.FILTERED_COUNT.value],
+    "numeric_array_fields": [
+        # FeatureMapFields.X_QUAL.value,
+        FeatureMapFields.X_SCORE.value,
+        FeatureMapFields.X_EDIST.value,
+        FeatureMapFields.X_LENGTH.value,
+        FeatureMapFields.X_MAPQ.value,
+        FeatureMapFields.X_INDEX.value,
+        FeatureMapFields.X_FC1.value,
+        FeatureMapFields.X_FC2.value,
+        FeatureMapFields.MAX_SOFTCLIP_LENGTH.value,
+        FeatureMapFields.X_FLAGS.value,
+        # "a3",
+        # "rq"
+    ],
+    "string_list_fields": [
+        FeatureMapFields.X_RN.value,
+        FeatureMapFields.X_CIGAR.value,
+        "rq"
+        # FeatureMapFields.IS_FORWARD.value,
+        # FeatureMapFields.IS_DUPLICATE.value,
+        # "tm"
+    ],
+    "fields_to_write_once": [
+        FeatureMapFields.READ_COUNT.value,
+        FeatureMapFields.TRINUC_CONTEXT_WITH_ALT.value,
+        FeatureMapFields.HMER_CONTEXT_REF.value,
+        FeatureMapFields.HMER_CONTEXT_ALT.value,
+        # FeatureMapFields.IS_CYCLE_SKIP.value,
+        FeatureMapFields.PREV_1.value,
+        FeatureMapFields.PREV_2.value,
+        FeatureMapFields.PREV_3.value,
+        FeatureMapFields.NEXT_1.value,
+        FeatureMapFields.NEXT_2.value,
+        FeatureMapFields.NEXT_3.value,
+    ],
+}
+
 
 def write_a_pileup_record(
-    record_dict: dict, rec_id: str, out_fh: pysam.VariantFile, header: pysam.VariantHeader, min_qual: int
+    record_dict: dict,
+    rec_id: str,
+    out_fh: pysam.VariantFile,
+    header: pysam.VariantHeader,
+    min_qual: int,
+    qual_agg_func: callable = np.max,
+    sample_name: str = "SAMPLE",
 ):
-    info_fields = [
-        "X_QUAL",
-        "X_RN",
-    ]
+    """
+    Write a pileup record to a vcf file
 
-    format_fields = ["GT", "AD", "DP", "VAF"]
+    Inputs:
+        record_dict (dict): A dictionary containing the record information
+        rec_id (str): The record id
+        out_fh (pysam.VariantFile): A pysam file handle of the output vcf file
+        header (pysam.VariantHeader): A pysam VarianyHeader pbject of the output vcf file
+        min_qual (int): The minimum quality threshold
+        qual_agg_func (function): The function to aggregate the quality scores (default: np.max)
+        sample_name (str): The name of the sample (default: "SAMPLE")
+    Output:
+        rec (pysam.VariantRecord): A pysam VariantRecord object
+
+    """
+    format_fields = [GT, AD, DP, VAF]
 
     rec = header.new_record()
-    rec.chrom = rec_id.split("-")[0]
-    rec.pos = int(rec_id.split("-")[1])
-    rec.ref = rec_id.split("-")[2]
-    rec.alts = [rec_id.split("-")[3]]
+    rec.chrom = rec_id[0]
+    rec.pos = rec_id[1]
+    rec.ref = rec_id[2]
+    rec.alts = rec_id[3]
     rec.id = "."
-    rec.qual = np.mean(record_dict["X_QUAL"])
-    record_dict["X_RN"] = "|".join(record_dict["X_RN"])
-    curr_filter = "PASS" if rec.qual >= min_qual else "FAIL"
-    record_dict["VAF"] = record_dict["FILTERED_COUNT"] / record_dict["DP"]
-    rec.filter.add(curr_filter)
+    rec.qual = qual_agg_func(record_dict[FeatureMapFields.X_QUAL.value])
+    # INFO fields to aggregate
+    # exceptions: X_QUAL
+    rec.info[FeatureMapFields.X_QUAL.value] = list(record_dict[FeatureMapFields.X_QUAL.value])
+    for field in fields_to_collect["numeric_array_fields"]:
+        if field in record_dict:
+            rec.info[field] = list(record_dict[field])
+    for field in fields_to_collect["string_list_fields"]:
+        if field in record_dict:
+            rec.info[field] = "|".join(record_dict[field])
 
-    for info_field in info_fields:
-        rec.info[info_field] = record_dict[info_field]
-
-    record_dict["GT"] = (0, 1)
-    record_dict["AD"] = [record_dict["FILTERED_COUNT"], record_dict["DP"] - record_dict["FILTERED_COUNT"]]
+    # FORMAT fields to aggregate
+    record_dict[VAF] = record_dict[FeatureMapFields.FILTERED_COUNT.value] / record_dict[DP]
+    record_dict[GT] = (0, 1)
+    record_dict[AD] = [
+        record_dict[DP] - record_dict[FeatureMapFields.FILTERED_COUNT.value],
+        record_dict[FeatureMapFields.FILTERED_COUNT.value],
+    ]
     for format_field in format_fields:
-        rec.samples["SAMPLE"][format_field] = record_dict[format_field]
-
+        rec.samples[sample_name][format_field] = record_dict[format_field]
+    # FILTER field
+    if rec.qual < min_qual:
+        curr_filter = "LowQual"
+    elif rec.qual >= min_qual and record_dict[FeatureMapFields.FILTERED_COUNT.value] == 1:
+        curr_filter = "SingleRead"
+    else:
+        curr_filter = "PASS"
+    rec.filter.add(curr_filter)
+    # write to file
     out_fh.write(rec)
     return rec
 
 
-def pileup_featuremap(featuremap: str, output_dir: str, genomic_interval: str, min_qual: int):
+def pileup_featuremap(
+    featuremap: str, output_vcf: str, genomic_interval: str = None, min_qual: int = 0, sample_name: str = "SAMPLE"
+):
     """
     Pileup featuremap vcf to a reagular vcf, save the mean quality and read count
+
+    Inputs:
+        featuremap (str): The input featuremap vcf file
+        output_vcf (str): The output pileup vcf file
+        genomic_interval (str): The genomic interval to pileup, format: chr:start-end
+        min_qual (int): The minimum quality threshold
+        sample_name (str): The name of the sample (default: "SAMPLE")
+    Output:
+        output_vcf (str): The output vcf file
     """
     cons_dict = defaultdict(dict)
     # process genomic interval input
-    if genomic_interval == "":
+    if genomic_interval is None:
         chrom = None
         start = None
         end = None
-    if genomic_interval != "":
+    if genomic_interval is not None:
         genomic_interval_list = genomic_interval.split(":")
         chrom = genomic_interval_list[0]
         start = int(genomic_interval_list[1].split("-")[0])
@@ -62,49 +144,67 @@ def pileup_featuremap(featuremap: str, output_dir: str, genomic_interval: str, m
     orig_header = pysam.VariantFile(featuremap).header
     header = pysam.VariantHeader()
     header.add_meta("fileformat", "VCFv4.2")
-    header.filters.add("FAIL", None, None, "Mean quality below threshold")
-    header.info.add(
-        "FILTERED_COUNT",
-        1,
-        "Integer",
-        "Number of reads containing this location that agree with alternative according to fitler",
+    # header.filters.add("PASS", None, None, "Aggregated quality above threshold, more than a single alternative read")
+    header.filters.add(
+        "SingleRead", None, None, "Aggregated quality above threshold, Only a single read agrees with alternative"
     )
-    header.info.add("X_READ_COUNT", 1, "Integer", "Number of reads containing this location")
-    header.info.add("X_QUAL", ".", "Float", "Quality of reads containing this location")
-    header.info.add("X_RN", 1, "String", "Read Name of reads containing this location, entries spearated by |")
-    header.info.add("X_AF", 1, "Float", "Allele frequency")
-    header.formats.add("GT", 1, "String", "Genotype")
-    header.formats.add("DP", 1, "Integer", "Read depth")
-    header.formats.add("AD", "R", "Integer", "Allelic depths")
-    header.formats.add("VAF", 1, "Float", "Allele frequency")
+    header.filters.add("LowQual", None, None, "Aggregated quality below threshold")
+    for field in fields_to_collect["numeric_aggregated_fields"]:
+        header.info.add(
+            field, orig_header.info[field].number, orig_header.info[field].type, orig_header.info[field].description
+        )
+    for field in fields_to_collect["numeric_array_fields"]:
+        header.info.add(field, ".", orig_header.info[field].type, orig_header.info[field].description)
+    for field in fields_to_collect["string_list_fields"]:
+        header.info.add(field, "1", orig_header.info[field].type, orig_header.info[field].description)
+    # exceptions: X_QUAL
+    header.info.add(FeatureMapFields.X_QUAL.value, ".", "Float", "Quality of reads containing this location")
+    header.formats.add(GT, 1, "String", "Genotype")
+    header.formats.add(DP, 1, "Integer", "Read depth")
+    header.formats.add(AD, "R", "Integer", "Allelic depths")
+    header.formats.add(VAF, 1, "Float", "Allele frequency")
     # copy contigs from original header
     for contig in orig_header.contigs:
         header.contigs.add(contig)
-    header.add_sample("SAMPLE")
+    header.add_sample(sample_name)
 
     # open an output file
-    output_vcf = pjoin(
-        output_dir, basename(featuremap).replace(".vcf.gz", f".{genomic_interval.replace(':', '-')}.pileup.vcf.gz")
-    )
     out_fh = pysam.VariantFile(output_vcf, "w", header=header)
 
     cons_dict = defaultdict(dict)
     with pysam.VariantFile(featuremap) as f:
+        prev_key = tuple()
         for rec in f.fetch(chrom, start, end):
-            rec_id = "-".join([str(x) for x in (rec.chrom, rec.pos, rec.ref, rec.alts[0])])
-            if "FILTERED_COUNT" not in cons_dict[rec_id]:
+            rec_id = (rec.chrom, rec.pos, rec.ref, rec.alts[0])
+            if FeatureMapFields.FILTERED_COUNT.value not in cons_dict[rec_id]:
                 if len(cons_dict.keys()) > 1:
                     # write to file
-                    prev_key = list(cons_dict.keys())[0]
                     write_a_pileup_record(cons_dict[prev_key], prev_key, out_fh, header, min_qual)
                     cons_dict.pop(prev_key)
-                cons_dict[rec_id]["FILTERED_COUNT"] = 0
-                cons_dict[rec_id]["X_QUAL"] = []
-                cons_dict[rec_id]["X_RN"] = []
-            cons_dict[rec_id]["FILTERED_COUNT"] += 1
-            cons_dict[rec_id]["X_QUAL"] += [rec.qual]
-            cons_dict[rec_id]["X_RN"] += [rec.info["X_RN"]]
-            cons_dict[rec_id]["DP"] = rec.info["X_READ_COUNT"]
+                # initialize the record
+                for field in fields_to_collect["numeric_aggregated_fields"]:
+                    cons_dict[rec_id][field] = 0
+                # exceptions: X_QUAL
+                cons_dict[rec_id]["X_QUAL"] = np.array([])
+                for field in fields_to_collect["numeric_array_fields"]:
+                    cons_dict[rec_id][field] = np.array([])
+                for field in fields_to_collect["string_list_fields"]:
+                    cons_dict[rec_id][field] = []
+            # update the record
+            for field in fields_to_collect["numeric_aggregated_fields"]:
+                cons_dict[rec_id][field] += 1
+            # exceptions: X_QUAL
+            cons_dict[rec_id]["X_QUAL"] = np.append(cons_dict[rec_id]["X_QUAL"], rec.qual)
+            for field in fields_to_collect["numeric_array_fields"]:
+                cons_dict[rec_id][field] = np.append(cons_dict[rec_id][field], rec.info[field])
+            for field in fields_to_collect["string_list_fields"]:
+                cons_dict[rec_id][field] += [rec.info[field]]
+            for field in fields_to_collect["fields_to_write_once"]:
+                cons_dict[rec_id][field] = rec.info[field]
+            cons_dict[rec_id][DP] = rec.info[FeatureMapFields.READ_COUNT.value]
+            prev_key = rec_id
+        # write last record
+        write_a_pileup_record(cons_dict[prev_key], prev_key, out_fh, header, min_qual)
 
     out_fh.close()
     pysam.tabix_index(output_vcf, preset="vcf", min_shift=0, force=True)
