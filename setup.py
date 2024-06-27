0b6c2f3a894559e81cc79b1b57032b214c53628b
Ilya Soifer <ilya.soifer@ultimagen.com>
2024-06-27 06:22:10 +0000

    version to 0.22

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="ugvc",
-    version="0.21",
+    version="0.22",
     packages=find_packages(),
     install_requires=[],
     scripts=[
