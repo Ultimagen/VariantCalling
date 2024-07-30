abf407f7a6c7b671c6ef0f199654008aeb02b613
Ilya Soifer <ilya.soifer@ultimagen.com>
2024-07-30 08:42:04 +0000

    Added cleanup and ability to cross with calling regions

--- ugvc/pipelines/vcfbed/gvcf_hcr_main.py
+++ ugvc/pipelines/vcfbed/gvcf_hcr_main.py
@@ -6,6 +6,7 @@ import tempfile
 import pybedtools
 
 import ugvc.vcfbed.manipulate_bed as mb
+from ugvc.vcfbed.interval_file import IntervalFile
 
 
 def argparse_gvcf_to_bed() -> argparse.ArgumentParser:
@@ -14,6 +15,7 @@ def argparse_gvcf_to_bed() -> argparse.ArgumentParser:
     parser.add_argument("--bed", type=str, required=True, help="Path to the output bed file.")
     parser.add_argument("--gq_threshold", type=int, default=20, help="GQ threshold.")
     parser.add_argument("--genome_file", type=str, required=True, help="Path to the .sizes file of the genome")
+    parser.add_argument("--calling_region", type=str, required=False, help="Calling regions if not whole genome")
     return parser
 
 
@@ -27,9 +29,24 @@ def run(argc: list) -> None:
         mb.gvcf_to_bed(args.gvcf, step1_file, args.gq_threshold, gt=False)
         logger.info("Step 2: Merge intervals of low confidence")
         step2_file = os.path.join(tmp_dir, "merged_low_confidence.bed")
-        pybedtools.BedTool(step1_file).merge().saveas(step2_file)  # type: ignore - issues in pybedtools in vscode
+        bt = pybedtools.BedTool(step1_file)
+        bt = bt.merge().saveas(step2_file)  # type: ignore - issues in pybedtools in vscode
+        bt.delete_temporary_history(ask=False)
         logger.info("Step 3: Create intervals of high confidence")
-        step3_file = args.bed
-        pybedtools.BedTool(step2_file).complement(g=args.genome_file).saveas(  # type: ignore
+
+        if not args.calling_region:
+            step3_file = args.bed
+        else:
+            step3_file = os.path.join(tmp_dir, "high_confidence.bed")
+        bt = pybedtools.BedTool(step2_file)
+        bt = bt.complement(g=args.genome_file).saveas(  # type: ignore
             step3_file
         )  # type: ignore - issues in pybedtools in vscode
+        bt.delete_temporary_history(ask=False)
+        if args.calling_region:
+            intv = IntervalFile(interval=args.calling_region)
+            step4_file = args.bed
+            logger.info("Step 4: Intersect with calling region")
+            bt = pybedtools.BedTool(step3_file)
+            bt = bt.intersect(b=intv.as_bed_file()).saveas(step4_file)
+            bt.delete_temporary_history(ask=False)
