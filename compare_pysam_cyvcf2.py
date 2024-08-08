b072b7a0f36dc6a240bd5979d7c63e5ba1c01a60
Inbal Zelig <inbal.zelig@ultimagen.com>
2024-08-08 15:06:57 +0000

    Add annotations and parametrize

--- compare_pysam_cyvcf2.py
+++ compare_pysam_cyvcf2.py
@@ -1,3 +1,4 @@
+import sys
 import time
 
 import pysam
@@ -7,33 +8,35 @@ from profiling import create_annotation
 vcf_in = "/data/Runs/call-FeatureMapProcess/input/Pa_46_333_LuNgs_08.vcf.gz"
 # contig = "chr20"
 
-create_annotation("start")
-time.sleep(10)
 
-#Cyvcf2 code
-start_time = time.time()
+if "cyvcf2" in sys.argv:
+    create_annotation("cyvcf2")
+    start_time = time.time()
 
-for _ in range(10):
-    records = 0
-    for variant in VCF(vcf_in, threads=2):
-        records += 1
-
-print(f"cyvcf2 time: {time.time() - start_time}")
-print(f"cyvcf2 records: {records}")
-create_annotation("cyvcf2", time_start=start_time)
-
-time.sleep(30)
-
-#Pysam code
-start_time = time.time()
-
-for _ in range(10):
-    records = 0
-
-    with pysam.VariantFile(vcf_in) as input_variant_file:
-        for record in input_variant_file:
+    for _ in range(10):
+        records = 0
+        for variant in VCF(vcf_in): #, threads=2):
             records += 1
 
-print(f"pysam time: {time.time() - start_time}")
-print(f"pysam records: {records}")
-create_annotation("pysam", time_start=start_time)
+    duration = time.time() - start_time
+    print(f"cyvcf2 {duration=}")
+    print(f"cyvcf2 {records=}")
+    create_annotation(f"cyvcf2: {int(duration)}s", time_start=start_time)
+
+    time.sleep(30)
+
+if "pysam" in sys.argv:
+    create_annotation("pysam")
+    start_time = time.time()
+
+    for _ in range(10):
+        records = 0
+
+        with pysam.VariantFile(vcf_in) as input_variant_file:
+            for record in input_variant_file:
+                records += 1
+
+    duration = time.time() - start_time
+    print(f"pysam {duration=}")
+    print(f"pysam {records=}")
+    create_annotation(f"pysam: {int(duration)}s", time_start=start_time)
