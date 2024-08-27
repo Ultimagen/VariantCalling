31afea2eb7a4844f3abf2cfc7d370b333c60a08c
avigailmo <avigail.moldovan@ultimagen.com>
2024-08-27 14:01:51 +0300

    reuse internal bed_writer

--- ugvc/pipelines/coverage_analysis.py
+++ ugvc/pipelines/coverage_analysis.py
@@ -53,7 +53,7 @@ from ugvc.utils.cloud_sync import cloud_sync
 from ugvc.utils.consts import COVERAGE, GCS_OAUTH_TOKEN, FileExtension
 from ugvc.utils.misc_utils import set_pyplot_defaults
 from ugvc.utils.plotting_utils import set_default_plt_rc_params
-from ugbio_core.bed_writer import BED_COLUMN_CHROM, BED_COLUMN_CHROM_END, BED_COLUMN_CHROM_START, parse_intervals_file
+from ugvc.vcfbed.bed_writer import BED_COLUMN_CHROM, BED_COLUMN_CHROM_END, BED_COLUMN_CHROM_START, parse_intervals_file
 
 MIN_CONTIG_LENGTH = 1000000  # contigs that are shorter than that won't be analyzed
 MIN_LENGTH_TO_SHOW = 10000000  # contigs that are shorter than that won't be shown on coverage plot
