4eb566a0cdd8eb5cf924334fe0d707eed2e382f0
Ilya Soifer <ilya.soifer@ultimagen.com>
2025-10-19 07:34:16 +0000

    Removed unused modules

--- ugvc/__main__.py
+++ ugvc/__main__.py
@@ -11,6 +11,8 @@ path = f"{dirname(dirname(__file__))}"
 if path not in sys.path:
     sys.path.insert(0, path)
 
+from ugbio_cloud_utils import cloud_sync
+
 # import pipeline modules implementing run(argv) method
 from ugbio_cnv import (
     annotate_FREEC_segments,
@@ -20,8 +22,11 @@ from ugbio_cnv import (
     plot_cnv_results,
     plot_FREEC_neutral_AF,
 )
-
-from ugvc.joint import compress_gvcf
+from ugbio_comparison import run_comparison_pipeline
+from ugbio_core import intersect_bed_regions, sorter_to_h5
+from ugbio_core.vcfbed import annotate_contig
+from ugbio_filtering import filter_variants_pipeline, train_models_pipeline, training_prep_pipeline
+from ugbio_filtering.sec import assess_sec_concordance, correct_systematic_errors, sec_training, sec_validation
 from ugbio_methylation import (
     concat_methyldackel_csvs,
     process_mbias,
@@ -29,6 +34,8 @@ from ugbio_methylation import (
     process_merge_context_no_cp_g,
     process_per_read,
 )
+
+from ugvc.joint import compress_gvcf
 from ugvc.pipelines import (
     convert_haploid_regions,
     correct_genotypes_by_imputation,
@@ -37,29 +44,10 @@ from ugvc.pipelines import (
     evaluate_concordance,
     vcfeval_flavors,
 )
-from ugbio_filtering import filter_variants_pipeline, train_models_pipeline, training_prep_pipeline
 from ugvc.pipelines.comparison import quick_fingerprinting
 from ugvc.pipelines.deepvariant import training_set_consistency_check
 from ugvc.pipelines.lpr import filter_vcf_with_lib_prep_recalibration_model, train_lib_prep_recalibration_model
-from ugbio_featuremap import annotate_featuremap, create_hom_snv_featuremap, featuremap_to_dataframe, \
-    pileup_featuremap
-from ugbio_mrd import (
-    generate_synthetic_signatures,
-    intersect_featuremap_with_signature
-)
-from ugbio_ppmseq import (
-    ppmSeq_qc_analysis
-)
-from ugbio_srsnv import (
-    srsnv_inference,
-    srsnv_training
-)
-from ugbio_core.vcfbed import annotate_contig
-from ugbio_core import intersect_bed_regions, sorter_stats_to_mean_coverage, sorter_to_h5
-from ugbio_comparison import run_comparison_pipeline
-from ugbio_filtering.sec import assess_sec_concordance, correct_systematic_errors, sec_training, sec_validation
 from ugvc.pipelines.vcfbed import calibrate_bridging_snvs, gvcf_hcr
-from ugbio_cloud_utils import cloud_sync
 
 # create a list of imported pipeline modules
 modules = [
@@ -82,19 +70,6 @@ modules = [
 ]
 
 sec_modules = [correct_systematic_errors, sec_training, sec_validation]
-
-mrd_modules = [
-    ppmSeq_qc_analysis,
-    sorter_stats_to_mean_coverage,
-    featuremap_to_dataframe,
-    intersect_featuremap_with_signature,
-    generate_synthetic_signatures,
-    annotate_featuremap,
-    srsnv_training,
-    srsnv_inference,
-    create_hom_snv_featuremap,
-    pileup_featuremap,
-]
 
 methylation_modules = [
     concat_methyldackel_csvs,
@@ -121,7 +96,6 @@ vcfbed_modules = [annotate_contig, intersect_bed_regions, calibrate_bridging_snv
 deepvariant_modules = [training_set_consistency_check]
 comparison_modules = [quick_fingerprinting]
 
-modules.extend(mrd_modules)
 modules.extend(sec_modules)
 modules.extend(misc_modules)
 modules.extend(methylation_modules)
