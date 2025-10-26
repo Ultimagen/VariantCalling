bd0bb42aec4f71b0bbd67616eeb6e933ab2e3278
Ilya Soifer <ilya.soifer@ultimagen.com>
2025-10-26 07:57:07 +0000

    Added ignore_filter parameter to collecting sv stats

--- ugvc/pipelines/sv_stats_collect.py
+++ ugvc/pipelines/sv_stats_collect.py
@@ -13,7 +13,7 @@ SVBINS = [0, 100, 300, 500, 1000, 5000, 10000, 100000, 1000000, float("inf")]
 SVLABELS = ["50-100", "100-300", "300-500", "0.5-1k", "1k-5k", "5k-10k", "10k-100k", "100k-1M", ">1M"]
 
 
-def collect_size_type_histograms(svcall_vcf):
+def collect_size_type_histograms(svcall_vcf, ignore_filter: bool = False) -> dict[str, pd.DataFrame]:
     """
     Collect size and type histograms from SV call VCF.
 
@@ -21,14 +21,19 @@ def collect_size_type_histograms(svcall_vcf):
     ----------
     svcall_vcf : str
         Path to the SV call VCF file.
+    ignore_filter : bool, optional
+        Whether to ignore the FILTER field in the VCF file, by default False
 
     Returns
     -------
-    dict[pd.DataFrame]
+    dict[str,pd.DataFrame]
         Dictionary containing size and type histograms.
     """
     # Read the VCF file
-    vcf_df = vcftools.get_vcf_df(svcall_vcf, custom_info_fields=["SVLEN", "SVTYPE"]).query("filter=='PASS'")
+    if ignore_filter:
+        vcf_df = vcftools.get_vcf_df(svcall_vcf, custom_info_fields=["SVLEN", "SVTYPE"])
+    else:
+        vcf_df = vcftools.get_vcf_df(svcall_vcf, custom_info_fields=["SVLEN", "SVTYPE"]).query("filter=='PASS'")
     vcf_df["svlen"] = vcf_df["svlen"].apply(lambda x: x[0] if isinstance(x, tuple) else x).fillna(0)
     vcf_df["binned_svlens"] = pd.cut(
         vcf_df["svlen"].abs(),
@@ -124,7 +129,9 @@ def concordance_with_gt_roc(df_base: pd.DataFrame, df_calls: pd.DataFrame) -> pd
     return pd.Series(dict(zip(["precision", "recall", "thresholds"], [precision, recall, thresholds])))
 
 
-def collect_sv_stats(svcall_vcf: str, concordance_h5: str | None = None) -> tuple[dict, dict, pd.Series]:
+def collect_sv_stats(
+    svcall_vcf: str, concordance_h5: str | None = None, ignore_filter: bool = False
+) -> tuple[dict, dict, pd.Series]:
     """
     Collect SV statistics from a VCF file. Optionally, collect also statistics about concordance with the ground truth
 
@@ -134,6 +141,8 @@ def collect_sv_stats(svcall_vcf: str, concordance_h5: str | None = None) -> tupl
         Path to the SV call VCF file.
     concordance_h5 : str, optional
         Path to the concordance HDF5 file, by default None
+    ignore_filter: bool, optional
+        Whether to ignore the FILTER field in the VCF file, by default False
 
     Returns
     -------
@@ -142,7 +151,7 @@ def collect_sv_stats(svcall_vcf: str, concordance_h5: str | None = None) -> tupl
         and false positive statistics seris (number of false positives
         per variant type and length)
     """
-    sv_stats = collect_size_type_histograms(svcall_vcf)
+    sv_stats = collect_size_type_histograms(svcall_vcf, ignore_filter=ignore_filter)
     concordance_stats = {}
     fp_stats = pd.Series(dtype="int64")
     if concordance_h5 is not None:
@@ -215,10 +224,18 @@ def run(args: list[str]):
     parser.add_argument(
         "--concordance_h5", type=str, help="Path to the concordance HDF5 file.", default=None, required=False
     )
+    parser.add_argument(
+        "--ignore_filter",
+        action="store_true",
+        help="Whether to ignore the FILTER field in the VCF file.",
+        default=False,
+    )
 
     p_args = parser.parse_args(args[1:])
 
-    sv_stats, concordance_stats, fp_stats_df = collect_sv_stats(p_args.svcall_vcf, p_args.concordance_h5)
+    sv_stats, concordance_stats, fp_stats_df = collect_sv_stats(
+        p_args.svcall_vcf, p_args.concordance_h5, p_args.ignore_filter
+    )
     results = {}
     if concordance_stats:
         concordance_df = pd.DataFrame({k: v for k, v in concordance_stats.items() if "concordance" in k}).T
